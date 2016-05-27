00131cf889e139f7d2471fb3138ddb0b08a1b38f
U-CORP\zhangv11 <zhangv11@CNADZHANGV1L2C.corp.emc.com>
2016-05-27 18:01:44 +0800

    Support horizontal bar in 25.0.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -38,6 +38,7 @@
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
 
 ;; Title
 (setq frame-title-format
