a0abdd309170db55aa4f1bb3232129387db9c553
Vincent Zhang <seagle0128@gmail.com>
2016-06-10 01:57:28 +0800

    Add custom example file.

---
+++ custom-example.el
@@ -0,0 +1,7 @@
+;;; custom.el --- user customization file      -*- no-byte-compile: t -*-
+;;; Copy custom-example.el to custom.el and change the variables
+
+;; (setq profile-init t)
+;; (setq restore-desktop nil)
+;; (setq auto-complete-system 'auto-complete)
+;; (setq completion-framework 'ivy)
