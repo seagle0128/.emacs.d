adfaeacabcc31f4fe8dd0b5c4b46e17c803b077e
Vincent Zhang <seagle0128@gmail.com>
2016-06-10 01:51:07 +0800

    Revert "Add custom file."
    This reverts commit faffe43a569817bc79793ccfe5a989adfe9df2ed.

--- custom.el
+++
@@ -1,6 +0,0 @@
-;;; custom.el --- user customization file      -*- no-byte-compile: t -*-
-
-;; (setq profile-init t)
-;; (setq restore-desktop nil)
-;; (setq auto-complete-system 'auto-complete)
-;; (setq completion-framework 'ivy)
