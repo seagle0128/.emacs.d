147169af6f4246bb1df5945e0e6350db22ec7287
Vincent Zhang <seagle0128@gmail.com>
2016-06-14 11:27:05 +0800

    Enhance ido mode.

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -32,75 +32,71 @@
 ;;
 ;;; Code:
 
-;; Smex
+(eval-when-compile (require 'ido))
+
+;; IDO
+(ido-mode 1)
+(ido-everywhere 1)
+(setq ido-use-filename-at-point 'guess)
+(setq ido-create-new-buffer 'always)
+(setq ido-enable-flex-matching t)
+
+(eval-after-load 'recentf
+  '(lambda ()
+     (defun ido-recentf-find-file ()
+       "Find a recent file using ido."
+       (interactive)
+       (let ((file (ido-completing-read "Choose recent file: "
+                                        (-map 'abbreviate-file-name recentf-list)
+                                        nil t)))
+         (when file
+           (find-file file))))
+     (bind-key "C-x C-r" 'ido-recentf-find-file)))
+
+(use-package ido-ubiquitous
+  :config (ido-ubiquitous-mode 1))
+
+(use-package ido-at-point
+  :config (ido-at-point-mode 1))
+
+(use-package ido-complete-space-or-hyphen
+  :config (ido-complete-space-or-hyphen-enable))
+
+(use-package ido-sort-mtime
+  :config (ido-sort-mtime-mode 1))
+
+(use-package ido-vertical-mode
+  :init
+  (progn
+    (setq ido-vertical-define-keys 'C-n-and-C-p-only)
+    (setq ido-vertical-show-count t)
+    )
+  :config (ido-vertical-mode 1))
+
+(use-package flx-ido
+  :config (flx-ido-mode 1))
+
+(use-package ido-load-library
+  :defer t
+  :init (defalias 'load-library 'ido-load-library))
+
+(use-package idomenu
+  :defer t
+  :bind ("C-." . idomenu))
+
 (use-package smex
   :defer t
   :bind (("M-x" . smex)
          ("M-X" . smex-major-mode-commands)
          ("C-c M-x" . execute-extended-command)))
 
-;; IDO
-(use-package ido
+(use-package swoop
   :defer t
-  :config
-  (progn
-    (ido-mode 1)
-    (ido-everywhere 1)
-    (setq ido-use-filename-at-point 'guess)
-    (setq ido-create-new-buffer 'always)
-    (setq ido-enable-flex-matching t)
-
-    (eval-after-load 'recentf
-      '(lambda ()
-         (defun ido-recentf-find-file ()
-           "Find a recent file using ido."
-           (interactive)
-           (let ((file (ido-completing-read "Choose recent file: "
-                                            (-map 'abbreviate-file-name recentf-list)
-                                            nil t)))
-             (when file
-               (find-file file))))
-         (bind-key "C-x C-r" 'ido-recentf-find-file)))
-
-    (use-package ido-ubiquitous
-      :config (ido-ubiquitous-mode 1))
-
-    (use-package ido-at-point
-      :config (ido-at-point-mode 1))
-
-    (use-package ido-complete-space-or-hyphen
-      :config (ido-complete-space-or-hyphen-enable))
-
-    (use-package ido-sort-mtime
-      :config (ido-sort-mtime-mode 1))
-
-    (use-package ido-vertical-mode
-      :init
-      (progn
-        (setq ido-vertical-define-keys 'C-n-and-C-p-only)
-        (setq ido-vertical-show-count t)
-        )
-      :config (ido-vertical-mode 1))
-
-    (use-package flx-ido
-      :config (flx-ido-mode 1))
-
-    (use-package ido-load-library
-      :defer t
-      :init (defalias 'load-library 'ido-load-library))
-
-    (use-package idomenu
-      :defer t
-      :bind ("C-." . idomenu))
-
-    (use-package swoop
-      :defer t
-      :bind (("C-o" . swoop)
-             ("C-M-o" . swoop-multi)
-             ("M-o" . swoop-pcre-regexp)
-             ("C-S-o" . swoop-back-to-last-position))
-      :init (setq swoop-font-size-change: nil))
-    ))
+  :bind (("C-o" . swoop)
+         ("C-M-o" . swoop-multi)
+         ("M-o" . swoop-pcre-regexp)
+         ("C-S-o" . swoop-back-to-last-position))
+  :init (setq swoop-font-size-change: nil))
 
 (provide 'init-ido)
 
