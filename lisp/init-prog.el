fbd5b4c38cf33daed89a2665d9a7ceccf8e39332
Vincent Zhang <seagle0128@gmail.com>
2016-06-21 21:56:27 +0800

    Fix robot-mode.

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -40,10 +40,6 @@
 (eval-when-compile (require 'aggressive-indent))
 (require 'robot-mode)
 (add-to-list 'auto-mode-alist '("\\.robot\\'" . robot-mode))
-(add-hook 'robot-mode-hook
-          '(lambda ()
-             (diminish 'robot-mode "")
-             (setq indent-tabs-mode t)))
 (eval-after-load 'aggressive-indent
   '(add-to-list 'aggressive-indent-excluded-modes 'robot-mode))
 
