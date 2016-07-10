96d6bc174bf0e76c0c2dbdebe35854faf98a3b3c
Vincent Zhang <seagle0128@gmail.com>
2016-07-10 15:42:10 +0800

    Use http for mepla.

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -42,8 +42,8 @@
 ;; (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 ;; Mirror
-(setq package-archives '(("gnu"   . "https://elpa.zilongshanren.com/gnu/")
-                         ("melpa" . "https://elpa.zilongshanren.com/melpa/")))
+(setq package-archives '(("gnu"   . "http://elpa.zilongshanren.com/gnu/")
+                         ("melpa" . "http://elpa.zilongshanren.com/melpa/")))
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
