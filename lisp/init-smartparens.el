3295fc27809ff8671d290d739e2dc6af81530a72
Vincent Zhang <seagle0128@gmail.com>
2016-07-10 23:46:59 +0800

    Enhance smarparens.

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -36,6 +36,53 @@
   :defer t
   :diminish smartparens-mode
   :commands sp-with-modes
+  :bind (:map smartparens-mode-map
+              ("C-M-a" . sp-beginning-of-sexp)
+              ("C-M-e" . sp-end-of-sexp)
+
+              ("C-<down>" . sp-down-sexp)
+              ("C-<up>"   . sp-up-sexp)
+              ("M-<down>" . sp-backward-down-sexp)
+              ("M-<up>"   . sp-backward-up-sexp)
+
+              ("C-M-f" . sp-forward-sexp)
+              ("C-M-b" . sp-backward-sexp)
+
+              ("C-M-n" . sp-next-sexp)
+              ("C-M-p" . sp-previous-sexp)
+
+              ;; ("C-S-f" . sp-forward-symbol)
+              ;; ("C-S-b" . sp-backward-symbol)
+
+              ("C-<right>" . sp-forward-slurp-sexp)
+              ("M-<right>" . sp-forward-barf-sexp)
+              ("C-<left>"  . sp-backward-slurp-sexp)
+              ("M-<left>"  . sp-backward-barf-sexp)
+
+              ("C-M-t" . sp-transpose-sexp)
+              ("C-M-k" . sp-kill-sexp)
+              ("C-k"   . sp-kill-hybrid-sexp)
+              ("M-k"   . sp-backward-kill-sexp)
+              ("C-M-w" . sp-copy-sexp)
+
+              ("C-M-d" . delete-sexp)
+
+              ("M-<backspace>" . backward-kill-word)
+              ("C-<backspace>" . sp-backward-kill-word)
+              ([remap sp-backward-kill-word] . backward-kill-word)
+
+              ("M-[" . sp-backward-unwrap-sexp)
+              ("M-]" . sp-unwrap-sexp)
+
+              ("C-x C-t" . sp-transpose-hybrid-sexp)
+
+              ("C-c ("  . wrap-with-parens)
+              ("C-c ["  . wrap-with-brackets)
+              ("C-c {"  . wrap-with-braces)
+              ("C-c '"  . wrap-with-single-quotes)
+              ("C-c \"" . wrap-with-double-quotes)
+              ("C-c _"  . wrap-with-underscores)
+              ("C-c `"  . wrap-with-back-quotes))
   :init
   (progn
     (add-hook 'after-init-hook 'smartparens-global-mode)
@@ -64,6 +111,26 @@
                   ("k" sp-kill-sexp "Kill" :color blue)
                   ("q" nil "Quit" :color blue))
                 smartparens-mode-map))
+
+    ;; Define wrapping functions
+    (defmacro def-pairs (pairs)
+      `(progn
+         ,@(loop for (key . val) in pairs
+                 collect
+                 `(defun ,(read (concat
+                                 "wrap-with-"
+                                 (prin1-to-string key)
+                                 "s"))
+                      (&optional arg)
+                    (interactive "p")
+                    (sp-wrap-with-pair ,val)))))
+
+    (def-pairs ((paren        . "(")
+                (bracket      . "[")
+                (brace        . "{")
+                (single-quote . "'")
+                (double-quote . "\"")
+                (back-quote   . "`")))
 
     ;; Pair Management
     (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
@@ -144,7 +211,8 @@
     (sp-with-modes '(malabar-mode c++-mode)
       (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET"))))
     (sp-local-pair 'c++-mode "/*" "*/" :post-handlers '((" | " "SPC")
-                                                        ("* ||\n[i]" "RET")))))
+                                                        ("* ||\n[i]" "RET")))
+    ))
 
 (provide 'init-smartparens)
 
