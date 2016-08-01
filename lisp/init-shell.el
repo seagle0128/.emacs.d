dc8c924a3912e9095f8f9734fbc482b3ac634552
Vincent Zhang <seagle0128@gmail.com>
2016-08-01 23:09:10 +0800

    Improve shell/term.

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -33,55 +33,63 @@
 ;;; Code:
 
 ;; Shell
-(setq system-uses-terminfo nil)
 
-(add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-(add-hook 'shell-mode-hook 'n-shell-mode-hook)
+(require 'init-const)
 
-(defun n-shell-mode-hook ()
-  "Shell mode customizations."
-  (local-set-key '[up] 'comint-previous-input)
-  (local-set-key '[down] 'comint-next-input)
-  (local-set-key '[(shift tab)] 'comint-next-matching-input-from-input)
-  (setq comint-input-sender 'n-shell-simple-send)
-  )
+(use-package shell
+  :defer t
+  :config
+  (progn
+    (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
+    (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+    (add-hook 'shell-mode-hook 'n-shell-mode-hook)
 
-(defun n-shell-simple-send (proc command)
-  "Various PROC COMMANDs pre-processing before sending to shell."
-  (cond
-   ;; Checking for clear command and execute it.
-   ((string-match "^[ \t]*clear[ \t]*$" command)
-    (comint-send-string proc "\n")
-    (erase-buffer)
-    )
-   ;; Checking for man command and execute it.
-   ((string-match "^[ \t]*man[ \t]*" command)
-    (comint-send-string proc "\n")
-    (setq command (replace-regexp-in-string "^[ \t]*man[ \t]*" "" command))
-    (setq command (replace-regexp-in-string "[ \t]+$" "" command))
-    ;;(message (format "command %s command" command))
-    (funcall 'man command)
-    )
-   ;; Send other commands to the default handler.
-   (t (comint-simple-send proc command))
-   )
-  )
+    (defun n-shell-mode-hook ()
+      "Shell mode customizations."
+      (local-set-key '[up] 'comint-previous-input)
+      (local-set-key '[down] 'comint-next-input)
+      (local-set-key '[(shift tab)] 'comint-next-matching-input-from-input)
+      (setq comint-input-sender 'n-shell-simple-send))
 
-;; Term
-(defvar term-program shell-file-name)
-(cond ((executable-find "fish")
-       (setq term-program "fish"))
-      ((executable-find "zsh")
-       (setq term-program "zsh")))
+    (defun n-shell-simple-send (proc command)
+      "Various PROC COMMANDs pre-processing before sending to shell."
+      (cond
+       ;; Checking for clear command and execute it.
+       ((string-match "^[ \t]*clear[ \t]*$" command)
+        (comint-send-string proc "\n")
+        (erase-buffer)
+        )
+       ;; Checking for man command and execute it.
+       ((string-match "^[ \t]*man[ \t]*" command)
+        (comint-send-string proc "\n")
+        (setq command (replace-regexp-in-string "^[ \t]*man[ \t]*" "" command))
+        (setq command (replace-regexp-in-string "[ \t]+$" "" command))
+        ;;(message (format "command %s command" command))
+        (funcall 'man command)
+        )
+       ;; Send other commands to the default handler.
+       (t (comint-simple-send proc command))
+       ))
+    ))
 
-(setenv "SHELL" term-program)
+;; Do not use term on Windows
+(when (not sys/win32p)
+  (defvar term-program shell-file-name)
+  (cond ((executable-find "fish")
+         (setq term-program "fish"))
+        ((executable-find "zsh")
+         (setq term-program "zsh")))
+
+  (setenv "SHELL" term-program))
 
 ;; Multi term
 (use-package multi-term
   :defer t
-  :init
+  :if (not sys/win32p)
+  :config
   (progn
+    (setq system-uses-terminfo nil)
+
     (setq multi-term-program term-program)
     ;; Disable yasnippet mode to enable TAB in term
     (add-hook 'term-mode-hook '(lambda() (yas-minor-mode -1)))
@@ -90,10 +98,13 @@
 ;; Shell Pop
 (use-package shell-pop
   :defer t
+  :if (not sys/win32p)
   :bind ([f8] . shell-pop)
-  :init
+  :config
   (progn
-    (setq shell-pop-shell-type '("ansi-term" "*ansi-term*" (lambda nil (ansi-term shell-pop-term-shell))))
+    (setq shell-pop-shell-type
+          '("ansi-term" "*ansi-term*"
+            (lambda nil (ansi-term shell-pop-term-shell))))
     (setq shell-pop-term-shell term-program)
     ;; (setq shell-pop-universal-key "C-t")
     (setq shell-pop-window-size 30)
