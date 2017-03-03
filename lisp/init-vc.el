04d8f94b30c4b0215ae4ee87e05beb9931e9e70b
Vincent Zhang <seagle0128@gmail.com>
2017-03-03 09:38:18 +0800

    Fix https://github.com/magit/magit/issues/3014.

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -44,7 +44,20 @@
   ;; Workaround for fixing
   ;; https://github.com/dgutov/diff-hl/issues/85
   ;; https://github.com/magit/magit/issues/3014
-  (magit-auto-revert-mode -1))
+  ;; (magit-auto-revert-mode -1)
+  (defun magit-turn-on-auto-revert-mode-if-desired (&optional file)
+    (if file
+        (--when-let (find-buffer-visiting file)
+          (with-current-buffer it
+            (magit-turn-on-auto-revert-mode-if-desired)))
+      (when (and buffer-file-name
+                 (file-readable-p buffer-file-name)
+                 (magit-toplevel)
+                 (or (not magit-auto-revert-tracked-only)
+                     (magit-file-tracked-p buffer-file-name)))
+        (unless (or auto-revert-mode global-auto-revert-mode)
+          (auto-revert-mode)))))
+  )
 
 (use-package git-timemachine :defer t)
 (use-package gitconfig-mode :defer t)
