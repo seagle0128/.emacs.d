7d1b18939ba8a91617a602788c1d4e6ac3525087
Vincent Zhang <seagle0128@gmail.com>
2017-03-12 22:58:31 +0800

    New: git-messenger.

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -63,6 +63,16 @@
   :diminish magit-gitflow-mode
   :init (add-hook 'magit-mode-hook 'turn-on-magit-gitflow))
 
+;;; Pop up last commit information of current line
+(use-package git-messenger
+  :commands git-messenger:copy-message
+  :bind (("C-x v p" . git-messenger:popup-message)
+         :map git-messenger-map
+         ("m" . git-messenger:copy-message))
+  :init
+  ;; Use magit-show-commit for showing status/diff commands
+  (setq git-messenger:use-magit-popup t))
+
 (use-package git-timemachine)
 (use-package gitconfig-mode)
 (use-package gitignore-mode)
