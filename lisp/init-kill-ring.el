e64544d4ff33ac20e387d9ea5a94207628c6ec98
Vincent Zhang <seagle0128@gmail.com>
2017-05-11 23:59:21 +0800

    Improve kill-ring.

--- lisp/init-kill-ring.el
+++ lisp/init-kill-ring.el
@@ -32,15 +32,17 @@
 ;;
 ;;; Code:
 
-(setq kill-ring-max 60)
+(setq kill-ring-max 200)
 
 ;; Save clipboard contents into kill-ring before replace them
 (setq save-interprogram-paste-before-kill t)
 
+;; Kill & Mark things easily
 (use-package easy-kill
   :bind (([remap kill-ring-save] . easy-kill)
          ([remap mark-sexp] . easy-mark)))
 
+;; Interactively insert items from kill-ring
 (use-package browse-kill-ring
   :bind ("C-c k" . browse-kill-ring)
   :init (add-hook 'after-init-hook 'browse-kill-ring-default-keybindings))
