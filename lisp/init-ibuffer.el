63a9d413707dd9dea0febca6dcc0a18cf1f33a0d
Vincent Zhang <seagle0128@gmail.com>
2017-05-13 00:03:27 +0800

    Enhance ibuffer-vc.

--- lisp/init-ibuffer.el
+++ lisp/init-ibuffer.el
@@ -32,20 +32,22 @@
 ;;
 ;;; Code:
 
-(use-package ibuffer
-  :ensure nil
-  :defines ibuffer-saved-filter-groups
-  :commands ibuffer-switch-to-saved-filter-groups
+;; Group ibuffer's list by VC project
+(use-package ibuffer-vc
   :bind ("C-x C-b" . ibuffer)
-  :config
-  (use-package ibuffer-vc
-    :init
-    (add-hook 'ibuffer-hook
-              (lambda ()
-                (ibuffer-vc-set-filter-groups-by-vc-root)
-                (unless (eq ibuffer-sorting-mode 'alphabetic)
-                  (ibuffer-do-sort-by-alphabetic)))))
-  )
+  :init
+  (setq ibuffer-formats
+        '((mark modified read-only vc-status-mini " "
+                (name 18 18 :left :elide)
+                " "
+                (size 9 -1 :right)
+                " "
+                (mode 16 16 :left :elide)
+                " "
+                (vc-status 16 16 :left)
+                " "
+                filename-and-process)))
+  (add-hook 'ibuffer-hook 'ibuffer-vc-set-filter-groups-by-vc-root))
 
 (provide 'init-ibuffer)
 
