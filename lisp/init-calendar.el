77f1980499084f1b8dd7e540af88b73b9b743c23
Vincent Zhang <seagle0128@gmail.com>
2017-07-22 18:23:33 +0800

    Enhance calendar.

--- lisp/init-calendar.el
+++ lisp/init-calendar.el
@@ -39,9 +39,28 @@
   :init
   (add-hook 'calendar-mode-hook 'cal-china-x-setup)
   :config
+  ;; Holidays
   (setq calendar-mark-holidays-flag t)
+
   (setq cal-china-x-important-holidays cal-china-x-chinese-holidays)
-  (setq calendar-holidays cal-china-x-important-holidays)
+  (setq cal-china-x-general-holidays
+        '((holiday-lunar 1 15 "元宵节")
+          (holiday-fixed 3 8 "妇女节")
+          (holiday-fixed 3 12 "植树节")
+          (holiday-fixed 5 4 "青年节")
+          (holiday-fixed 6 1 "儿童节")
+          (holiday-fixed 9 10 "教师节")))
+  (setq other-holidays
+        '((holiday-fixed 2 14 "情人节")
+          (holiday-fixed 4 1 "愚人节")
+          (holiday-fixed 12 25 "圣诞节")
+          (holiday-float 5 0 2 "母亲节")
+          (holiday-float 6 0 3 "父亲节")
+          (holiday-float 11 4 4 "感恩节")))
+  (setq calendar-holidays
+        (append cal-china-x-important-holidays
+                cal-china-x-general-holidays
+                other-holidays))
 
   ;; `S' can show the time of sunrise and sunset on Calendar
   (setq calendar-location-name "Chengdu"
