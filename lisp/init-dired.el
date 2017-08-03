7566d041b23b2171cf3cbc8de043b00aefeb85a0
Vincent Zhang <seagle0128@gmail.com>
2017-08-03 10:51:22 +0800

    Improve dired-rainbow.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -91,22 +91,38 @@
   (use-package dired-rainbow
     :commands dired-rainbow-define dired-rainbow-define-chmod
     :init
-    (dired-rainbow-define html "#4e9a06" ("htm" "html" "xhtml" "xml"))
-    (dired-rainbow-define prog "yellow3" ("el" "py" "rb" "pl" "c" "cpp" "cxx" "h" "hpp" "hxx" "swift" "go" "java" "js"))
-    (dired-rainbow-define text "YellowGreen" ("txt" "md" "org" "ini" "conf"))
-    (dired-rainbow-define doc "SpringGreen3" ("doc" "docx" "ppt" "pptx" "xls" "xlsx"))
     (dired-rainbow-define dotfiles "gray" "\\..*")
-    (dired-rainbow-define misc "gray50" ("DS_Store" "projectile" "cache" "elc" "dat" "meta"))
-    (dired-rainbow-define media "#ce5c00" ("mp3" "mp4" "MP3" "MP4" "wav" "wma" "wmv" "mov" "3gp" "avi" "mpg" "mkv" "flv" "ogg" "rm" "rmvb"))
-    (dired-rainbow-define picture "purple3" ("bmp" "jpg" "jpeg" "gif" "png" "tiff" "ico" "svg" "psd" "pcd" "raw" "exif" "BMP" "JPG" "PNG"))
-    (dired-rainbow-define archive "SaddleBrown" ("zip" "tar" "gz" "7z" "rar" "gzip" "001" "ace" "bz2" "bzip2" "cab" "jar" "iso"))
+
+    (dired-rainbow-define web "#4e9a06" ("htm" "html" "xhtml" "xml" "xaml" "css" "js"
+                                         "json" "asp" "aspx" "haml" "php" "jsp" "ts"
+                                         "coffee" "scss" "less" "phtml"))
+    (dired-rainbow-define prog "yellow3" ("el" "l" "ml" "py" "rb" "pl" "pm" "c"
+                                          "cpp" "cxx" "c++" "h" "hpp" "hxx" "h++"
+                                          "m" "cs" "mk" "make" "swift" "go" "java"
+                                          "asm" "robot" "yml" "rake" "lua"))
+    (dired-rainbow-define sh "GreenYellow" ("sh" "bash" "zsh" "fish" "csh" "awk"
+                                            "ps1" "psm1" "psd1" "bat" "cmd"))
+    (dired-rainbow-define text "YellowGreen" ("txt" "md" "org" "ini" "conf" "rc"
+                                              "vim" "vimrc" "exrc"))
+    (dired-rainbow-define doc "SpringGreen3" ("doc" "docx" "ppt" "pptx" "xls"
+                                              "xlsx" "pdf" "texi"))
+    (dired-rainbow-define misc "gray50" ("DS_Store" "projectile" "cache" "elc"
+                                         "dat" "meta"))
+    (dired-rainbow-define media "#ce5c00" ("mp3" "mp4" "MP3" "MP4" "wav" "wma"
+                                           "wmv" "mov" "3gp" "avi" "mpg" "mkv"
+                                           "flv" "ogg" "rm" "rmvb"))
+    (dired-rainbow-define picture "purple3" ("bmp" "jpg" "jpeg" "gif" "png" "tiff"
+                                             "ico" "svg" "psd" "pcd" "raw" "exif"
+                                             "BMP" "JPG" "PNG"))
+    (dired-rainbow-define archive "SaddleBrown" ("zip" "tar" "gz" "tgz" "7z" "rar"
+                                                 "gzip" "xz" "001" "ace" "bz2" "lz"
+                                                 "lzma" "bzip2" "cab" "jar" "iso"))
 
     ;; boring regexp due to lack of imagination
-    (dired-rainbow-define log (:inherit default
-                                        :italic t) ".*\\.log")
+    (dired-rainbow-define log (:inherit default :italic t) ".*\\.log")
 
     ;; highlight executable files, but not directories
-    (dired-rainbow-define-chmod executable-unix "Green" "-[rw-]+x.*"))
+    (dired-rainbow-define-chmod executable-unix "green" "-[rw-]+x.*"))
 
   ;; Highlights dired buffer like k
   (use-package dired-k
