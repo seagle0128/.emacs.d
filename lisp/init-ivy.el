cd6c9642eee358d74afc2af2be95e3aa9158ed42
Vincent Zhang <seagle0128@gmail.com>
2017-08-26 00:19:55 +0800

    Use faster search tools in counsel-grep-or-swiper.

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -34,7 +34,7 @@
 
 (use-package counsel
   :diminish ivy-mode counsel-mode
-  :bind (("C-s" . swiper)
+  :bind (("C-s" . counsel-grep-or-swiper)
          ("C-S-s" . swiper-all)
 
          ("C-c C-r" . ivy-resume)
@@ -79,6 +79,15 @@
   (setq swiper-action-recenter t)
   (setq counsel-find-file-at-point t)
   (setq counsel-yank-pop-separator "\n-------\n")
+
+  ;; Use faster search tools: ripgrep or the silver search
+  (let ((command
+         (cond
+          ((executable-find "rg")
+           "rg -i -M 120 --no-heading --line-number --color never '%s' %s")
+          ((executable-find "ag")
+           "ag -i -m 120 --noheading --nocolor --nofilename --numbers '%s' %s"))))
+    (setq counsel-grep-base-command command))
 
   (with-eval-after-load 'projectile
     (setq projectile-completion-system 'ivy))
