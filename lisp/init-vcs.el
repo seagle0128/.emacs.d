f83d8ad3772231b37fa5a0c9406ed33d06779fea
Vincent Zhang <seagle0128@gmail.com>
2017-08-25 13:52:39 +0800

    Revert "Feature: Pretty magit: http://www.modernemacs.com/post/pretty-magit."
    This reverts commit bb7d4ab1c80ec7294f355ad45b514ab4f49bc065.

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -41,64 +41,7 @@
          ("C-c M-g" . magit-file-popup))
   :config
   (when sys/win32p
-    (setenv "GIT_ASKPASS" "git-gui--askpass"))
-
-  ;; Pretty magit http://www.modernemacs.com/post/pretty-magit
-  (defmacro pretty-magit (WORD ICON PROPS &optional NO-PROMPT?)
-    "Replace sanitized WORD with ICON, PROPS and by default add to prompts."
-    `(prog1
-         (add-to-list 'pretty-magit-alist
-                      (list (rx bow (group ,WORD (eval (if ,NO-PROMPT? "" ":"))))
-                            ,ICON ',PROPS))
-       (unless ,NO-PROMPT?
-         (add-to-list 'pretty-magit-prompt (concat ,WORD ": ")))))
-
-  (setq pretty-magit-alist nil)
-  (setq pretty-magit-prompt nil)
-  (pretty-magit "Feature" ? (:foreground "slate gray" :height 1.2))
-  (pretty-magit "Add"     ? (:foreground "#375E97" :height 1.2))
-  (pretty-magit "Fix"     ? (:foreground "#FB6542" :height 1.2))
-  (pretty-magit "Clean"   ? (:foreground "#FFBB00" :height 1.2))
-  (pretty-magit "Docs"    ? (:foreground "#3F681C" :height 1.2))
-  ;; (pretty-magit "origin"  ? (:box t :height 1.2) t)
-  (pretty-magit "master"  ? (:box t :height 1.2) t)
-  (pretty-magit "origin"  ? (:box t :height 1.2) t)
-
-  (defun add-magit-faces ()
-    "Add face properties and compose symbols for buffer from pretty-magit."
-    (interactive)
-    (with-silent-modifications
-      (--each pretty-magit-alist
-        (-let (((rgx icon props) it))
-          (save-excursion
-            (goto-char (point-min))
-            (while (search-forward-regexp rgx nil t)
-              (compose-region
-               (match-beginning 1) (match-end 1) icon)
-              (when props
-                (add-face-text-property
-                 (match-beginning 1) (match-end 1) props))))))))
-
-  (advice-add 'magit-status :after 'add-magit-faces)
-  (advice-add 'magit-refresh-buffer :after 'add-magit-faces)
-
-  (with-eval-after-load 'ivy
-    (setq use-magit-commit-prompt-p nil)
-    (defun use-magit-commit-prompt (&rest args)
-      (setq use-magit-commit-prompt-p t))
-
-    (defun magit-commit-prompt ()
-      "Magit prompt and insert commit header with faces."
-      (interactive)
-      (when use-magit-commit-prompt-p
-        (setq use-magit-commit-prompt-p nil)
-        (insert (ivy-read "Commit Type " pretty-magit-prompt
-                          :require-match t :sort t :preselect "Add: "))
-        (add-magit-faces)))
-
-    (remove-hook 'git-commit-setup-hook 'with-editor-usage-message)
-    (add-hook 'git-commit-setup-hook 'magit-commit-prompt)
-    (advice-add 'magit-commit :after 'use-magit-commit-prompt)))
+    (setenv "GIT_ASKPASS" "git-gui--askpass")))
 
 ;; Gitflow externsion for Magit
 (use-package magit-gitflow
