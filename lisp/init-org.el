e7f8948d0bbb6df6ef11fcbb3f0fbc95ca0e76a6
Vincent Zhang <seagle0128@gmail.com>
2017-08-29 12:02:35 +0800

    Fix: org-pomodor keybinding ("P") in org-agenda.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -84,7 +84,7 @@
   ;; Pomodoro
   (use-package org-pomodoro
     :bind (:map org-agenda-mode-map
-                ("p" . org-pomodoro)))
+                ("P" . org-pomodoro)))
 
   ;; Visually summarize progress
   (use-package org-dashboard)
