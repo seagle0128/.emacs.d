598d69a3777b5ba8c90dd8962e0bf325822f2efb
Vincent Zhang <seagle0128@gmail.com>
2017-09-05 19:49:35 +0800

    Clean: emacs lisp mode and eldoc mode.

--- lisp/init-emacs-lisp.el
+++ lisp/init-emacs-lisp.el
@@ -35,7 +35,7 @@
 (require 'init-custom)
 
 ;; Emacs lisp mode
-;; For <=24 it's emacs-lisp-mode
+;; Note: `elisp-mode' was called `emacs-lisp-mode' in <=24
 (use-package elisp-mode
   :ensure nil
   :bind (:map emacs-lisp-mode-map
@@ -43,17 +43,19 @@
               ("C-c C-c" . eval-defun)
               ("C-c C-b" . eval-buffer)))
 
-;; Enable Eldoc in lisp modes
-;; global-eldoc-mode is enabled by default in 25.
+;; Show function arglist or variable docstring
 (use-package eldoc
   :ensure nil
   :if (<= emacs-major-version 24)
   :diminish eldoc-mode
-  :init (dolist (hook '(emacs-lisp-mode-hook
-                        lisp-interaction-mode-hook
-                        ielm-mode-hook
-                        eval-expression-minibuffer-setup-hook))
-          (add-hook hook 'eldoc-mode)))
+  :init
+  ;; Enable Eldoc in lisp modes in 24
+  ;; `global-eldoc-mode' is enabled by default in 25.
+  (dolist (hook '(emacs-lisp-mode-hook
+                  lisp-interaction-mode-hook
+                  ielm-mode-hook
+                  eval-expression-minibuffer-setup-hook))
+    (add-hook hook 'eldoc-mode)))
 
 ;; Interactive macro expander
 (use-package macrostep
@@ -73,59 +75,6 @@
                           lisp-interaction-mode-hook
                           ielm-mode-hook))
             (add-hook hook 'turn-on-elisp-slime-nav-mode))))
-
-;; Byte compiler
-(defun byte-compile-init-dir ()
-  "Byte-compile all your dotfiles."
-  (interactive)
-  (ignore-errors
-    (byte-recompile-file user-init-file 0 0)
-    (byte-recompile-directory (expand-file-name "lisp" user-emacs-directory) 0)
-    (byte-recompile-directory (expand-file-name "site-lisp" user-emacs-directory) 0)))
-
-;; (add-hook 'kill-emacs-hook 'byte-compile-init-dir)
-
-(defun recompile-el-on-save ()
-  "If you're saving an elisp file, likely the .elc is no longer valid."
-  (add-hook 'after-save-hook
-            (lambda ()
-              "Only byte-compile files in init dirs."
-              (when (string-match
-                     "\\.*\.emacs\.d\/\\(lisp\\|site-lisp\\)\/.*\\el$"
-                     buffer-file-name)
-                (byte-recompile-file buffer-file-name 0 0)))
-            nil
-            t))
-
-;; (add-hook 'emacs-lisp-mode-hook 'recompile-el-on-save)
-
-;; Only on X
-(when (display-graphic-p)
-  ;; Describe symbol at point
-  (defun my-describe-symbol-at-point (symbol)
-    "Display the full documentation of SYMBOL (function and variable) in tooltip."
-    (interactive (list (symbol-at-point)))
-    (if (null symbol)
-        (pos-tip-show
-         "** You didn't specify a symbol! **" '("red"))
-      (pos-tip-show
-       (with-temp-buffer
-         (let ((standard-output (current-buffer))
-               (help-xref-following t))
-           (help-mode)
-           (read-only-mode -1)
-           (prin1 symbol)
-           (princ " is ")
-           (save-window-excursion
-             (if (fboundp 'describe-symbol)
-                 (describe-symbol symbol)
-               (with-no-warnings
-                 (help-xref-interned symbol))))
-           (buffer-string)))
-       nil nil nil 0)))
-
-  (bind-key "<f1>" 'my-describe-symbol-at-point emacs-lisp-mode-map)
-  (bind-key "<f1>" 'my-describe-symbol-at-point lisp-interaction-mode-map))
 
 (provide 'init-emacs-lisp)
 
