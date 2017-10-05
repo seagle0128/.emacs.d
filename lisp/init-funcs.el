d2458888256401943cfb035b6a751f39af7d8d53
Vincent Zhang <seagle0128@gmail.com>
2017-10-05 23:54:20 +0800

    Improve proxy settings.

--- lisp/init-funcs.el
+++ lisp/init-funcs.el
@@ -78,18 +78,33 @@
   (set-buffer-file-coding-system 'utf-8)
   (save-buffer))
 
-;; Toggle network proxy
-(defun toggle-proxy ()
-  "Toggle network(htpp/https) proxy."
+;; Configure network proxy
+(defun show-proxy ()
+  "Show http/https proxy."
   (interactive)
   (if url-proxy-services
-      (progn
-        (setq url-proxy-services nil)
-        (message "No proxy"))
-    (progn
-      (setq url-proxy-services `(("http" . ,my-proxy)
-                                 ("https" . ,my-proxy)))
-      (message "Set proxy to %s" my-proxy))))
+      (message "Current proxy is \"%s\"" my-proxy)
+    (message "No proxy")))
+
+(defun set-proxy ()
+  "Set http/https proxy."
+  (interactive)
+  (setq url-proxy-services `(("http" . ,my-proxy)
+                             ("https" . ,my-proxy)))
+  (show-proxy))
+
+(defun unset-proxy ()
+  "Unset http/https proxy."
+  (interactive)
+  (setq url-proxy-services nil)
+  (show-proxy))
+
+(defun toggle-proxy ()
+  "Toggle http/https proxy."
+  (interactive)
+  (if url-proxy-services
+      (unset-proxy)
+    (set-proxy)))
 
 (provide 'init-funcs)
 
