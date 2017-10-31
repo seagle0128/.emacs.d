71047957312fdf50b310b4fb8d7538e84f87ec81
Vincent Zhang <seagle0128@gmail.com>
2017-10-31 12:09:02 +0800

    Improve w32 hotkeys.

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -46,10 +46,6 @@
   (setq w32-pass-lwindow-to-system nil)
   (setq w32-lwindow-modifier 'super) ; Left Windows key
 
-  (setq w32-pass-rwindow-to-system nil)
-  (setq w32-rwindow-modifier 'super) ; Right Windows key
-
-  (setq w32-pass-apps-to-system nil)
   (setq w32-apps-modifier 'hyper) ; Menu/App key
 
   (w32-register-hot-key [s-]))
