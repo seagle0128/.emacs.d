c25795a2deee368f774b5975167fec2e97f6ee0d
Vincent Zhang <seagle0128@gmail.com>
2017-11-20 18:05:43 +0800

    Don't show annotations in company-go. It's too long.

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -88,9 +88,7 @@
 
   (with-eval-after-load 'company
     (use-package company-go
-      :init
-      (setq company-go-show-annotation t)
-      (cl-pushnew (company-backend-with-yas 'company-go) company-backends)))
+      :init (cl-pushnew (company-backend-with-yas 'company-go) company-backends)))
 
   (with-eval-after-load 'projectile
     ;; M-x `go-projectile-install-tools'
