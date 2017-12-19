20e213caf3bd0a9df161f60758c14a327162a8aa
Vincent Zhang <seagle0128@gmail.com>
2017-12-19 17:13:26 +0800

    Update web mode.

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -110,7 +110,7 @@
 
 ;; Major mode for editing web templates
 (use-package web-mode
-  :mode "\\.\\(phtml\\|php|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tmpl\\)$"
+  :mode "\\.\\(phtml\\|php|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\)$"
   :config
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
