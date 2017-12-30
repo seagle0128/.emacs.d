9a0722f455d5c9c1bd341bc5f2f90c2ac4b4a8ac
Vincent Zhang <seagle0128@gmail.com>
2017-12-31 00:27:44 +0800

    Improve keybindings for `ag', `pt', `rg'.

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -95,10 +95,9 @@
 
 ;; Search utils: `ag', `rg', `pt'
 (use-package ag
-  :bind (("C-c s a" . ag))
   :init
   (with-eval-after-load 'projectile
-    (bind-key "C-c p s s" 'ag-project projectile-mode-map))
+    (bind-key "s s" 'ag-project projectile-command-map))
   :config
   (setq ag-highlight-search t)
   (setq ag-reuse-buffers t))
@@ -108,27 +107,37 @@
   (setq wgrep-auto-save-buffer t)
   (setq wgrep-change-readonly-file t))
 
+(use-package pt
+  :init
+  (with-eval-after-load 'projectile
+    (bind-key "s p" 'projectile-pt projectile-command-map)))
+
 (use-package rg
   :init
   (add-hook 'after-init-hook #'rg-enable-default-bindings)
   (if (fboundp 'wgrep-ag-setup)
       (add-hook 'rg-mode-hook #'wgrep-ag-setup))
-  (with-eval-after-load 'projectile
-    (bind-key "C-c p s r" 'rg-project projectile-mode-map))
   :config
   (setq rg-custom-type-aliases nil)
   (setq rg-group-result t)
   (setq rg-show-columns t)
+
+  (with-eval-after-load 'projectile
+    (bind-key "s r" 'rg-project projectile-command-map))
+
+  (when (fboundp 'ag)
+    (bind-key "a" 'ag rg-global-map))
+  (when (fboundp 'pt-regexp)
+    (bind-key "P" 'pt-regexp rg-global-map))
+
   (with-eval-after-load 'counsel
-    (bind-key "c" 'counsel-rg rg-global-map))
+    (bind-key "c r" 'counsel-rg rg-global-map)
+    (bind-key "c s" 'counsel-ag rg-global-map)
+    (bind-key "c p" 'counsel-pt rg-global-map)
+    (bind-key "c f" 'counsel-fzf rg-global-map))
+
   (with-eval-after-load 'counsel-projectile
     (bind-key "s r" 'rg-project counsel-projectile-command-map)))
-
-(use-package pt
-  :bind (("C-c s P" . pt-regexp))
-  :init
-  (with-eval-after-load 'projectile
-    (bind-key "C-c p s p" 'projectile-pt projectile-mode-map)))
 
 ;; Tramp
 (use-package docker-tramp)
