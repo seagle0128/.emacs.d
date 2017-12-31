1120115f68cf06d291bef1bb0611c89bf71a0ffa
Vincent Zhang <seagle0128@gmail.com>
2017-12-31 17:44:07 +0800

    Minor fix.

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -75,8 +75,7 @@
 
   ;; don't use default value but manage it ourselves
   (setq popwin:special-display-config
-        '(
-          ;; basic
+        '(;; Emacs
           ("*Help*" :dedicated t :position bottom :stick t :noselect nil :height 0.4)
           ("*compilation*" :dedicated t :position bottom :stick t :noselect t :height 0.4)
           ("*Compile-Log*" :dedicated t :position bottom :stick t :noselect t :height 0.4)
@@ -87,31 +86,29 @@
           ("^*Man.+*$" :regexp t :position bottom :stick t :noselect nil :height 0.4)
           ("^*WoMan.+*$" :regexp t :position bottom)
           ("^*Backtrace.+*$" :regexp t :dedicated t :position bottom :stick t :noselect nil)
-          ("*Colors*" :dedicated t :position bottom)
+
+          ;; Kill Ring
           ("*Kill Ring*" :dedicated t :position bottom)
 
-          ;; flycheck
+          ;; Flycheck
           ("\*flycheck errors\*.+*$" :regexp t :position bottom :stick t :noselect nil)
 
-          ;; magit
-          ;; (magit-status-mode :dedicated t :position bottom :stick t :height 0.5)
-          ;; (magit-diff-mode :dedicated t :position bottom :stick t :noselect t :height 0.5)
-
-          ;; youdao dict
+          ;; Youdao dict
           ("*Youdao Dictionary*" :dedicated t :position bottom)
 
-          ;; paradox
+          ;; Paradox
           ("*Paradox Report*" :dedicated t :position bottom :noselect t)
 
-          ;; process
+          ;; List
+          ("*Colors*" :dedicated t :position bottom)
           ("*Process List*" :dedicated t :position bottom)
-          ("*Process-Enviornment*" :dedicated t :position bottom)
+          ("*Process-Environment*" :dedicated t :position bottom)
 
           ;; undo-tree
           (" *undo-tree*" :dedicated t :position right :stick t :noselect nil :width 60)
           ("*undo-tree Diff*" :dedicated t :postion bottom :stick t :noselect nil :height 0.3)
 
-          ;; search
+          ;; Search
           ("*grep*" :dedicated t :position bottom :stick t :noselect nil)
           ("*ag search*" :dedicated t :position bottom :stick t :noselect nil :height 0.4)
           ("*rg*" :dedicated t :position bottom :stick t :noselect nil :height 0.4)
@@ -120,17 +117,21 @@
           ("\*ivy-occur.+*$" :regexp t :position bottom :stick t :noselect nil)
           ("*xref*" :dedicated t :position bottom :stick nil :noselect nil)
 
-          ;; vc
+          ;; VC
           ("*vc-diff*" :dedicated t :position bottom :stick t :noselect nil)
           ("*vc-change-log*" :dedicated t :position bottom :stick t :noselect nil)
 
-          ;; scripts
+          ;; Magit
+          ;; (magit-status-mode :dedicated t :position bottom :stick t :height 0.5)
+          ;; (magit-diff-mode :dedicated t :position bottom :stick t :noselect t :height 0.5)
+
+          ;; Script
           ("*shell*" :dedicated t :position bottom :stick t :noselect nil :height 0.3)
           ("*Python*" :dedicated t :position bottom :stick t :noselect t :height 0.3)
           ("*Ruby*" :dedicated t :position bottom :stick t :noselect t :height 0.3)
           ("*quickrun*" :dedicated t :position bottom :stick t :noselect t :height 0.4)
 
-          ;; go
+          ;; Go
           ("^*godoc.+*$" :regexp t :position bottom :stick nil :noselect nil)
           ("*golint*" :dedicated t :position bottom :stick t :noselect nil)
           ("*govet*" :dedicated t :position bottom :stick t :noselect nil)
@@ -138,7 +139,7 @@
           ("*Gofmt Errors*" :dedicated t :position bottom :stick t :noselect nil)
           ("*Go Test*" :dedicated t :position bottom :stick t :noselect nil)
 
-          ;; test
+          ;; Test
           ("*ert*" :dedicated t :position bottom :stick t :noselect nil)
           ("*nosetests*" :dedicated t :position bottom :stick t :noselect nil))))
 
