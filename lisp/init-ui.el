5e06237f2cdcc0f0fe7f035d0c5fa786064d17aa
Vincent Zhang <seagle0128@gmail.com>
2018-01-06 22:24:42 +0800

    Optimize: Using cache in `cnfonts`.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -89,6 +89,7 @@
   (add-hook 'after-init-hook #'cnfonts-enable)
   :config
   (setq cnfonts-keep-frame-size nil)
+  (setq cnfonts-use-cache t)
   (setq cnfonts-profiles
         '("program1" "program2" "program3" "org-mode" "read-book"))
   (setq cnfonts--profiles-steps '(("program1" . 4)
