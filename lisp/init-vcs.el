52d7add4e044fd9549dccf51d44184d1d63476c2
Vincent Zhang <seagle0128@gmail.com>
2018-03-15 17:11:19 +0800

    Update keybindings for vcs.

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -66,22 +66,25 @@
 ;;; Pop up last commit information of current line
 (use-package git-messenger
   :commands git-messenger:copy-message
-  :bind (("C-x v p" . git-messenger:popup-message)
-         :map git-messenger-map
-         ("m" . git-messenger:copy-message))
+  :bind (:map vc-prefix-map
+              ("p" . git-messenger:popup-message)
+              :map git-messenger-map
+              ("m" . git-messenger:copy-message))
   :init
   ;; Use magit-show-commit for showing status/diff commands
   (setq git-messenger:use-magit-popup t))
 
 ;; Walk through git revisions of a file
 (use-package git-timemachine
-  :bind (("C-x v t" . git-timemachine)))
+  :bind (:map vc-prefix-map
+              ("t" . git-timemachine)))
 
 ;; Highlighting regions by last updated time
 (use-package smeargle
-  :bind (("C-x v S" . smeargle)
-         ("C-x v C" . smeargle-commits)
-         ("C-x v R" . smeargle-clear)))
+  :bind (:map vc-prefix-map
+              ("S" . smeargle)
+              ("C" . smeargle-commits)
+              ("R" . smeargle-clear)))
 
 ;; Git modes
 (use-package gitattributes-mode)
