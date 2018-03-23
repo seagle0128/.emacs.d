94d7489701596be4fb8d050a63698972156b3f75
Vincent Zhang <seagle0128@gmail.com>
2018-03-24 01:15:05 +0800

    Minor impovements of company.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -48,8 +48,8 @@
   ;; aligns annotation to the right hand side
   (setq company-tooltip-align-annotations t)
 
-  (setq company-tooltip-limit 15                      ; bigger popup window
-        company-idle-delay .3                         ; decrease delay before autocompletion popup shows
+  (setq company-tooltip-limit 12                      ; bigger popup window
+        company-idle-delay .2                         ; decrease delay before autocompletion popup shows
         company-echo-delay 0                          ; remove annoying blinking
         company-minimum-prefix-length 2
         company-require-match nil
