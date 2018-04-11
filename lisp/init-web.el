c986bac9734435e731ab46d48053b389b69e9ec6
Vincent Zhang <seagle0128@gmail.com>
2018-04-12 00:24:16 +0800

    Enhance `js2-mode`.

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -59,12 +59,19 @@
 
 ;; Improved JavaScript editing mode
 (use-package js2-mode
-  :mode "\\.js$"
-  :interpreter "node"
+  :mode (("\\.js\\" . js2-mode)
+         ("\\.jsx?\\" . js2-jsx-mode))
+  :interpreter (("node" . js2-mode)
+                ("node" . js2-jsx-mode))
   :init
   (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
   (add-hook 'js2-mode-hook #'js2-highlight-unused-variables-mode)
   :config
+  (with-eval-after-load 'flycheck
+    (if (or (executable-find "jshint")
+            (executable-find "jslint"))
+        (setq js2-mode-show-strict-warnings nil)))
+
   (use-package js2-refactor
     :diminish js2-refactor-mode
     :init (add-hook 'js2-mode-hook #'js2-refactor-mode)
