fc661612313d6825c08bfdd18f6f926d2298b0a3
Vincent Zhang <seagle0128@gmail.com>
2018-04-13 00:28:33 +0800

    Revert flycheck to '(save idle-change new-line mode-enabled).

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -38,7 +38,7 @@
   :config
   (setq flycheck-indication-mode 'right-fringe)
   (setq flycheck-emacs-lisp-load-path 'inherit)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  ;; (setq flycheck-check-syntax-automatically '(save mode-enabled))
 
   ;; Display Flycheck errors in GUI tooltips
   (use-package flycheck-pos-tip
