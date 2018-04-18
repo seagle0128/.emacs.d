12987d04df66ffe8868c5cbd31620a0ee59f2339
Vincent Zhang <seagle0128@gmail.com>
2018-04-19 00:13:24 +0800

    Improve `symbol-overlay`.

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -43,6 +43,9 @@
   :bind (("M-i" . symbol-overlay-put)
          ("M-n" . symbol-overlay-jump-next)
          ("M-p" . symbol-overlay-jump-prev)
+         ("M-N" . symbol-overlay-switch-forward)
+         ("M-P" . symbol-overlay-switch-backward)
+         ("M-C" . symbol-overlay-remove-all)
          ([M-f3] . symbol-overlay-remove-all))
   :init (add-hook 'prog-mode-hook #'symbol-overlay-mode))
 
