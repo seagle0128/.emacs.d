25a95ba23c712877b5c5d06bcfef80c3af444ff2
Vincent Zhang <seagle0128@gmail.com>
2018-06-03 21:29:12 +0800

    Sort completion candidates via prescient.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -64,6 +64,12 @@
     :init (company-quickhelp-mode 1)
     :config (setq company-quickhelp-delay 0.8))
 
+  ;; Sort completion candidates
+  (use-package company-prescient
+    :init
+    (company-prescient-mode 1)
+    (prescient-persist-mode 1))
+
   ;; Support yas in commpany
   ;; Note: Must be the last to involve all backends
   (defvar company-enable-yas t
