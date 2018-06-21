7ad308dfcb7929bc5ad909730143bae675eda478
Vincent Zhang <seagle0128@gmail.com>
2018-06-21 19:17:31 +0800

    Disable helpful by default. It's usefule but very slow.

--- lisp/init-emacs-lisp.el
+++ lisp/init-emacs-lisp.el
@@ -58,9 +58,9 @@
 
 ;; A better *Help* buffer
 (use-package helpful
-  :bind (("C-h f" . helpful-callable)
-         ("C-h v" . helpful-variable)
-         ("C-h k" . helpful-key)
+  :bind (;; ("C-h f" . helpful-callable)
+         ;; ("C-h v" . helpful-variable)
+         ;; ("C-h k" . helpful-key)
          ("C-h C-d" . helpful-at-point)
          ("C-h F" . helpful-function)
          ("C-h M" . helpful-macro)
