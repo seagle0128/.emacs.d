f2f5091e02c14678c1469c6c10e3099fab02707e
Vincent Zhang <seagle0128@gmail.com>
2018-06-28 19:44:44 +0800

    Refactor init-org.el.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -38,10 +38,8 @@
          ("C-c B" . org-switchb)
          :map org-mode-map
          ("C-c l" . org-store-link))
-  :init
-  (add-hook 'org-mode-hook (lambda ()
-                             (org-indent-mode 1)
-                             (diminish 'org-indent-mode)))
+  :hook ((org-mode . org-indent-mode)
+         (org-indent-mode . (lambda() (diminish 'org-indent-mode))))
   :config
   (setq org-agenda-files '("~/org"))
   (setq org-todo-keywords
@@ -56,7 +54,7 @@
 
   (unless sys/win32p
     (use-package org-fancy-priorities
-      :unless sys/win32p
+      :defines org-fancy-priorities-list
       :hook (org-mode . org-fancy-priorities-mode)
       :config (setq org-fancy-priorities-list '("⚡" "⬆" "⬇" "☕"))))
 
@@ -90,6 +88,7 @@
 
   ;; Presentation
   (use-package org-tree-slide
+    :functions (org-display-inline-images org-remove-inline-images)
     :config
     (add-hook 'org-tree-slide-play-hook
               (lambda ()
@@ -110,9 +109,21 @@
   ;; Visually summarize progress
   (use-package org-dashboard)
 
-  (with-eval-after-load 'hydra
-    (defhydra hydra-org-template (:color blue :hint nil)
-      "
+  (eval-when-compile
+    (with-eval-after-load 'hydra
+      (defun hot-expand (str &optional mod)
+        "Expand org template."
+        (let (text)
+          (when (region-active-p)
+            (setq text (buffer-substring (region-beginning) (region-end)))
+            (delete-region (region-beginning) (region-end)))
+          (insert str)
+          (org-try-structure-completion)
+          (when mod (insert mod) (forward-line))
+          (when text (insert text))))
+
+      (defhydra hydra-org-template (:color blue :hint nil)
+        "
 _c_enter  qu_o_te     _e_macs-lisp    _L_aTeX:
 _l_atex   _E_xample   p_y_thon        _i_ndex:
 _a_scii   _v_erse     ip_Y_thon       _I_NCLUDE:
@@ -120,50 +131,39 @@ _s_rc     _g_o        _r_uby          _H_TML:
 _h_tml    _S_HELL     _p_erl          _A_SCII:
 ^ ^       ^ ^         _P_erl tangled  plant_u_ml
 "
-      ("s" (hot-expand "<s"))
-      ("E" (hot-expand "<e"))
-      ("o" (hot-expand "<q"))
-      ("v" (hot-expand "<v"))
-      ("c" (hot-expand "<c"))
-      ("l" (hot-expand "<l"))
-      ("h" (hot-expand "<h"))
-      ("a" (hot-expand "<a"))
-      ("L" (hot-expand "<L"))
-      ("i" (hot-expand "<i"))
-      ("e" (hot-expand "<s" "emacs-lisp"))
-      ("y" (hot-expand "<s" "python :results output"))
-      ("Y" (hot-expand "<s" "ipython :session :exports both :results raw drawer\n$0"))
-      ("g" (hot-expand "<s" "go :imports '\(\"fmt\"\)"))
-      ("p" (hot-expand "<s" "perl"))
-      ("r" (hot-expand "<s" "ruby"))
-      ("S" (hot-expand "<s" "sh"))
-      ("u" (hot-expand "<s" "plantuml :file CHANGE.png"))
-      ("P" (progn
-             (insert "#+HEADERS: :results output :exports both :shebang \"#!/usr/bin/env perl\"\n")
-             (hot-expand "<s" "perl")))
-      ("I" (hot-expand "<I"))
-      ("H" (hot-expand "<H"))
-      ("A" (hot-expand "<A"))
-      ("<" self-insert-command "ins")
-      ("q" nil "quit"))
+        ("s" (hot-expand "<s"))
+        ("E" (hot-expand "<e"))
+        ("o" (hot-expand "<q"))
+        ("v" (hot-expand "<v"))
+        ("c" (hot-expand "<c"))
+        ("l" (hot-expand "<l"))
+        ("h" (hot-expand "<h"))
+        ("a" (hot-expand "<a"))
+        ("L" (hot-expand "<L"))
+        ("i" (hot-expand "<i"))
+        ("e" (hot-expand "<s" "emacs-lisp"))
+        ("y" (hot-expand "<s" "python :results output"))
+        ("Y" (hot-expand "<s" "ipython :session :exports both :results raw drawer\n$0"))
+        ("g" (hot-expand "<s" "go :imports '\(\"fmt\"\)"))
+        ("p" (hot-expand "<s" "perl"))
+        ("r" (hot-expand "<s" "ruby"))
+        ("S" (hot-expand "<s" "sh"))
+        ("u" (hot-expand "<s" "plantuml :file CHANGE.png"))
+        ("P" (progn
+               (insert "#+HEADERS: :results output :exports both :shebang \"#!/usr/bin/env perl\"\n")
+               (hot-expand "<s" "perl")))
+        ("I" (hot-expand "<I"))
+        ("H" (hot-expand "<H"))
+        ("A" (hot-expand "<A"))
+        ("<" self-insert-command "ins")
+        ("q" nil "quit"))
 
-    (defun hot-expand (str &optional mod)
-      "Expand org template."
-      (let (text)
-        (when (region-active-p)
-          (setq text (buffer-substring (region-beginning) (region-end)))
-          (delete-region (region-beginning) (region-end)))
-        (insert str)
-        (org-try-structure-completion)
-        (when mod (insert mod) (forward-line))
-        (when text (insert text))))
-
-    (bind-key "<"
-              (lambda () (interactive)
-                (if (or (region-active-p) (looking-back "^" 1))
-                    (hydra-org-template/body)
-                  (self-insert-command 1)))
-              org-mode-map)))
+      (bind-key "<"
+                (lambda () (interactive)
+                  (if (or (region-active-p) (looking-back "^" 1))
+                      (hydra-org-template/body)
+                    (self-insert-command 1)))
+                org-mode-map))))
 
 (provide 'init-org)
 
