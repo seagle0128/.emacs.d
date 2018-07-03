5ae9aabf63195a3baf6d6b6e3003c53afb987ebe
Vincent Zhang <seagle0128@gmail.com>
2018-07-04 01:02:00 +0800

    Fix flycheck.

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -43,14 +43,14 @@
   ;; Display Flycheck errors in GUI tooltips
   (if (display-graphic-p)
       (use-package flycheck-pos-tip
-        :hook (after-init . flycheck-pos-tip-mode)
+        :init (flycheck-pos-tip-mode)
         :config (setq flycheck-pos-tip-timeout 30))
     (use-package flycheck-popup-tip
-      :hook (after-init . flycheck-popup-tip-mode)))
+      :init (flycheck-popup-tip-mode)))
 
   ;; Jump to and fix syntax errors via `avy'
   (use-package avy-flycheck
-    :hook (after-init . avy-flycheck-setup)))
+    :init (avy-flycheck-setup)))
 
 (provide 'init-flycheck)
 
