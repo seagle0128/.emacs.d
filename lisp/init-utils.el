c4f2ed5089e484d7fc21130f41afa44e5753f251
Vincent Zhang <seagle0128@gmail.com>
2018-07-12 00:12:58 +0800

    Improve treemacs: Single click to expand; Enable fringe indicator.

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -42,10 +42,24 @@
 
 ;; A tree layout file explorer
 (use-package treemacs
-  :commands (treemacs-follow-mode treemacs-filewatch-mode treemacs-git-mode)
+  :defines winum-keymap
+  :commands (treemacs-follow-mode
+             treemacs-filewatch-mode
+             treemacs-fringe-indicator-mode
+             treemacs-git-mode)
   :bind (([f8]        . treemacs)
          ("M-9"       . treemacs-select-window)
-         ("C-x 1"     . treemacs-delete-other-windows))
+         ("C-x 1"     . treemacs-delete-other-windows)
+         ("C-x t 1"   . treemacs-delete-other-windows)
+         ("C-x t t"   . treemacs)
+         ("C-x t B"   . treemacs-bookmark)
+         ("C-x t C-t" . treemacs-find-file)
+         ("C-x t M-t" . treemacs-find-tag)
+         :map treemacs-mode-map
+         ([mouse-1]   . treemacs-single-click-expand-action))
+  :init
+  (with-eval-after-load 'winum
+    (bind-key (kbd "M-9") #'treemacs-select-window winum-keymap))
   :config
   (setq treemacs-collapse-dirs              (if (executable-find "python") 3 0)
         treemacs-file-event-delay           5000
@@ -68,6 +82,7 @@
 
   (treemacs-follow-mode t)
   (treemacs-filewatch-mode t)
+  (treemacs-fringe-indicator-mode t)
   (pcase (cons (not (null (executable-find "git")))
                (not (null (executable-find "python3"))))
     (`(t . t)
