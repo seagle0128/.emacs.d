08914daf21d3843c37d7c4b672e6e68d44c86fe7
Vincent Zhang <seagle0128@gmail.com>
2018-07-16 13:53:04 +0800

    Fix lsp issue while first loading.

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -46,19 +46,19 @@
     (message "LSP server restarted."))
 
   (require 'lsp-imenu)
-  (add-hook 'lsp-after-open-hook 'lsp-enable-imenu)
+  (add-hook 'lsp-after-open-hook 'lsp-enable-imenu))
 
-  (use-package lsp-ui
-    :bind (:map lsp-ui-mode-map
-                ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
-                ([remap xref-find-references] . lsp-ui-peek-find-references))
-    :hook (lsp-mode . lsp-ui-mode))
+(use-package lsp-ui
+  :bind (:map lsp-ui-mode-map
+              ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+              ([remap xref-find-references] . lsp-ui-peek-find-references))
+  :hook (lsp-mode . lsp-ui-mode))
 
-  (use-package company-lsp
-    :after company
-    :defines company-backends
-    :functions company-backend-with-yas
-    :init (cl-pushnew (company-backend-with-yas 'company-lsp) company-backends)))
+(use-package company-lsp
+  :after company
+  :defines company-backends
+  :functions company-backend-with-yas
+  :init (cl-pushnew (company-backend-with-yas 'company-lsp) company-backends))
 
 ;; Go support for lsp-mode using Sourcegraph's Go Language Server
 ;; Install: go get github.com/sourcegraph/go-langserver
