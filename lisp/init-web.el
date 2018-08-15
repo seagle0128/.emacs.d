563cd8bc015e6ea23ae6a48f398389e77852fe05
Vincent Zhang <seagle0128@gmail.com>
2018-08-15 22:08:43 +0800

    Fix warnings.

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -92,7 +92,8 @@
 (unless centaur-lsp
   (use-package tide
     :diminish tide-mode
-    :defines company-backends
+    :defines (company-backends tide-format-options)
+    :functions (tide-setup tide-hl-identifier-mode)
     :preface
     (defun setup-tide-mode ()
       "Setup tide mode."
