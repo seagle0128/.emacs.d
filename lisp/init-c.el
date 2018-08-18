c94203ab07e43c0abccc2b5919182aa8592e8133
Vincent Zhang <seagle0128@gmail.com>
2018-08-18 14:44:53 +0800

    Improve irony mode.

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -43,28 +43,55 @@
                            (setq tab-width 4)
                            (setq c-basic-offset 4)))
   :config
-  (unless 'centaur-lsp
-    (use-package irony-mode
+  (unless centaur-lsp
+    (use-package irony
+      :defines (irony-mode-map irony-server-w32-pipe-buffer-size)
       :hook (((c-mode c++-mode objc-mode) . irony-mode)
              (irony-mode . irony-cdb-autosetup-compile-options))
       :config
-      (with-eval-after-load 'counsel
-        (bind-keys :map irony-mode-map
-                   ([remap completion-at-point] . counsel-irony)
-                   ([remap complete-symbol] . counsel-irony)))
-
       ;; Windows performance tweaks
       (when (boundp 'w32-pipe-read-delay)
         (setq w32-pipe-read-delay 0))
       ;; Set the buffer size to 64K on Windows (from the original 4K)
       (when (boundp 'w32-pipe-buffer-size)
-        (setq irony-server-w32-pipe-buffer-size (* 64 1024))))
+        (setq irony-server-w32-pipe-buffer-size (* 64 1024)))
+
+      (with-eval-after-load 'counsel
+        (bind-keys :map irony-mode-map
+                   ([remap completion-at-point] . counsel-irony)
+                   ([remap complete-symbol] . counsel-irony)))
+
+      (use-package irony-eldoc
+        :hook (irony-mode . irony-eldoc))
+
+      (with-eval-after-load 'company
+        (use-package company-irony
+          :init (cl-pushnew (company-backend-with-yas 'company-irony) company-backends))
+        (use-package company-irony-c-headers
+          :init (cl-pushnew (company-backend-with-yas 'company-irony-c-headers) company-backends)))
+
+      (with-eval-after-load 'flycheck
+        (use-package flycheck-irony
+          :hook (flycheck-mode . flycheck-irony-setup))))
+
+    ;; Ivy for GNU global
+    (with-eval-after-load 'counsel
+      (use-package counsel-gtags
+        :defines (counsel-gtags-mode-map counsel-gtags-auto-update)
+        :diminish counsel-gtags-mode
+        :bind (:map counsel-gtags-mode-map
+                    ("M-." . counsel-gtags-find-definition)
+                    ("M-r" . counsel-gtags-find-reference)
+                    ("M-s" . counsel-gtags-find-symbol)
+                    ("M-," . counsel-gtags-go-backward))
+        :hook ((c-mode c++-mode objc-mode) . counsel-gtags-mode)
+        :config (setq counsel-gtags-auto-update t)))
 
     ;; Company mode backend for C/C++ header files
-    (use-package company-c-headers
-      :after company
-      :functions company-backend-with-yas
-      :init (cl-pushnew (company-backend-with-yas 'company-c-headers) company-backends))))
+    (with-eval-after-load 'company
+      (use-package company-c-headers
+        :functions company-backend-with-yas
+        :init (cl-pushnew (company-backend-with-yas 'company-c-headers) company-backends)))))
 
 (provide 'init-c)
 
