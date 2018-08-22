24ebba82ebfbf07dcf82981c23fb9a91be977faf
Vincent Zhang <seagle0128@gmail.com>
2018-08-22 15:36:31 +0800

    Minor fix.

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -35,14 +35,14 @@
 
 ;; FIXME: DO NOT copy package-selected-packages to init/custom file forcibly.
 ;; https://github.com/jwiegley/use-package/issues/383#issuecomment-247801751
-(defun save-selected-packages (&optional value)
+(defun my-save-selected-packages (&optional value)
   "Set and (don't!) save `package-selected-packages' to VALUE."
   (when value
     (setq package-selected-packages value))
   (unless after-init-time
     (add-hook 'after-init-hook #'package--save-selected-packages)))
 
-(advice-add 'package--save-selected-packages :override #'save-selected-packages)
+(advice-add 'package--save-selected-packages :override #'my-save-selected-packages)
 
 ;;
 ;; ELPA: refer to https://github.com/melpa/melpa and https://elpa.emacs-china.org/.
