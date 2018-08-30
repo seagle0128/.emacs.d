cdc7137acfb9202f9a7ed00e2221664be66d37b4
Vincent Zhang <seagle0128@gmail.com>
2018-08-31 00:05:36 +0800

    Fix: Unable to load magit-svn at the installation.

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -40,29 +40,29 @@
          ("C-c M-g" . magit-file-popup))
   :config
   (when sys/win32p
-    (setenv "GIT_ASKPASS" "git-gui--askpass"))
+    (setenv "GIT_ASKPASS" "git-gui--askpass")))
 
-  ;; Github integration
-  (use-package magithub
-    :init
-    (setq magithub-api-timeout 5)
-    (magithub-feature-autoinject t))
+;; Github integration
+(use-package magithub
+  :init
+  (setq magithub-api-timeout 5)
+  (magithub-feature-autoinject t))
 
-  ;; Gitflow externsion for Magit
-  (use-package magit-gitflow
-    :diminish magit-gitflow-mode
-    :functions magit-define-popup-action
-    :bind (:map magit-status-mode-map
-                ("G" . magit-gitflow-popup))
-    :hook (magit-mode . turn-on-magit-gitflow)
-    :config
-    (magit-define-popup-action 'magit-dispatch-popup
-      ?G "GitFlow" #'magit-gitflow-popup ?!))
+;; Gitflow externsion for Magit
+(use-package magit-gitflow
+  :diminish magit-gitflow-mode
+  :functions magit-define-popup-action
+  :bind (:map magit-status-mode-map
+              ("G" . magit-gitflow-popup))
+  :hook (magit-mode . turn-on-magit-gitflow)
+  :config
+  (magit-define-popup-action 'magit-dispatch-popup
+    ?G "GitFlow" #'magit-gitflow-popup ?!))
 
-  ;; Git-Svn extension for Magit
-  (use-package magit-svn
-    :diminish magit-svn-mode
-    :hook (magit-mode . magit-svn-mode)))
+;; Git-Svn extension for Magit
+(use-package magit-svn
+  :diminish magit-svn-mode
+  :hook (magit-mode . magit-svn-mode))
 
 ;;; Pop up last commit information of current line
 (use-package git-messenger
