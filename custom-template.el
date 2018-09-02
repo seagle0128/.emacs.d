1d2c0a199ffe318434a6799bf4ffd3c8f22d309a
Vincent Zhang <seagle0128@gmail.com>
2018-09-03 01:46:48 +0800

    Update custom template.

--- custom-template.el
+++ custom-template.el
@@ -1,7 +1,7 @@
 ;;; custom.el --- user customization file    -*- no-byte-compile: t -*-
 ;;; Commentary:
-;;;       Copy custom-template.el to custom.el and change the configurations,
-;;;       then restart Emacs.
+;;;       Copy custom-template.el to custom.el and change the configurations, then restart Emacs.
+;;;       Put your own configurations in custom-post.el to override default configurations.
 ;;; Code:
 
 ;; (setq centaur-logo nil)                        ; Logo file or nil (official logo)
@@ -28,6 +28,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
