2bced3cdb16c12e39d50b877ed225b4a3f96056c
Vincent Zhang <seagle0128@gmail.com>
2018-09-19 18:25:23 +0800

    Remove: markdown-preview-mode. It doesn't work well.

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -66,41 +66,7 @@
       (add-hook 'markdown-mode-hook #'set-markdownlint-config)))
 
   ;; Preview
-  (setq markdown-css-paths '("http://thomasf.github.io/solarized-css/solarized-light.min.css"))
-
-  (use-package markdown-preview-mode
-    :bind (:map markdown-mode-command-map
-                ("P" . markdown-preview-mode))
-    :config
-    (setq markdown-preview-stylesheets
-          (list "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.9.0/github-markdown.min.css"
-                "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css" "
-  <style>
-   .markdown-body {
-     box-sizing: border-box;
-     min-width: 200px;
-     max-width: 980px;
-     margin: 0 auto;
-     padding: 45px;
-   }
-
-   @media (max-width: 767px) {
-     .markdown-body {
-       padding: 15px;
-     }
-   }
-  </style>
-"))
-    (setq markdown-preview-javascript
-          (list "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js" "
-  <script>
-   $(document).on('mdContentChange', function() {
-     $('pre code').each(function(i, block) {
-       hljs.highlightBlock(block);
-     });
-   });
-  </script>
-"))))
+  (setq markdown-css-paths '("http://thomasf.github.io/solarized-css/solarized-light.min.css")))
 
 (provide 'init-markdown)
 
