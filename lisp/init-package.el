515b34d722ec827dfa3439650af41bf0e77948c8
Vincent Zhang <seagle0128@gmail.com>
2018-11-19 15:12:36 +0800

    Refactor my-save-selected-packages.

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -36,11 +36,9 @@
 ;; HACK: DO NOT copy package-selected-packages to init/custom file forcibly.
 ;; https://github.com/jwiegley/use-package/issues/383#issuecomment-247801751
 (defun my-save-selected-packages (&optional value)
-  "Set and (don't!) save `package-selected-packages' to VALUE."
+  "Set `package-selected-packages' to VALUE but don't save to `custom-file'."
   (when value
-    (setq package-selected-packages value))
-  (unless after-init-time
-    (add-hook 'after-init-hook #'package--save-selected-packages)))
+    (setq package-selected-packages value)))
 (advice-add 'package--save-selected-packages :override #'my-save-selected-packages)
 
 ;;
