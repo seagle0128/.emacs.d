7428d4ef1b97a26ebb303faa0c3445b16285a8cf
Vincent Zhang <seagle0128@gmail.com>
2018-11-29 16:12:24 +0800

    Update recentf exclude list.

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -80,6 +80,7 @@
   (add-to-list 'recentf-exclude (expand-file-name package-user-dir))
   (add-to-list 'recentf-exclude ".cache")
   (add-to-list 'recentf-exclude ".cask")
+  (add-to-list 'recentf-exclude ".elfeed")
   (add-to-list 'recentf-exclude "bookmarks")
   (add-to-list 'recentf-exclude "recentf")
   (add-to-list 'recentf-exclude "COMMIT_EDITMSG\\'"))
