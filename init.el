0cc6d6b64efd50522954174e7e6f8aea1c479f21
Vincent Zhang <seagle0128@gmail.com>
2018-12-17 00:51:57 +0800

    Fix #42: Add subdirectories of site-lisp to load-path.

--- init.el
+++ init.el
@@ -52,7 +52,15 @@
   "Update `load-path'."
   (push (expand-file-name "site-lisp" user-emacs-directory) load-path)
   (push (expand-file-name "lisp" user-emacs-directory) load-path))
+
+(defun add-subdirs-to-load-path (&rest _)
+  "Add subdirectories to `load-path'."
+  (let ((default-directory
+          (expand-file-name "site-lisp" user-emacs-directory)))
+    (normal-top-level-add-subdirs-to-load-path)))
+
 (advice-add #'package-initialize :after #'update-load-path)
+(advice-add #'package-initialize :after #'add-subdirs-to-load-path)
 
 (update-load-path)
 
