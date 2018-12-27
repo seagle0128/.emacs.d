ee0b9186aa526c0cf99570422847b4463d8059a2
Vincent Zhang <seagle0128@gmail.com>
2018-12-28 01:46:54 +0800

    Improve frame/window hydra.

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -57,97 +57,120 @@
   :custom-face
   (aw-leading-char-face ((t (:inherit 'error :bold t :height 1.2))))
   (aw-mode-line-face ((t (:inherit 'mode-line-emphasis :bold t))))
+  :preface
+  (defun toggle-window-split ()
+    (interactive)
+    (if (= (count-windows) 2)
+        (let* ((this-win-buffer (window-buffer))
+               (next-win-buffer (window-buffer (next-window)))
+               (this-win-edges (window-edges (selected-window)))
+               (next-win-edges (window-edges (next-window)))
+               (this-win-2nd (not (and (<= (car this-win-edges)
+                                          (car next-win-edges))
+                                       (<= (cadr this-win-edges)
+                                          (cadr next-win-edges)))))
+               (splitter
+                (if (= (car this-win-edges)
+                       (car (window-edges (next-window))))
+                    'split-window-horizontally
+                  'split-window-vertically)))
+          (delete-other-windows)
+          (let ((first-win (selected-window)))
+            (funcall splitter)
+            (if this-win-2nd (other-window 1))
+            (set-window-buffer (selected-window) this-win-buffer)
+            (set-window-buffer (next-window) next-win-buffer)
+            (select-window first-win)
+            (if this-win-2nd (other-window 1))))))
   :hook (after-init . ace-window-display-mode)
   :config
   ;; (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 
-  (when (package-installed-p 'hydra)
-    ;; https://github.com/abo-abo/ace-window/wiki/Hydra
-    ;; hydra-frame-window is designed from ace-window (C-x o w) and
-    ;; matches aw-dispatch-alist with a few extra
-    (defhydra hydra-frame-window (:color red :hint nil)
-      "
-^Delete^                       ^Frame resize^             ^Window^                Window Size^^^^^^   ^Text^                         (__)
-_0_: delete-frame              _g_: resize-frame-right    _t_: toggle               ^ ^ _k_ ^ ^        _K_                           (oo)
-_1_: delete-other-frames       _H_: resize-frame-left     _e_: ace-swap-win         _h_ ^+^ _l_        ^+^                     /------\\/
-_2_: make-frame                _F_: fullscreen            ^ ^                       ^ ^ _j_ ^ ^        _J_                    / |    ||
-_d_: kill-and-delete-frame     _n_: make-frame            _w_: ace-delete-window    _b_alance^^^^      ^ ^                 *  /\\---/\\  ~~  C-x o w ;
+  (eval-when-compile
+    (with-eval-after-load 'hydra
+      ;; https://github.com/abo-abo/ace-window/wiki/Hydra
+      ;; hydra-frame-window is designed from `ace-window' and
+      ;; matches aw-dispatch-alist with a few extra
+      (defhydra hydra-frame-window (:color red :hint t)
+        "
+^Frame^                         ^Window^         Window Size^^^^^^  ^Text^                 (__)
+_0_: delete-frame               _t_: toggle        ^ ^ _k_ ^ ^        _K_                   (oo)
+_1_: delete-other-frames        _e_: swap          _h_ ^+^ _l_        ^+^             /------\\/
+_2_: make-frame                 _w_: delete        ^ ^ _j_ ^ ^        _J_            / |    ||
+_F_: fullscreen                 ^ ^                _b_alance^^^^      ^ ^        *  /\\---/\\  ~~  C-c w/C-x o w
 "
-      ("0" delete-frame :exit t)
-      ("1" delete-other-frames :exit t)
-      ("2" make-frame  :exit t)
-      ("b" balance-windows)
-      ("d" kill-and-delete-frame :exit t)
-      ("e" ace-swap-window)
-      ("F" toggle-frame-fullscreen)   ;; is <f11>
-      ("g" resize-frame-right :exit t)
-      ("H" resize-frame-left :exit t)  ;; aw-dispatch-alist uses h, I rebind here so hjkl can be used for size
-      ("n" make-frame :exit t)
-      ;; ("r" reverse-windows)
-      ("t" toggle-window-spilt)
-      ("w" ace-delete-window :exit t)
-      ("x" delete-frame :exit t)
-      ("K" text-scale-decrease)
-      ("J" text-scale-increase)
-      ("h" shrink-window-horizontally)
-      ("k" shrink-window)
-      ("j" enlarge-window)
-      ("l" enlarge-window-horizontally)
-      ("q" nil "quit"))
-    (add-to-list 'aw-dispatch-alist '(?w hydra-frame-window/body) t)))
+        ("0" delete-frame :exit t)
+        ("1" delete-other-frames :exit t)
+        ("2" make-frame  :exit t)
+        ("b" balance-windows)
+        ("e" ace-swap-window)
+        ("F" toggle-frame-fullscreen)
+        ("t" toggle-window-split)
+        ("w" ace-delete-window :exit t)
+        ("x" delete-frame :exit t)
+        ("K" text-scale-decrease)
+        ("J" text-scale-increase)
+        ("h" shrink-window-horizontally)
+        ("k" shrink-window)
+        ("j" enlarge-window)
+        ("l" enlarge-window-horizontally)
+        ("q" nil "quit"))
+      (add-to-list 'aw-dispatch-alist '(?w hydra-frame-window/body) t)
+      (bind-key "C-c w" #'hydra-frame-window/body))))
 
 ;; Enforce rules for popups
+(defvar shackle--popup-window-list nil) ; all popup windows
+(defvar-local shackle--current-popup-window nil) ; current popup window
+(put 'shackle--current-popup-window 'permanent-local t)
+
 (use-package shackle
   :hook (after-init . shackle-mode)
   :config
-  (defun view-last-popup-buffer ()
-    "View last popup buffer."
-    (interactive)
-    (ignore-errors
-      (display-buffer shackle-last-buffer)))
-  (bind-key "C-h z" #'view-last-popup-buffer)
+  (eval-when-compile
+    (defun shackle-last-popup-buffer ()
+      "View last popup buffer."
+      (interactive)
+      (ignore-errors
+        (display-buffer shackle-last-buffer)))
+    (bind-key "C-h z" #'shackle-last-popup-buffer)
 
-  ;; Add keyword: `autoclose'
-  (defvar shackle--popup-window-list nil) ; all popup windows
-  (defvar-local shackle--current-popup-window nil) ; current popup window
-  (put 'shackle--current-popup-window 'permanent-local t)
+    ;; Add keyword: `autoclose'
+    (defun shackle-display-buffer-hack (fn buffer alist plist)
+      (let ((window (funcall fn buffer alist plist)))
+        (setq shackle--current-popup-window window)
 
-  (defun shackle-display-buffer-hack (fn buffer alist plist)
-    (let ((window (funcall fn buffer alist plist)))
-      (setq shackle--current-popup-window window)
+        (when (plist-get plist :autoclose)
+          (push (cons window buffer) shackle--popup-window-list))
+        window))
 
-      (when (plist-get plist :autoclose)
-        (push (cons window buffer) shackle--popup-window-list))
-      window))
+    (defun shackle-close-popup-window-hack (&rest _)
+      "Close current popup window via `C-g'."
+      (setq shackle--popup-window-list
+            (cl-loop for (window . buffer) in shackle--popup-window-list
+                     if (and (window-live-p window)
+                             (equal (window-buffer window) buffer))
+                     collect (cons window buffer)))
+      ;; `C-g' can deactivate region
+      (when (and (called-interactively-p 'interactive)
+                 (not (region-active-p)))
+        (let (window buffer)
+          (if (one-window-p)
+              (progn
+                (setq window (selected-window))
+                (when (equal (buffer-local-value 'shackle--current-popup-window
+                                                 (window-buffer window))
+                             window)
+                  (winner-undo)))
+            (setq window (caar shackle--popup-window-list))
+            (setq buffer (cdar shackle--popup-window-list))
+            (when (and (window-live-p window)
+                       (equal (window-buffer window) buffer))
+              (delete-window window)
 
-  (defun shackle-close-popup-window-hack (&rest _)
-    "Close current popup window via `C-g'."
-    (setq shackle--popup-window-list
-          (cl-loop for (window . buffer) in shackle--popup-window-list
-                   if (and (window-live-p window)
-                           (equal (window-buffer window) buffer))
-                   collect (cons window buffer)))
-    ;; `C-g' can deactivate region
-    (when (and (called-interactively-p 'interactive)
-               (not (region-active-p)))
-      (let (window buffer)
-        (if (one-window-p)
-            (progn
-              (setq window (selected-window))
-              (when (equal (buffer-local-value 'shackle--current-popup-window
-                                               (window-buffer window))
-                           window)
-                (winner-undo)))
-          (setq window (caar shackle--popup-window-list))
-          (setq buffer (cdar shackle--popup-window-list))
-          (when (and (window-live-p window)
-                     (equal (window-buffer window) buffer))
-            (delete-window window)
+              (pop shackle--popup-window-list))))))
 
-            (pop shackle--popup-window-list))))))
-
-  (advice-add #'keyboard-quit :before #'shackle-close-popup-window-hack)
-  (advice-add #'shackle-display-buffer :around #'shackle-display-buffer-hack)
+    (advice-add #'keyboard-quit :before #'shackle-close-popup-window-hack)
+    (advice-add #'shackle-display-buffer :around #'shackle-display-buffer-hack))
 
   ;; rules
   (setq shackle-default-size 0.3)
