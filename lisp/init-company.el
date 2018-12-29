2f7c54e5427800a1fbec470a0ccb6b165682d1bf
Vincent Zhang <seagle0128@gmail.com>
2018-12-29 23:34:14 +0800

    Add company-box: icons and quickhelp in company.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -59,8 +59,62 @@
         company-dabbrev-ignore-case nil
         company-dabbrev-downcase nil)
 
+  ;; Icons and quickhelp
+  (when emacs/>=26p
+    (use-package company-box
+      :functions (all-the-icons-faicon
+                  all-the-icons-material
+                  all-the-icons-alltheicon)
+      :hook (company-mode . company-box-mode)
+      :init (setq company-box-enable-icon (display-graphic-p))
+      :config
+      (setq company-box-backends-colors nil)
+
+      (with-eval-after-load 'all-the-icons
+        (setq company-box-icons-unknown
+              (all-the-icons-faicon "question-circle" :v-adjust -0.0575))
+
+        (setq company-box-icons-elisp
+              (list
+               (all-the-icons-faicon "cube" :v-adjust -0.0575)        ; Function
+               (all-the-icons-faicon "tag" :v-adjust -0.0575)         ; Variable
+               (all-the-icons-faicon "cog" :v-adjust -0.0575)         ; Feature
+               (all-the-icons-material "palette" :v-adjust -0.2)      ; Face
+               ))
+
+        (setq company-box-icons-yasnippet
+              (all-the-icons-faicon "code" :v-adjust -0.0575))       ; Snippet
+
+        (setq company-box-icons-lsp
+              `(( 1  . ,(all-the-icons-faicon "file-text-o" :v-adjust -0.0575))     ; Text
+                ( 2  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575))            ; Method
+                ( 3  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575))            ; Function
+                ( 4  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575))            ; Constructor
+                ( 5  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Field
+                ( 6  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Variable
+                ( 7  . ,(all-the-icons-faicon "cog" :v-adjust -0.0575))             ; Class
+                ( 8  . ,(all-the-icons-faicon "cogs" :v-adjust -0.0575))            ; Interface
+                ( 9  . ,(all-the-icons-alltheicon "less"))                          ; Module
+                (10  . ,(all-the-icons-faicon "wrench" :v-adjust -0.0575))          ; Property
+                (11  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Unit
+                (12  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Value
+                (13  . ,(all-the-icons-material "content_copy" :v-adjust -0.2))     ; Enum
+                (14  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Keyword
+                (15  . ,(all-the-icons-material "content_paste" :v-adjust -0.2))    ; Snippet
+                (16  . ,(all-the-icons-material "palette" :v-adjust -0.2))          ; Color
+                (17  . ,(all-the-icons-faicon "file" :v-adjust -0.0575))            ; File
+                (18  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Reference
+                (19  . ,(all-the-icons-faicon "folder" :v-adjust -0.0575))          ; Folder
+                (20  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; EnumMember
+                (21  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Constant
+                (22  . ,(all-the-icons-faicon "cog" :v-adjust -0.0575))             ; Struct
+                (23  . ,(all-the-icons-faicon "bolt" :v-adjust -0.0575))            ; Event
+                (24  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Operator
+                (25  . ,(all-the-icons-faicon "cog" :v-adjust -0.0575))             ; TypeParameter
+                )))))
+
   ;; Popup documentation for completion candidates
-  (when (display-graphic-p)
+  (when (and (not emacs/>=26p) (display-graphic-p))
     (use-package company-quickhelp
       :bind (:map company-active-map
                   ("M-h" . company-quickhelp-manual-begin))
