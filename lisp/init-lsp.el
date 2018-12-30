a2d95d32e2efab41cc911725bfa0f602cd3211c0
Vincent Zhang <seagle0128@gmail.com>
2018-12-31 05:23:45 +0800

    Revert "Enable yasnippet in company-lsp."
    This reverts commit 531ec2260e1503b617cfb2d72d4a3388212882a7.

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -43,7 +43,6 @@
    ;; https://github.com/emacs-lsp/lsp-mode#supported-languages
    (use-package lsp-mode
      :diminish lsp-mode
-     :defines (lsp-clients-typescript-server lsp-clients-typescript-server-args)
      :hook (prog-mode . lsp)
      :init
      (setq lsp-auto-guess-root t)       ; Detect project root
@@ -77,9 +76,10 @@
      (add-to-list 'org-babel-lang-list (if emacs/>=26p "shell" "sh"))
      (dolist (lang org-babel-lang-list)
        (eval `(lsp-org-babel-enbale ,lang)))
-
+     :config
      (setq lsp-clients-typescript-server "typescript-language-server"
-           lsp-clients-typescript-server-args '("--stdio")))
+           lsp-clients-typescript-server-args '("--stdio"))
+     (require 'lsp-clients))
 
    (use-package lsp-ui
      :bind (:map lsp-ui-mode-map
@@ -87,9 +87,7 @@
                  ([remap xref-find-references] . lsp-ui-peek-find-references)
                  ("C-c u" . lsp-ui-imenu)))
 
-   (use-package company-lsp
-     :hook (lsp-after-open . (lambda ()
-                               (setq-local company-backends '((company-lsp :with company-yasnippet))))))
+   (use-package company-lsp)
 
    ;; C/C++/Objective-C support
    (use-package ccls
