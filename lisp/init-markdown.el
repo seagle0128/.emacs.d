e3a54977c84d06c0216fffec2c5d68312a0011d5
Vincent Zhang <seagle0128@gmail.com>
2019-01-04 00:59:12 +0800

    Fix: markdown preview.

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -78,7 +78,6 @@
          (markdown-mode . set-flycheck-markdownlint))
   :mode (("README\\.md\\'" . gfm-mode))
   :config
-  (setq markdown-command-needs-filename t)
   (setq markdown-content-type "application/xhtml+xml")
   (setq markdown-css-paths '("https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css"
                              "http://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/github.min.css"))
