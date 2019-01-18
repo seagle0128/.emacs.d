93f752b103bb1e29a53beb383303cc7a78572361
Vincent Zhang <seagle0128@gmail.com>
2019-01-18 16:46:07 +0800

    Beautify company box.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -84,9 +84,9 @@
 
         (setq company-box-icons-elisp
               (list
-               (all-the-icons-faicon "cube" :v-adjust -0.0575)        ; Function
-               (all-the-icons-faicon "tag" :v-adjust -0.0575)         ; Variable
-               (all-the-icons-faicon "cog" :v-adjust -0.0575)         ; Feature
+               (all-the-icons-faicon "cube" :v-adjust -0.0575 :face 'font-lock-constant-face)        ; Function
+               (all-the-icons-faicon "tag" :v-adjust -0.0575 :face 'font-lock-keyword-face)         ; Variable
+               (all-the-icons-faicon "cog" :v-adjust -0.0575 :face 'font-lock-warning-face)         ; Feature
                (all-the-icons-material "palette" :v-adjust -0.2)      ; Face
                ))
 
@@ -95,30 +95,30 @@
 
         (setq company-box-icons-lsp
               `(( 1  . ,(all-the-icons-faicon "file-text-o" :v-adjust -0.0575))     ; Text
-                ( 2  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575))            ; Method
-                ( 3  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575))            ; Function
-                ( 4  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575))            ; Constructor
-                ( 5  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Field
-                ( 6  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Variable
-                ( 7  . ,(all-the-icons-faicon "cog" :v-adjust -0.0575))             ; Class
+                ( 2  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575 :face font-lock-constant-face))            ; Method
+                ( 3  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575 :face font-lock-constant-face))            ; Function
+                ( 4  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575 :face font-lock-constant-face))            ; Constructor
+                ( 5  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575 :face 'font-lock-warning-face))             ; Field
+                ( 6  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575 :face 'font-lock-warning-face))             ; Variable
+                ( 7  . ,(all-the-icons-faicon "cog" :v-adjust -0.0575 :face 'font-lock-warning-face))             ; Class
                 ( 8  . ,(all-the-icons-faicon "cogs" :v-adjust -0.0575))            ; Interface
                 ( 9  . ,(all-the-icons-alltheicon "less"))                          ; Module
                 (10  . ,(all-the-icons-faicon "wrench" :v-adjust -0.0575))          ; Property
                 (11  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Unit
-                (12  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Value
-                (13  . ,(all-the-icons-material "content_copy" :v-adjust -0.2))     ; Enum
-                (14  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Keyword
+                (12  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575 :face 'font-lock-keyword-face))             ; Value
+                (13  . ,(all-the-icons-faicon "file-text-o" :v-adjust -0.0575 :face 'font-lock-warning-face))     ; Enum
+                (14  . ,(all-the-icons-material "format_align_center" :v-adjust -0.2))             ; Keyword
                 (15  . ,(all-the-icons-material "content_paste" :v-adjust -0.2))    ; Snippet
                 (16  . ,(all-the-icons-material "palette" :v-adjust -0.2))          ; Color
                 (17  . ,(all-the-icons-faicon "file" :v-adjust -0.0575))            ; File
                 (18  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Reference
                 (19  . ,(all-the-icons-faicon "folder" :v-adjust -0.0575))          ; Folder
-                (20  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; EnumMember
-                (21  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Constant
-                (22  . ,(all-the-icons-faicon "cog" :v-adjust -0.0575))             ; Struct
-                (23  . ,(all-the-icons-faicon "bolt" :v-adjust -0.0575))            ; Event
+                (20  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575 :face 'font-lock-keyword-face))             ; EnumMember
+                (21  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575 :face 'font-lock-keyword-face))             ; Constant
+                (22  . ,(all-the-icons-faicon "cog" :v-adjust -0.0575 :face 'font-lock-warning-face))             ; Struct
+                (23  . ,(all-the-icons-faicon "bolt" :v-adjust -0.0575 :face 'font-lock-warning-face))            ; Event
                 (24  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))             ; Operator
-                (25  . ,(all-the-icons-faicon "cog" :v-adjust -0.0575))             ; TypeParameter
+                (25  . ,(all-the-icons-faicon "cog" :v-adjust -0.0575 :face 'font-lock-warning-face))             ; TypeParameter
                 )))))
 
   ;; Popup documentation for completion candidates
