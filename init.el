e12a2e44e8f3c90517da253492278774bb6cb776
Vincent Zhang <seagle0128@gmail.com>
2019-01-27 03:48:53 +0800

    Add local directory to load-path.

--- init.el
+++ init.el
@@ -56,7 +56,8 @@
 (defun update-load-path (&rest _)
   "Update `load-path'."
   (push (expand-file-name "site-lisp" user-emacs-directory) load-path)
-  (push (expand-file-name "lisp" user-emacs-directory) load-path))
+  (push (expand-file-name "lisp" user-emacs-directory) load-path)
+  (push "." load-path))
 
 (defun add-subdirs-to-load-path (&rest _)
   "Add subdirectories to `load-path'."
