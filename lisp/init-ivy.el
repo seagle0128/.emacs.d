26b37fa57d818162af533c107a60085f8ff365bb
Vincent Zhang <seagle0128@gmail.com>
2019-01-30 01:19:14 +0800

    Improve my-ivy-format-function.

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -121,7 +121,7 @@
         "Transform CANDS into a string for minibuffer."
         (ivy--format-function-generic
          (lambda (str)
-           (concat (all-the-icons-octicon "chevron-right" :v-adjust -0.05)
+           (concat (all-the-icons-octicon "chevron-right" :v-adjust -0.04)
                    " "
                    (ivy--add-face str 'ivy-current-match)))
          (lambda (str)
