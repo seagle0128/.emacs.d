b016ad96d73f912b8663d49cef7f4b7bd4ec454c
Vincent Zhang <seagle0128@gmail.com>
2019-02-16 00:20:25 +0800

    Revert 0b2d1f6: Add local directly to flycheck-emacs-lisp-load-path.

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -38,7 +38,7 @@
   :hook (after-init . global-flycheck-mode)
   :config
   (setq flycheck-indication-mode 'right-fringe)
-  (setq flycheck-emacs-lisp-load-path (push "." load-path))
+  (setq flycheck-emacs-lisp-load-path 'inherit)
 
   ;; Only check while saving and opening files
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
