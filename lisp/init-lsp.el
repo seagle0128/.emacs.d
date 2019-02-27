f66354cb5bc1e76551b6b4f44e4593beeb89bc7a
Vincent Zhang <seagle0128@gmail.com>
2019-02-28 02:01:34 +0700

    Improve the faces of lsp-ui-doc.

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -55,6 +55,8 @@
        (setq lsp-clients-python-library-directories '("/usr/local/" "/usr/"))))
 
    (use-package lsp-ui
+     :custom-face
+     (lsp-ui-doc-background ((t `(:background ,(face-background 'default)))))
      :bind (:map lsp-ui-mode-map
                  ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
                  ([remap xref-find-references] . lsp-ui-peek-find-references)
@@ -64,6 +66,7 @@
            lsp-ui-doc-include-signature t
            lsp-ui-doc-position 'at-point
            lsp-ui-doc-use-webkit t
+           lsp-ui-doc-border (face-foreground 'default)
 
            lsp-ui-sideline-enable nil
            lsp-ui-sideline-ignore-duplicate t)
