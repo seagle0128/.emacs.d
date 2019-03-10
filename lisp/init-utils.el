aed8136e6ac8876fd267b0fe00ffb70a5c83a2a0
Vincent Zhang <seagle0128@gmail.com>
2019-03-10 23:54:34 +0700

    Add youdao-dictionary-search-at-point-posframe.

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -42,8 +42,37 @@
 
 ;; Youdao Dictionay
 (use-package youdao-dictionary
+  :functions (posframe-show posframe-hide)
+  :preface
+  (with-eval-after-load 'posframe
+    (defun youdao-dictionary-search-at-point-posframe ()
+      "Search word at point and display result with `posframe'."
+      (interactive)
+      (let ((word (youdao-dictionary--region-or-word)))
+        (if word
+            (progn
+              (with-current-buffer (get-buffer-create youdao-dictionary-buffer-name)
+                (let ((inhibit-read-only t))
+                  (erase-buffer)
+                  (youdao-dictionary-mode)
+                  (insert (youdao-dictionary--format-result word))
+                  (goto-char (point-min))
+                  (set (make-local-variable 'youdao-dictionary-current-buffer-word) word)))
+              (posframe-show youdao-dictionary-buffer-name
+                             :position (point))
+              (unwind-protect
+                  (push (read-event) unread-command-events)
+                (posframe-hide youdao-dictionary-buffer-name)))
+          (message "Nothing to look up")))))
+
+  (defun my-youdao-search-at-point ()
+    (interactive)
+    (if (display-graphic-p)
+        (if (fboundp 'youdao-dictionary-search-at-point-posframe)
+            (youdao-dictionary-search-at-point-posframe)
+          (youdao-dictionary-search-at-point-tooltip))))
   :bind (("C-c y" . youdao-dictionary-search-at-point)
-         ("C-c Y" . youdao-dictionary-search-at-point-tooltip))
+         ("C-c Y" . my-youdao-search-at-point))
   :config
   ;; Cache documents
   (setq url-automatic-caching t)
