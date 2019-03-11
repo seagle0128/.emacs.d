40ca14f03e4732b658d6700e7fcf6adb4c873303
Vincent Zhang <seagle0128@gmail.com>
2019-03-12 01:14:49 +0700

    Update company-box configurations accordingly.
    See https://github.com/sebastiencs/company-box/pull/42.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -76,62 +76,8 @@
       :init (setq company-box-enable-icon (display-graphic-p))
       :config
       (setq company-box-backends-colors nil)
-
-      (with-eval-after-load 'all-the-icons
-        (eval-and-compile
-          (defun my-company-box-icon (family icon &rest args)
-            "Defines icons using `all-the-icons' for `company-box'."
-            (when icon
-              (let ((icon (pcase family
-                            ('octicon (all-the-icons-octicon icon :v-adjust -0.05 args))
-                            ('faicon (all-the-icons-faicon icon :v-adjust -0.0575))
-                            ('material (all-the-icons-material icon :v-adjust -0.225 args))
-                            ('alltheicon (all-the-icons-alltheicon icon args)))))
-                (unless (symbolp icon)
-                  (concat icon
-                          (propertize " " 'face 'variable-pitch)))))))
-
-        (setq company-box-icons-unknown
-              (my-company-box-icon 'octicon "file-text"))
-
-        (setq company-box-icons-elisp
-              (list
-               (my-company-box-icon 'faicon "cube")        ; Function
-               (my-company-box-icon 'faicon "tag")         ; Variable
-               (my-company-box-icon 'faicon "cog")         ; Feature
-               (my-company-box-icon 'material "palette")   ; Face
-               ))
-
-        (setq company-box-icons-yasnippet
-              (my-company-box-icon 'octicon "file-code"))  ; Snippet
-
-        (setq company-box-icons-lsp
-              `(( 1  . ,(my-company-box-icon 'faicon "file-text-o"))     ; Text
-                ( 2  . ,(my-company-box-icon 'faicon "cube"))            ; Method
-                ( 3  . ,(my-company-box-icon 'faicon "cube"))            ; Function
-                ( 4  . ,(my-company-box-icon 'faicon "cube"))            ; Constructor
-                ( 5  . ,(my-company-box-icon 'faicon "tag"))             ; Field
-                ( 6  . ,(my-company-box-icon 'faicon "tag"))             ; Variable
-                ( 7  . ,(my-company-box-icon 'faicon "cog"))             ; Class
-                ( 8  . ,(my-company-box-icon 'faicon "cogs"))            ; Interface
-                ( 9  . ,(my-company-box-icon 'alltheicon "less"))        ; Module
-                (10  . ,(my-company-box-icon 'faicon "wrench"))          ; Property
-                (11  . ,(my-company-box-icon 'faicon "tag"))             ; Unit
-                (12  . ,(my-company-box-icon 'faicon "tag"))             ; Value
-                (13  . ,(my-company-box-icon 'faicon "file-text-o"))     ; Enum
-                (14  . ,(my-company-box-icon 'material "format_align_center")) ; Keyword
-                (15  . ,(my-company-box-icon 'material "content_paste")) ; Snippet
-                (16  . ,(my-company-box-icon 'material "palette"))       ; Color
-                (17  . ,(my-company-box-icon 'faicon "file"))            ; File
-                (18  . ,(my-company-box-icon 'faicon "tag"))             ; Reference
-                (19  . ,(my-company-box-icon 'faicon "folder"))          ; Folder
-                (20  . ,(my-company-box-icon 'faicon "tag"))             ; EnumMember
-                (21  . ,(my-company-box-icon 'faicon "tag"))             ; Constant
-                (22  . ,(my-company-box-icon 'faicon "cog"))             ; Struct
-                (23  . ,(my-company-box-icon 'faicon "bolt"))            ; Event
-                (24  . ,(my-company-box-icon 'faicon "tag"))             ; Operator
-                (25  . ,(my-company-box-icon 'faicon "cog"))             ; TypeParameter
-                )))))
+      (if (display-graphic-p)
+          (setq company-box-icons-alist company-box-icons-all-the-icons))))
 
   ;; Popup documentation for completion candidates
   (when (and (not emacs/>=26p) (display-graphic-p))
