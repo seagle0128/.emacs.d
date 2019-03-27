ee57ddf29514ecc85552be2d751559601d284a26
Vincent Zhang <seagle0128@gmail.com>
2019-03-27 22:43:19 +0800

    Add persp-switch-to-buffer to ivy-rich.

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -243,6 +243,17 @@
               (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
              :predicate
              (lambda (cand) (get-buffer cand)))
+            persp-switch-to-buffer
+            (:columns
+             ((ivy-rich-buffer-icon)
+              (ivy-rich-candidate (:width 30))
+              (ivy-rich-switch-buffer-size (:width 7))
+              (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
+              (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
+              (ivy-rich-switch-buffer-project (:width 15 :face success))
+              (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
+             :predicate
+             (lambda (cand) (get-buffer cand)))
             counsel-M-x
             (:columns
              ((counsel-M-x-transformer (:width 50))
