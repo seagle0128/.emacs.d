d3742886dffe45cea6de4fe2d2e1f5df2dcb6c15
Vincent Zhang <seagle0128@gmail.com>
2019-04-17 00:52:44 +0800

    Remove the workaround of ein.
    See https://github.com/millejoh/emacs-ipython-notebook/issues/496 and
    https://github.com/millejoh/emacs-ipython-notebook/pull/514.

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -79,12 +79,7 @@
 (use-package ein
   :diminish ein:notebook-mode
   :defines ein:completion-backend
-  :init
-  (setq ein:completion-backend 'ein:use-company-backend)
-
-  ;; WORKAROUND:https://github.com/millejoh/emacs-ipython-notebook/issues/496
-  (with-eval-after-load 'ido
-    (defalias 'ido-completing-read 'completing-read)))
+  :init (setq ein:completion-backend 'ein:use-company-backend))
 
 (provide 'init-python)
 
