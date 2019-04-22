a882435279da8b4e38610226d177db633d68d102
Vincent Zhang <seagle0128@gmail.com>
2019-04-23 01:17:27 +0800

    Format and refactor ibuffer.

--- lisp/init-ibuffer.el
+++ lisp/init-ibuffer.el
@@ -63,17 +63,16 @@
                                     " " (mode 16 16 :left :elide) " " filename-and-process)
                               (mark " " (name 16 -1) " " filename)))))
 
-  (defun my/ibuffer-find-file ()
-    (interactive)
-    (let ((default-directory (let ((buf (ibuffer-current-buffer)))
-			       (if (buffer-live-p buf)
-				   (with-current-buffer buf
-				     default-directory)
-				 default-directory))))
-      (counsel-find-file default-directory)))
-
   (with-eval-after-load 'counsel
-    (advice-add #'ibuffer-find-file :override #'my/ibuffer-find-file))
+    (defun my-ibuffer-find-file ()
+      (interactive)
+      (let ((default-directory (let ((buf (ibuffer-current-buffer)))
+                                 (if (buffer-live-p buf)
+                                     (with-current-buffer buf
+                                       default-directory)
+                                   default-directory))))
+        (counsel-find-file default-directory)))
+    (advice-add #'ibuffer-find-file :override #'my-ibuffer-find-file))
 
   ;; Group ibuffer's list by project root
   (use-package ibuffer-projectile
