4bf3a84ea56ed7487ccd5bfceefa7c052ac08cff
Vincent Zhang <seagle0128@gmail.com>
2019-05-04 16:09:23 +0800

    Update shackle list for *lsp-help*.

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -197,6 +197,7 @@ _F_ullscreen            _o_ther         _b_alance^^^^          ^ ^         *  /\
           ("*Youdao Dictionary*" :size 0.3 :align 'below :autoclose t)
           ("*Finder*" :select t :size 0.3 :align 'below :autoclose t)
           ("^\\*elfeed-entry" :regexp t :size 0.7 :align 'below :autoclose t)
+          ("^\\*lsp-help\\*$" :regexp t :size 0.4 :align 'below :autoclose t)
 
           (ag-mode :select t :align 'below)
           (grep-mode :select t :align 'below)
