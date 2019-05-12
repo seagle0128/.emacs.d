c016c82e02619dff9b80804d447c88848ee1fd05
Vincent Zhang <seagle0128@gmail.com>
2019-05-13 02:24:49 +0800

    Adjust gc-cons-threshold.

--- init.el
+++ init.el
@@ -38,12 +38,12 @@
 ;; Speed up startup
 (defvar default-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
-(setq gc-cons-threshold 80000000)
+(setq gc-cons-threshold 40000000)
 (add-hook 'emacs-startup-hook
           (lambda ()
             "Restore defalut values after init."
             (setq file-name-handler-alist default-file-name-handler-alist)
-            (setq gc-cons-threshold 400000)
+            (setq gc-cons-threshold 800000)
             (if (boundp 'after-focus-change-function)
                 (add-function :after after-focus-change-function
                               (lambda ()
