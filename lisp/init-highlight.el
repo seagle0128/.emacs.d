96cf8f78e9add3900c777c30fe608729a07dc721
Vincent Zhang <seagle0128@gmail.com>
2019-05-14 03:01:53 +0800

    Don't display first level of indentation.

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -83,6 +83,12 @@
     (setq highlight-indent-guides-method 'character)
     (setq highlight-indent-guides-responsive 'top)
 
+    ;; Don't display first level of indentation
+    (defun my-indent-guides-for-all-but-first-column (level responsive display)
+      (unless (< level 1)
+        (highlight-indent-guides--highlighter-default level responsive display)))
+    (setq highlight-indent-guides-highlighter-function #'my-indent-guides-for-all-but-first-column)
+
     ;; Disable `highlight-indent-guides-mode' in `swiper'
     ;; https://github.com/DarthFennec/highlight-indent-guides/issues/40
     (with-eval-after-load 'ivy
