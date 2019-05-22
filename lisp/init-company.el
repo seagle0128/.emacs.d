65d3914e6ee769df418b17914618aa25c56bbe15
Vincent Zhang <seagle0128@gmail.com>
2019-05-22 21:17:43 +0800

    Adjust delay time of the company help.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -78,7 +78,7 @@
       (setq company-box-backends-colors nil
             company-box-show-single-candidate t
             company-box-max-candidates 50
-            company-box-doc-delay 0.8
+            company-box-doc-delay 0.5
             company-box-icons-alist 'company-box-icons-all-the-icons)
 
       ;; Support `company-common'
@@ -159,7 +159,7 @@
       :bind (:map company-active-map
                   ([remap company-show-doc-buffer] . company-quickhelp-manual-begin))
       :hook (global-company-mode . company-quickhelp-mode)
-      :init (setq company-quickhelp-delay 0.8))))
+      :init (setq company-quickhelp-delay 0.5))))
 
 (provide 'init-company)
 
