8c662aa562dae968b2db0ab3575e889157595afb
Vincent Zhang <seagle0128@gmail.com>
2019-05-24 02:46:22 +0800

    Delay displaying window numbers to avoid mode-line blinking.

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -67,9 +67,9 @@
                (this-win-edges (window-edges (selected-window)))
                (next-win-edges (window-edges (next-window)))
                (this-win-2nd (not (and (<= (car this-win-edges)
-                                        (car next-win-edges))
-                                     (<= (cadr this-win-edges)
-                                        (cadr next-win-edges)))))
+                                           (car next-win-edges))
+                                       (<= (cadr this-win-edges)
+                                           (cadr next-win-edges)))))
                (splitter
                 (if (= (car this-win-edges)
                        (car (window-edges (next-window))))
@@ -83,7 +83,7 @@
             (set-window-buffer (next-window) next-win-buffer)
             (select-window first-win)
             (if this-win-2nd (other-window 1))))))
-  :hook (after-init . ace-window-display-mode)
+  :hook (emacs-startup . ace-window-display-mode)
   :config
   ;; (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 
