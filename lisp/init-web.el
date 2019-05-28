778aebc147a74139e5ff73054167e6871b45f5e5
Vincent Zhang <seagle0128@gmail.com>
2019-05-28 16:20:02 +0800

    Add restclient tools.

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -127,6 +127,19 @@
 (use-package haml-mode)
 (use-package php-mode)
 
+;; REST
+(use-package restclient
+  :mode ("\\.http\\'" . restclient-mode)
+  :config
+  (use-package restclient-test
+    :diminish
+    :hook (restclient-mode . restclient-test-mode))
+
+  (with-eval-after-load 'company
+    (use-package company-restclient
+      :defines company-backends
+      :init (add-to-list 'company-backends 'company-restclient))))
+
 (provide 'init-web)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
