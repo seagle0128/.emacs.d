105781811377ca376698513bbb6e38a6cd74a610
Vincent Zhang <seagle0128@gmail.com>
2019-06-03 00:04:07 +0800

    Update lsp-ui.

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -64,8 +64,8 @@
                  ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
                  ([remap xref-find-references] . lsp-ui-peek-find-references)
                  ("C-c u" . lsp-ui-imenu))
-     :init (setq lsp-ui-doc-use-webkit (featurep 'xwidget-internal)
-                 lsp-ui-doc-enable (not lsp-ui-doc-use-webkit)
+     :init (setq lsp-ui-doc-enable t
+                 lsp-ui-doc-use-webkit t
                  lsp-ui-doc-include-signature t
                  lsp-ui-doc-position 'at-point
                  lsp-ui-doc-border (face-foreground 'default)
