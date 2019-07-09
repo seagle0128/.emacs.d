a0f49d196eeaf67b237b6403cfd59742497a7cb4
Vincent Zhang <seagle0128@gmail.com>
2019-07-09 14:48:23 +0800

    Refactor.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -81,12 +81,7 @@
     :config
     (defun my-all-the-icons-dired--display ()
       "Display the icons of files without colors in a dired buffer."
-      ;; Don't display icons after dired commands (e.g insert-subdir, create-directory)
-      ;; @see https://github.com/jtbm37/all-the-icons-dired/issues/11
-      (all-the-icons-dired--reset)
-
-      (when (and (not all-the-icons-dired-displayed) dired-subdir-alist)
-        (setq-local all-the-icons-dired-displayed t)
+      (when dired-subdir-alist
         (let ((inhibit-read-only t)
               (remote-p (and (fboundp 'tramp-tramp-file-p)
                              (tramp-tramp-file-p default-directory))))
