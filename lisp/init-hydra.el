9c7116378a9a3ce9507dd27a25c6842495d36a22
Vincent Zhang <seagle0128@gmail.com>
2019-07-26 02:08:03 +0800

    Fix incorrect face of hydra title.

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -47,7 +47,7 @@
   (cl-defun pretty-hydra-title (title &optional icon-type icon-name
                                       &key face height v-adjust)
     "Add an icon in the hydra title."
-    (let ((face (or face '(:inherit highlight :inverse-video t)))
+    (let ((face (or face `(:foreground ,(face-background 'highlight))))
           (height (or height 1.0))
           (v-adjust (or v-adjust 0.0)))
       (concat
