0a63dc7ea3b655c9c5dbd769fd19fa5b071a74fd
Vincent Zhang <seagle0128@gmail.com>
2019-07-28 02:32:33 +0800

    Make sure displaying the mode-line correctly after switching frames.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -61,93 +61,6 @@
   (push '(tool-bar-lines . 0) default-frame-alist)
   (push '(vertical-scroll-bars) default-frame-alist))
 
-;; Mode-line
-(use-package doom-modeline
-  :hook (after-init . doom-modeline-mode)
-  :init
-  ;; prevent flash of unstyled modeline at startup
-  (unless after-init-time
-    (setq doom-modeline--old-format mode-line-format)
-    (setq-default mode-line-format nil))
-
-  (setq doom-modeline-major-mode-color-icon t
-        doom-modeline-minor-modes nil
-        doom-modeline-mu4e nil)
-  :bind ("C-<f6>" . doom-modeline-hydra/body)
-  :pretty-hydra
-  ((:title (pretty-hydra-title "Mode Line" 'fileicon "emacs")
-    :color amaranth :quit-key "q")
-   ("Icon"
-    (("i" (setq doom-modeline-icon (not doom-modeline-icon))
-      "display icons" :toggle doom-modeline-icon)
-     ("m" (setq doom-modeline-major-mode-icon (not doom-modeline-major-mode-icon))
-      "major mode" :toggle doom-modeline-major-mode-icon)
-     ("c" (setq doom-modeline-major-mode-color-icon (not doom-modeline-major-mode-color-icon))
-      "colorful major mode" :toggle doom-modeline-major-mode-color-icon)
-     ("s" (setq doom-modeline-buffer-state-icon (not doom-modeline-buffer-state-icon))
-      "buffer state" :toggle doom-modeline-buffer-state-icon)
-     ("d" (setq doom-modeline-buffer-modification-icon (not doom-modeline-buffer-modification-icon))
-      "modification" :toggle doom-modeline-buffer-modification-icon)
-     ("p" (setq doom-modeline-persp-name-icon (not doom-modeline-persp-name-icon))
-      "perspective" :toggle doom-modeline-persp-name-icon))
-    "Segment"
-    (("M" (setq doom-modeline-minor-modes (not doom-modeline-minor-modes))
-      "minor modes" :toggle doom-modeline-minor-modes)
-     ("W" (setq doom-modeline-enable-word-count (not doom-modeline-enable-word-count))
-      "word count" :toggle doom-modeline-enable-word-count)
-     ("E" (setq doom-modeline-buffer-encoding (not doom-modeline-buffer-encoding))
-      "encoding" :toggle doom-modeline-buffer-encoding)
-     ("I" (setq doom-modeline-indent-info (not doom-modeline-indent-info))
-      "indent" :toggle doom-modeline-indent-info)
-     ("L" (setq doom-modeline-lsp (not doom-modeline-lsp))
-      "lsp" :toggle doom-modeline-lsp)
-     ("P" (setq doom-modeline-persp-name (not doom-modeline-persp-name))
-      "perspective" :toggle doom-modeline-persp-name)
-     ("G" (setq doom-modeline-github (not doom-modeline-github))
-      "github" :toggle doom-modeline-github)
-     ("U" (setq doom-modeline-mu4e (not doom-modeline-mu4e))
-      "mu4e" :toggle doom-modeline-mu4e)
-     ("R" (setq doom-modeline-irc (not doom-modeline-irc))
-      "irc" :toggle doom-modeline-irc)
-     ("S" (setq doom-modeline-checker-simple-format (not doom-modeline-checker-simple-format))
-      "simple checker" :toggle doom-modeline-checker-simple-format)
-     ("V" (setq doom-modeline-env-version (not doom-modeline-env-version))
-      "version" :toggle doom-modeline-env-version))
-    "Style"
-    (("t u" (setq doom-modeline-buffer-file-name-style 'truncate-upto-project)
-      "truncate upto project"
-      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-upto-project))
-     ("t f" (setq doom-modeline-buffer-file-name-style 'truncate-from-project)
-      "truncate from project"
-      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-from-project))
-     ("t w" (setq doom-modeline-buffer-file-name-style 'truncate-with-project)
-      "truncate with project"
-      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-with-project))
-     ("t e" (setq doom-modeline-buffer-file-name-style 'truncate-except-project)
-      "truncate except project"
-      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-except-project))
-     ("t r" (setq doom-modeline-buffer-file-name-style 'truncate-upto-root)
-      "truncate upto root"
-      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-upto-root))
-     ("t a" (setq doom-modeline-buffer-file-name-style 'truncate-all)
-      "truncate all"
-      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-all))
-     ("r f" (setq doom-modeline-buffer-file-name-style 'relative-from-project)
-      "relative from project"
-      :toggle (eq doom-modeline-buffer-file-name-style 'relative-from-project))
-     ("r t" (setq doom-modeline-buffer-file-name-style 'relative-to-project)
-      "relative to project"
-      :toggle (eq doom-modeline-buffer-file-name-style 'relative-to-project))
-     ("f" (setq doom-modeline-buffer-file-name-style 'file-name)
-      "file name"
-      :toggle (eq doom-modeline-buffer-file-name-style 'file-name))
-     ("b" (setq doom-modeline-buffer-file-name-style 'buffer-name)
-      "buffer name"
-      :toggle (eq doom-modeline-buffer-file-name-style 'buffer-name))))))
-
-(use-package hide-mode-line
-  :hook (((completion-list-mode completion-in-region-mode) . hide-mode-line-mode)))
-
 ;; Theme
 (if (centaur-compatible-theme-p centaur-theme)
     (progn
@@ -274,6 +187,98 @@
   (progn
     (warn "The current theme may not be compatible with Centaur!")
     (centaur-load-theme centaur-theme)))
+
+;; Mode-line
+(use-package doom-modeline
+  :custom-face (mode-line ((t (:foreground ,(face-foreground 'default)))))
+  :hook ((after-init . doom-modeline-mode)
+         (after-load-theme . (lambda ()
+                               (set-face-foreground
+                                'mode-line
+                                (face-foreground 'default)))))
+  :init
+  ;; prevent flash of unstyled modeline at startup
+  (unless after-init-time
+    (setq doom-modeline--old-format mode-line-format)
+    (setq-default mode-line-format nil))
+
+  (setq doom-modeline-major-mode-color-icon t
+        doom-modeline-minor-modes nil
+        doom-modeline-mu4e nil)
+  :bind ("C-<f6>" . doom-modeline-hydra/body)
+  :pretty-hydra
+  ((:title (pretty-hydra-title "Mode Line" 'fileicon "emacs")
+    :color amaranth :quit-key "q")
+   ("Icon"
+    (("i" (setq doom-modeline-icon (not doom-modeline-icon))
+      "display icons" :toggle doom-modeline-icon)
+     ("m" (setq doom-modeline-major-mode-icon (not doom-modeline-major-mode-icon))
+      "major mode" :toggle doom-modeline-major-mode-icon)
+     ("c" (setq doom-modeline-major-mode-color-icon (not doom-modeline-major-mode-color-icon))
+      "colorful major mode" :toggle doom-modeline-major-mode-color-icon)
+     ("s" (setq doom-modeline-buffer-state-icon (not doom-modeline-buffer-state-icon))
+      "buffer state" :toggle doom-modeline-buffer-state-icon)
+     ("d" (setq doom-modeline-buffer-modification-icon (not doom-modeline-buffer-modification-icon))
+      "modification" :toggle doom-modeline-buffer-modification-icon)
+     ("p" (setq doom-modeline-persp-name-icon (not doom-modeline-persp-name-icon))
+      "perspective" :toggle doom-modeline-persp-name-icon))
+    "Segment"
+    (("M" (setq doom-modeline-minor-modes (not doom-modeline-minor-modes))
+      "minor modes" :toggle doom-modeline-minor-modes)
+     ("W" (setq doom-modeline-enable-word-count (not doom-modeline-enable-word-count))
+      "word count" :toggle doom-modeline-enable-word-count)
+     ("E" (setq doom-modeline-buffer-encoding (not doom-modeline-buffer-encoding))
+      "encoding" :toggle doom-modeline-buffer-encoding)
+     ("I" (setq doom-modeline-indent-info (not doom-modeline-indent-info))
+      "indent" :toggle doom-modeline-indent-info)
+     ("L" (setq doom-modeline-lsp (not doom-modeline-lsp))
+      "lsp" :toggle doom-modeline-lsp)
+     ("P" (setq doom-modeline-persp-name (not doom-modeline-persp-name))
+      "perspective" :toggle doom-modeline-persp-name)
+     ("G" (setq doom-modeline-github (not doom-modeline-github))
+      "github" :toggle doom-modeline-github)
+     ("U" (setq doom-modeline-mu4e (not doom-modeline-mu4e))
+      "mu4e" :toggle doom-modeline-mu4e)
+     ("R" (setq doom-modeline-irc (not doom-modeline-irc))
+      "irc" :toggle doom-modeline-irc)
+     ("S" (setq doom-modeline-checker-simple-format (not doom-modeline-checker-simple-format))
+      "simple checker" :toggle doom-modeline-checker-simple-format)
+     ("V" (setq doom-modeline-env-version (not doom-modeline-env-version))
+      "version" :toggle doom-modeline-env-version))
+    "Style"
+    (("t u" (setq doom-modeline-buffer-file-name-style 'truncate-upto-project)
+      "truncate upto project"
+      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-upto-project))
+     ("t f" (setq doom-modeline-buffer-file-name-style 'truncate-from-project)
+      "truncate from project"
+      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-from-project))
+     ("t w" (setq doom-modeline-buffer-file-name-style 'truncate-with-project)
+      "truncate with project"
+      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-with-project))
+     ("t e" (setq doom-modeline-buffer-file-name-style 'truncate-except-project)
+      "truncate except project"
+      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-except-project))
+     ("t r" (setq doom-modeline-buffer-file-name-style 'truncate-upto-root)
+      "truncate upto root"
+      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-upto-root))
+     ("t a" (setq doom-modeline-buffer-file-name-style 'truncate-all)
+      "truncate all"
+      :toggle (eq doom-modeline-buffer-file-name-style 'truncate-all))
+     ("r f" (setq doom-modeline-buffer-file-name-style 'relative-from-project)
+      "relative from project"
+      :toggle (eq doom-modeline-buffer-file-name-style 'relative-from-project))
+     ("r t" (setq doom-modeline-buffer-file-name-style 'relative-to-project)
+      "relative to project"
+      :toggle (eq doom-modeline-buffer-file-name-style 'relative-to-project))
+     ("f" (setq doom-modeline-buffer-file-name-style 'file-name)
+      "file name"
+      :toggle (eq doom-modeline-buffer-file-name-style 'file-name))
+     ("b" (setq doom-modeline-buffer-file-name-style 'buffer-name)
+      "buffer name"
+      :toggle (eq doom-modeline-buffer-file-name-style 'buffer-name))))))
+
+(use-package hide-mode-line
+  :hook (((completion-list-mode completion-in-region-mode) . hide-mode-line-mode)))
 
 ;; Icons
 ;; NOTE: Must run `M-x all-the-icons-install-fonts', and install fonts manually on Windows
