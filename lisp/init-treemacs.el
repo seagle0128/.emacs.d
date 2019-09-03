60cac9a385215c3a1c10cd10ffb32860fc7b279f
Vincent Zhang <seagle0128@gmail.com>
2019-09-04 00:43:08 +0800

    Improve treemacs.

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -37,7 +37,6 @@
 (when emacs/>=25.2p
   ;; A tree layout file explorer
   (use-package treemacs
-    :defines winum-keymap
     :commands (treemacs-follow-mode
                treemacs-filewatch-mode
                treemacs-fringe-indicator-mode
@@ -52,44 +51,17 @@
            ("C-x t M-t" . treemacs-find-tag)
            :map treemacs-mode-map
            ([mouse-1]   . treemacs-single-click-expand-action))
-    :init
-    (with-eval-after-load 'winum
-      (bind-key (kbd "M-9") #'treemacs-select-window winum-keymap))
     :config
     (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay      0.5
-          treemacs-display-in-side-window        t
-          treemacs-eldoc-display                 t
-          treemacs-file-event-delay              5000
-          treemacs-file-follow-delay             0.2
+          treemacs-sorting                       'alphabetic-case-insensitive-desc
           treemacs-follow-after-init             t
-          treemacs-git-command-pipe              ""
-          treemacs-goto-tag-strategy             'refetch-index
-          treemacs-indentation                   2
-          treemacs-indentation-string            " "
-          treemacs-is-never-other-window         nil
-          treemacs-max-git-entries               5000
-          treemacs-no-png-images                 nil
-          treemacs-no-delete-other-windows       t
-          treemacs-project-follow-cleanup        nil
-          treemacs-recenter-distance             0.1
-          treemacs-recenter-after-file-follow    nil
-          treemacs-recenter-after-tag-follow     nil
-          treemacs-recenter-after-project-jump   'always
-          treemacs-recenter-after-project-expand 'on-distance
-          treemacs-show-cursor                   nil
-          treemacs-show-hidden-files             t
-          treemacs-silent-filewatch              nil
-          treemacs-silent-refresh                nil
-          treemacs-sorting                       'alphabetic-desc
-          treemacs-space-between-root-nodes      t
-          treemacs-tag-follow-cleanup            t
-          treemacs-tag-follow-delay              1.5
+          treemacs-is-never-other-window         t
+          treemacs-silent-filewatch              t
+          treemacs-silent-refresh                t
           treemacs-width                         30)
 
     (treemacs-follow-mode t)
     (treemacs-filewatch-mode t)
-    ;; (treemacs-fringe-indicator-mode t)
     (pcase (cons (not (null (executable-find "git")))
                  (not (null (executable-find "python3"))))
       (`(t . t)
@@ -97,7 +69,7 @@
       (`(t . _)
        (treemacs-git-mode 'simple)))
 
-    ;; Projectile integration for treemacs
+    ;; Projectile integration
     (use-package treemacs-projectile
       :after projectile
       :bind (([M-f8] . treemacs-projectile)
