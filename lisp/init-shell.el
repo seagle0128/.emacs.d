66cbd056796274a8ba60223fa2c47d951190a458
Vincent Zhang <seagle0128@gmail.com>
2019-09-22 23:36:06 +0800

    [Compatibility] Don't apply xterm-color-filter in rg-mode.

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -98,7 +98,10 @@
   ;; For compilation buffers
   (setq compilation-environment '("TERM=xterm-256color"))
   (defun my-advice-compilation-filter (f proc string)
-    (funcall f proc (xterm-color-filter string)))
+    (funcall f proc
+             (if (eq major-mode 'rg-mode) ; compatible with `rg'
+                 string
+               (xterm-color-filter string))))
   (advice-add 'compilation-filter :around #'my-advice-compilation-filter))
 
 ;; Better term
