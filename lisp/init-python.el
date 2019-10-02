4b74650a7a50d697e2019b5fbb505b5ddb299dfa
Vincent Zhang <seagle0128@gmail.com>
2019-10-02 20:37:12 +0800

    Default to Python 3.

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -44,6 +44,12 @@
                                   (process-query-on-exit-flag
                                    (get-process "Python"))))
   :init
+  ;; Default to Python 3. Prefer the versioned Python binaries since some
+  ;; systems stupidly make the unversioned one point at Python 2.
+  (when (and (executable-find "python3")
+             (string= python-shell-interpreter "python"))
+    (setq python-shell-interpreter "python3"))
+
   ;; Disable readline based native completion
   (setq python-shell-completion-native-enable nil)
   :config
