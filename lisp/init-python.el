f547624a5a63e2ee53593c349ed2e81f78ea7a0f
Vincent Zhang <seagle0128@gmail.com>
2019-10-05 01:44:47 +0800

    Fix： Symbol's value as variable is void: python-shell-interpreter.

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -44,15 +44,15 @@
                                   (process-query-on-exit-flag
                                    (get-process "Python"))))
   :init
+  ;; Disable readline based native completion
+  (setq python-shell-completion-native-enable nil)
+  :config
   ;; Default to Python 3. Prefer the versioned Python binaries since some
   ;; systems stupidly make the unversioned one point at Python 2.
   (when (and (executable-find "python3")
              (string= python-shell-interpreter "python"))
     (setq python-shell-interpreter "python3"))
 
-  ;; Disable readline based native completion
-  (setq python-shell-completion-native-enable nil)
-  :config
   ;; Env vars
   (with-eval-after-load 'exec-path-from-shell
     (exec-path-from-shell-copy-env "PYTHONPATH"))
