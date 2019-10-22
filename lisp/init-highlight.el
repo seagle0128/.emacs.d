7494b1b3f6593a2e9ae15061d67fea1711fbb74d
Vincent Zhang <seagle0128@gmail.com>
2019-10-22 15:57:59 +0800

    Support :extend attribute in hl-line.

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -36,6 +36,7 @@
 ;; Highlight the current line
 (use-package hl-line
   :ensure nil
+  :custom-face (hl-line ((t (:extend t)))) ; FIXME: compatible with 27
   :hook (after-init . global-hl-line-mode))
 
 ;; Highlight matching parens
