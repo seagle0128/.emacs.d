b76ada76a38331f3e9a6f166caf820d3d9a91caa
Vincent Zhang <seagle0128@gmail.com>
2019-11-07 22:47:54 +0800

    Update eshell configurations for 27.

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -33,10 +33,8 @@
 ;; Emacs command shell
 (use-package eshell
   :ensure nil
-  :defines (compilation-last-buffer eshell-prompt-function)
-  :commands (eshell/alias
-             eshell-send-input eshell-flatten-list
-             eshell-interactive-output-p eshell-parse-command)
+  :defines eshell-prompt-function
+  :functions eshell/alias
   :hook (eshell-mode . (lambda ()
                          (bind-key "C-l" 'eshell/clear eshell-mode-map)
                          (eshell/alias "f" "find-file $1")
@@ -44,71 +42,75 @@
                          (eshell/alias "d" "dired $1")
                          (eshell/alias "ll" "ls -l")
                          (eshell/alias "la" "ls -al")))
-  :preface
-  (defun eshell/clear ()
-    "Clear the eshell buffer."
-    (interactive)
-    (let ((inhibit-read-only t))
-      (erase-buffer)
-      (eshell-send-input)))
-
-  (defun eshell/emacs (&rest args)
-    "Open a file (ARGS) in Emacs.  Some habits die hard."
-    (if (null args)
-        ;; If I just ran "emacs", I probably expect to be launching
-        ;; Emacs, which is rather silly since I'm already in Emacs.
-        ;; So just pretend to do what I ask.
-        (bury-buffer)
-      ;; We have to expand the file names or else naming a directory in an
-      ;; argument causes later arguments to be looked for in that directory,
-      ;; not the starting directory
-      (mapc #'find-file (mapcar #'expand-file-name (eshell-flatten-list (reverse args))))))
-
-  (defalias 'eshell/e 'eshell/emacs)
-
-  (defun eshell/ec (&rest args)
-    "Compile a file (ARGS) in Emacs.  Use `compile' to do background make."
-    (if (eshell-interactive-output-p)
-        (let ((compilation-process-setup-function
-               (list 'lambda nil
-                     (list 'setq 'process-environment
-                           (list 'quote (eshell-copy-environment))))))
-          (compile (eshell-flatten-and-stringify args))
-          (pop-to-buffer compilation-last-buffer))
-      (throw 'eshell-replace-command
-             (let ((l (eshell-stringify-list (eshell-flatten-list args))))
-               (eshell-parse-command (car l) (cdr l))))))
-  (put 'eshell/ec 'eshell-no-numeric-conversions t)
-
-  (defun eshell-view-file (file)
-    "View FILE.  A version of `view-file' which properly rets the eshell prompt."
-    (interactive "fView file: ")
-    (unless (file-exists-p file) (error "%s does not exist" file))
-    (let ((buffer (find-file-noselect file)))
-      (if (eq (get (buffer-local-value 'major-mode buffer) 'mode-class)
-              'special)
-          (progn
-            (switch-to-buffer buffer)
-            (message "Not using View mode because the major mode is special"))
-        (let ((undo-window (list (window-buffer) (window-start)
-                                 (+ (window-point)
-                                    (length (funcall eshell-prompt-function))))))
-          (switch-to-buffer buffer)
-          (view-mode-enter (cons (selected-window) (cons nil undo-window))
-                           'kill-buffer)))))
-
-  (defun eshell/less (&rest args)
-    "Invoke `view-file' on a file (ARGS).  \"less +42 foo\" will go to line 42 in the buffer for foo."
-    (while args
-      (if (string-match "\\`\\+\\([0-9]+\\)\\'" (car args))
-          (let* ((line (string-to-number (match-string 1 (pop args))))
-                 (file (pop args)))
-            (eshell-view-file file)
-            (forward-line line))
-        (eshell-view-file (pop args)))))
-
-  (defalias 'eshell/more 'eshell/less)
   :config
+  (with-no-warnings
+    (unless (fboundp #'flatten-tree)
+      (defalias #'flatten-tree #'eshell-flatten-list))
+
+    (defun eshell/clear ()
+      "Clear the eshell buffer."
+      (interactive)
+      (let ((inhibit-read-only t))
+        (erase-buffer)
+        (eshell-send-input)))
+
+    (defun eshell/emacs (&rest args)
+      "Open a file (ARGS) in Emacs.  Some habits die hard."
+      (if (null args)
+          ;; If I just ran "emacs", I probably expect to be launching
+          ;; Emacs, which is rather silly since I'm already in Emacs.
+          ;; So just pretend to do what I ask.
+          (bury-buffer)
+        ;; We have to expand the file names or else naming a directory in an
+        ;; argument causes later arguments to be looked for in that directory,
+        ;; not the starting directory
+        (mapc #'find-file (mapcar #'expand-file-name (flatten-tree (reverse args))))))
+
+    (defalias 'eshell/e 'eshell/emacs)
+
+    (defun eshell/ec (&rest args)
+      "Compile a file (ARGS) in Emacs.  Use `compile' to do background make."
+      (if (eshell-interactive-output-p)
+          (let ((compilation-process-setup-function
+                 (list 'lambda nil
+                       (list 'setq 'process-environment
+                             (list 'quote (eshell-copy-environment))))))
+            (compile (eshell-flatten-and-stringify args))
+            (pop-to-buffer compilation-last-buffer))
+        (throw 'eshell-replace-command
+               (let ((l (eshell-stringify-list (flatten-tree args))))
+                 (eshell-parse-command (car l) (cdr l))))))
+    (put 'eshell/ec 'eshell-no-numeric-conversions t)
+
+    (defun eshell-view-file (file)
+      "View FILE.  A version of `view-file' which properly rets the eshell prompt."
+      (interactive "fView file: ")
+      (unless (file-exists-p file) (error "%s does not exist" file))
+      (let ((buffer (find-file-noselect file)))
+        (if (eq (get (buffer-local-value 'major-mode buffer) 'mode-class)
+                'special)
+            (progn
+              (switch-to-buffer buffer)
+              (message "Not using View mode because the major mode is special"))
+          (let ((undo-window (list (window-buffer) (window-start)
+                                   (+ (window-point)
+                                      (length (funcall eshell-prompt-function))))))
+            (switch-to-buffer buffer)
+            (view-mode-enter (cons (selected-window) (cons nil undo-window))
+                             'kill-buffer)))))
+
+    (defun eshell/less (&rest args)
+      "Invoke `view-file' on a file (ARGS).  \"less +42 foo\" will go to line 42 in the buffer for foo."
+      (while args
+        (if (string-match "\\`\\+\\([0-9]+\\)\\'" (car args))
+            (let* ((line (string-to-number (match-string 1 (pop args))))
+                   (file (pop args)))
+              (eshell-view-file file)
+              (forward-line line))
+          (eshell-view-file (pop args)))))
+
+    (defalias 'eshell/more 'eshell/less))
+
   ;;  Display extra information for prompt
   (use-package eshell-prompt-extras
     :after esh-opt
