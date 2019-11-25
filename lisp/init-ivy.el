7137d6873f484ceb938fba8f778a6a399cf1b4f3
Vincent Zhang <seagle0128@gmail.com>
2019-11-26 03:54:29 +0800

    Use gls in counsel-find-file-occur.

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -135,9 +135,13 @@
   (setq counsel-find-file-at-point t
         counsel-yank-pop-separator "\n────────\n")
 
-  ;; Use faster search tool: ripgrep (rg)
+  ;; Use the faster search tool: ripgrep (`rg')
   (when (executable-find "rg")
-    (setq counsel-grep-base-command "rg -S --no-heading --line-number --color never '%s' %s"))
+    (setq counsel-grep-base-command "rg -S --no-heading --line-number --color never '%s' %s")
+    (when (and sys/macp (executable-find "gls"))
+      (setq counsel-find-file-occur-use-find nil
+            counsel-find-file-occur-cmd
+            "gls -a | grep -i -E '%s' | tr '\\n' '\\0' | xargs -0 gls -d --group-directories-first")))
   :config
   (with-no-warnings
     ;; Pre-fill search keywords
