54d720158f11298298845ae1f81703d898e92e42
Vincent Zhang <seagle0128@gmail.com>
2019-12-13 04:37:33 +0800

    Fix ivy sorting.

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -265,22 +265,25 @@ This is for use in `ivy-re-builders-alist'."
         (ivy-prescient-re-builder str)))
 
     (setq ivy-prescient-retain-classic-highlighting t
-          ivy-re-builders-alist '((counsel-ag . ivy-prescient-non-fuzzy)
-                                  (counsel-rg . ivy-prescient-non-fuzzy)
-                                  (counsel-pt . ivy-prescient-non-fuzzy)
-                                  (counsel-grep . ivy-prescient-non-fuzzy)
-                                  (counsel-imenu . ivy-prescient-non-fuzzy)
-                                  (counsel-yank-pop . ivy-prescient-non-fuzzy)
-                                  (swiper . ivy-prescient-non-fuzzy)
-                                  (swiper-isearch . ivy-prescient-non-fuzzy)
-                                  (swiper-all . ivy-prescient-non-fuzzy)
-                                  (lsp-ivy-workspace-symbol . ivy-prescient-non-fuzzy)
-                                  (lsp-ivy-global-workspace-symbol . ivy-prescient-non-fuzzy)
-                                  (insert-char . ivy-prescient-non-fuzzy)
-                                  (counsel-unicode-char . ivy-prescient-non-fuzzy)
-                                  (t . ivy-prescient-re-builder))
-          ivy-prescient-sort-commands '(:not swiper swiper-isearch ivy-switch-buffer
-                                        counsel-grep counsel-ag counsel-yank-pop))
+          ivy-re-builders-alist
+          '((counsel-ag . ivy-prescient-non-fuzzy)
+            (counsel-rg . ivy-prescient-non-fuzzy)
+            (counsel-pt . ivy-prescient-non-fuzzy)
+            (counsel-grep . ivy-prescient-non-fuzzy)
+            (counsel-imenu . ivy-prescient-non-fuzzy)
+            (counsel-yank-pop . ivy-prescient-non-fuzzy)
+            (swiper . ivy-prescient-non-fuzzy)
+            (swiper-isearch . ivy-prescient-non-fuzzy)
+            (swiper-all . ivy-prescient-non-fuzzy)
+            (lsp-ivy-workspace-symbol . ivy-prescient-non-fuzzy)
+            (lsp-ivy-global-workspace-symbol . ivy-prescient-non-fuzzy)
+            (insert-char . ivy-prescient-non-fuzzy)
+            (counsel-unicode-char . ivy-prescient-non-fuzzy)
+            (t . ivy-prescient-re-builder))
+          ivy-prescient-sort-commands
+          '(:not swiper swiper-isearch ivy-switch-buffer
+            counsel-grep counsel-git-grep counsel-ag counsel-imenu
+            counsel-yank-pop counsel-recentf counsel-buffer-or-recentf))
 
     (ivy-prescient-mode 1))
 
