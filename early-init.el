a94e2566a2d1f901e23f9aed26f7bdb3e14c67e6
Vincent Zhang <seagle0128@gmail.com>
2019-12-28 20:07:03 +1300

    [Enhancement] Optimize the startup experience.
    Inhibit resizing frame, and disable the unnecessary UI elements.

--- early-init.el
+++ early-init.el
@@ -32,18 +32,22 @@
 ;;; Code:
 
 ;; Defer garbage collection further back in the startup process
-(setq gc-cons-threshold (if (display-graphic-p) 400000000 100000000))
+(setq gc-cons-threshold most-positive-fixnum)
 
 ;; Package initialize occurs automatically, before `user-init-file' is
 ;; loaded, but after `early-init-file'. We handle package
 ;; initialization, so we must prevent Emacs from doing it early!
 (setq package-enable-at-startup nil)
 
+;; Inhibit resizing frame
+(setq frame-inhibit-implied-resize t)
+
 ;; Faster to disable these here (before they've been initialized)
-(unless (and (display-graphic-p) (eq system-type 'darwin))
-  (push '(menu-bar-lines . 0) default-frame-alist))
+(push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+(when (eq system-type 'darwin)
+  (push '(ns-transparent-titlebar . t) default-frame-alist))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; early-init.el ends here
