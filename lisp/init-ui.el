06e75ca627009c3a8c78dbb33cf710e856b78de2
Vincent Zhang <seagle0128@gmail.com>
2019-12-31 18:40:47 +1300

    Add minions.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -129,16 +129,16 @@
 
 ;; Mode-line
 (use-package doom-modeline
+  :custom
+  (doom-modeline-minor-modes t)
+  (doom-modeline-unicode-fallback t)
+  (doom-modeline-mu4e nil)
   :hook (after-init . doom-modeline-mode)
   :init
   ;; prevent flash of unstyled modeline at startup
   (unless after-init-time
     (setq doom-modeline--old-format mode-line-format)
     (setq-default mode-line-format nil))
-
-  (setq doom-modeline-major-mode-color-icon t
-        doom-modeline-minor-modes nil
-        doom-modeline-mu4e nil)
   :bind (:map doom-modeline-mode-map
          ("C-<f6>" . doom-modeline-hydra/body))
   :pretty-hydra
@@ -232,6 +232,10 @@
 
 (use-package hide-mode-line
   :hook (((completion-list-mode completion-in-region-mode) . hide-mode-line-mode)))
+
+;; A minor-mode menu for mode-line
+(use-package minions
+  :hook (doom-modeline-mode . minions-mode))
 
 ;; Icons
 ;; NOTE: Must run `M-x all-the-icons-install-fonts', and install fonts manually on Windows
