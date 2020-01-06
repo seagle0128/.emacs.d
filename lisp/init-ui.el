2a781ce054a55ec2919137431c033c745d43a45b
Vincent Zhang <seagle0128@gmail.com>
2020-01-07 01:52:17 +0800

    Remove workaround of doom-visual-bell.
    See https://github.com/hlissner/emacs-doom-themes/issues/339.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -76,34 +76,6 @@
 
         ;; Enable flashing mode-line on errors
         (doom-themes-visual-bell-config)
-        ;; WORKAROUND: use legacy codes
-        (set-face-attribute 'doom-visual-bell nil
-                            :inherit 'mode-line
-                            :background (face-foreground 'error)
-                            :inverse-video 'unspecified)
-        (defvar doom-themes--bell-p nil)
-        (defun doom-themes-visual-bell-fn ()
-          "Blink the mode-line red briefly. Set `ring-bell-function' to this to use it."
-          (unless doom-themes--bell-p
-            (let ((old-remap (copy-alist face-remapping-alist)))
-              (setq doom-themes--bell-p t)
-              (setq face-remapping-alist
-                    (append (delete (assq 'mode-line face-remapping-alist)
-                                    face-remapping-alist)
-                            '((mode-line doom-visual-bell))))
-              (force-mode-line-update)
-              (run-with-timer 0.15 nil
-                              (lambda (remap buf)
-                                (with-current-buffer buf
-                                  (when (assq 'mode-line face-remapping-alist)
-                                    (setq face-remapping-alist remap
-                                          doom-themes--bell-p nil))
-                                  (force-mode-line-update)))
-                              old-remap
-                              (current-buffer)))))
-
-        ;; Corrects (and improves) org-mode's native fontification.
-        (doom-themes-org-config)
 
         ;; Enable customized theme (`all-the-icons' must be installed!)
         (setq doom-themes-treemacs-theme "doom-colors")
