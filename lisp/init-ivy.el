bc0882b28eff53d2d31904a506170c8942191036
Vincent Zhang <seagle0128@gmail.com>
2020-01-07 15:47:45 +0800

    Fix #145: counsel-grep-base-command.

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -137,7 +137,7 @@
 
   ;; Use the faster search tool: ripgrep (`rg')
   (when (executable-find "rg")
-    (setq counsel-grep-base-command "rg -S --no-heading --line-number --color never '%s' %s")
+    (setq counsel-grep-base-command "rg -S --no-heading --line-number --color never %s %s")
     (when (and sys/macp (executable-find "gls"))
       (setq counsel-find-file-occur-use-find nil
             counsel-find-file-occur-cmd
