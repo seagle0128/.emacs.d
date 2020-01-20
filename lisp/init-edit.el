3b36f0e801fea96515d8e10fd609b40886a62914
Vincent Zhang <seagle0128@gmail.com>
2020-01-20 21:56:16 +0800

    Refactor: derived-mode-p.

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -162,12 +162,8 @@
   ;; Be slightly less aggressive in C/C++/C#/Java/Go/Swift
   (add-to-list
    'aggressive-indent-dont-indent-if
-   '(and (or (derived-mode-p 'c-mode)
-             (derived-mode-p 'c++-mode)
-             (derived-mode-p 'csharp-mode)
-             (derived-mode-p 'java-mode)
-             (derived-mode-p 'go-mode)
-             (derived-mode-p 'swift-mode))
+   '(and (derived-mode-p 'c-mode 'c++-mode 'csharp-mode
+                         'java-mode 'go-mode 'swift-mode)
          (null (string-match "\\([;{}]\\|\\b\\(if\\|for\\|while\\)\\b\\)"
                              (thing-at-point 'line))))))
 
