01d544408736b836c5c2c2724fb663c54bc07503
Vincent Zhang <seagle0128@gmail.com>
2020-02-18 01:39:44 +0800

    Update keybindings for company.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -38,13 +38,12 @@
   :defines (company-dabbrev-ignore-case company-dabbrev-downcase)
   :commands company-abort
   :bind (("M-/" . company-complete)
-         ("<backtab>" . company-yasnippet)
+         ("C-M-i" . company-complete)
          :map company-active-map
-         ("C-p" . company-select-previous)
-         ("C-n" . company-select-next)
+         ;; ("C-p" . company-select-previous)
+         ;; ("C-n" . company-select-next)
          ("<tab>" . company-complete-common-or-cycle)
          ("<backtab>" . my-company-yasnippet)
-         ;; ("C-c C-y" . my-company-yasnippet)
          :map company-search-map
          ("C-p" . company-select-previous)
          ("C-n" . company-select-next))
