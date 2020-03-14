c16f6935b57fed8609f5d6f8526dac623f0e3de2
Vincent Zhang <seagle0128@gmail.com>
2020-03-14 23:15:32 +0800

    [Feature] Better yasnippet integration in company.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -65,10 +65,36 @@
         company-require-match nil
         company-dabbrev-ignore-case nil
         company-dabbrev-downcase nil
-        company-global-modes '(not erc-mode message-mode help-mode gud-mode eshell-mode shell-mode)
-        company-backends '(company-capf)
+        company-global-modes '(not erc-mode message-mode help-mode
+                                   gud-mode eshell-mode shell-mode)
         company-frontends '(company-pseudo-tooltip-frontend
                             company-echo-metadata-frontend))
+
+  ;; `yasnippet' integration
+  (with-no-warnings
+    (defun company-backend-with-yas (backend)
+      "Add `yasnippet' to company backend."
+      (if (and (listp backend) (member 'company-yasnippet backend))
+          backend
+        (append (if (consp backend) backend (list backend))
+                '(:with company-yasnippet))))
+
+    (defun my-company-enbale-yas (&rest _)
+      "Enable `yasnippet' in `company'."
+      (setq company-backends (mapcar #'company-backend-with-yas company-backends)))
+    ;; Enable in current backends
+    (my-company-enbale-yas)
+    ;; Support `company-lsp'
+    (advice-add #'lsp--auto-configure :after #'my-company-enbale-yas)
+
+    (defun my-company-yasnippet-disable-inline (fun command &optional arg &rest _ignore)
+      "Enable yasnippet but disable it inline."
+      (if (eq command 'prefix)
+          (when-let ((prefix (funcall fun 'prefix)))
+            (unless (memq (char-before (- (point) (length prefix))) '(?. ?> ?\())
+              prefix))
+        (funcall fun command arg)))
+    (advice-add #'company-yasnippet :around #'my-company-yasnippet-disable-inline))
 
   ;; Better sorting and filtering
   (use-package company-prescient
@@ -96,8 +122,12 @@
                                             (substring (propertize candidate 'face 'company-box-candidate)
                                                        (length company-common) nil)))
                   (align-string (when annotation
-                                  (concat " " (and company-tooltip-align-annotations
-                                                   (propertize " " 'display `(space :align-to (- right-fringe ,(or len-a 0) 1)))))))
+                                  (concat
+                                   " "
+                                   (and company-tooltip-align-annotations
+                                        (propertize " "
+                                                    'display
+                                                    `(space :align-to (- right-fringe ,(or len-a 0) 1)))))))
                   (space company-box--space)
                   (icon-p company-box-enable-icon)
                   (annotation-string (and annotation (propertize annotation 'face 'company-box-annotation)))
