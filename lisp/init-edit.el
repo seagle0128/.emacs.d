973e90254763ddb6cffe2ec47bbe5bd64280815a
Vincent Zhang <seagle0128@gmail.com>
2020-03-25 09:33:07 +0800

    Disable aggressive-indent in scala-mode.
    Fix #191.

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -177,7 +177,7 @@
                             (aggressive-indent-mode -1)))))
   :config
   ;; Disable in some modes
-  (dolist (mode '(asm-mode web-mode html-mode css-mode go-mode prolog-inferior-mode))
+  (dolist (mode '(asm-mode web-mode html-mode css-mode go-mode scala-mode prolog-inferior-mode))
     (push mode aggressive-indent-excluded-modes))
 
   ;; Disable in some commands
