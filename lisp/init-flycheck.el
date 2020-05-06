2ac268d399f83ae2417992d1115cc747cb3b64fa
Vincent Zhang <seagle0128@gmail.com>
2020-05-06 08:47:18 +0800

    Disable flycheck in lisp-indentation-mode.

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -38,8 +38,8 @@
   :commands flycheck-redefine-standard-error-levels
   :hook (after-init . global-flycheck-mode)
   :init (setq flycheck-global-modes
-              '(not text-mode outline-mode fundamental-mode org-mode
-                    diff-mode shell-mode eshell-mode term-mode vterm-mode)
+              '(not text-mode outline-mode fundamental-mode lisp-interaction-mode
+                    org-mode diff-mode shell-mode eshell-mode term-mode vterm-mode)
               flycheck-emacs-lisp-load-path 'inherit
               flycheck-indication-mode (if (display-graphic-p)
                                            'right-fringe
