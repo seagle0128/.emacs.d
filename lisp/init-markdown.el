f74dcce4db90f5a23ea1bc788c91b7a4a8892a18
Youmu <condy0919@gmail.com>
2020-06-14 12:25:51 +0800

    Inhibit markdown builtin style map prompt
    Use `which-key` instead.

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -85,6 +85,7 @@ mermaid.initialize({
 
   ;; Use `which-key' instead
   (advice-add #'markdown--command-map-prompt :override #'ignore)
+  (advice-add #'markdown--style-map-prompt   :override #'ignore)
   :config
   (add-to-list 'markdown-code-lang-modes '("mermaid" . mermaid-mode))
 
