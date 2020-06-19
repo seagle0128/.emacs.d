9dc1283d23d458fcde5e94b96a9f888f221a49b7
Vincent Zhang <seagle0128@gmail.com>
2020-06-20 00:21:43 +0800

    Disable lsp-auto-guess-root.

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -66,7 +66,6 @@
      (setq read-process-output-max (* 1024 1024)) ;; 1MB
 
      (setq lsp-keymap-prefix "C-c l"
-           lsp-auto-guess-root t
            lsp-keep-workspace-alive nil
            lsp-prefer-capf t
            lsp-signature-auto-activate nil
