315b7b64c3f34b420ab4625d55ed94fb2023d099
Chetan Koneru <chetan.koneru@optum.com>
2020-08-14 13:16:14 -0500

    fixed font ligatures
    if emacs version<=27.0 we need to define the char sets to apply font-ligatures
    but with emacs>=27.1, HarfBuzz support is added and emacs can support
    font ligatures from out of the box.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -401,7 +401,7 @@
           . (lambda () (setq-local composition-function-table composition-ligature-table))))
   :config
   ;; support ligatures, some toned down to prevent hang
-  (unless emacs/>=27p
+  (when (version<= "27.0" emacs-version)
     (let ((alist
            '((33 . ".\\(?:\\(==\\|[!=]\\)[!=]?\\)")
              (35 . ".\\(?:\\(###?\\|_(\\|[(:=?[_{]\\)[#(:=?[_{]?\\)")
