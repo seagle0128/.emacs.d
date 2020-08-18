6f54478ab67512a3b895de27e60b184a18cca11b
Vincent Zhang <seagle0128@gmail.com>
2020-08-18 16:10:58 +0800

    Update keybindings of default-text-scale.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -373,6 +373,9 @@
 (use-package default-text-scale
   :hook (after-init . default-text-scale-mode)
   :bind (:map default-text-scale-mode-map
+         ("s-=" . default-text-scale-increase)
+         ("s--" . default-text-scale-decrease)
+         ("s-0" . default-text-scale-reset)
          ("C-s-=" . default-text-scale-increase)
          ("C-s--" . default-text-scale-decrease)
          ("C-s-0" . default-text-scale-reset)))
