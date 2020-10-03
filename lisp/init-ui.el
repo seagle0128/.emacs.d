cb468c74965571f2f280dcad0391d4fa819c6845
Vincent Zhang <seagle0128@gmail.com>
2020-10-03 13:20:17 +0800

    Update bongo icons.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -267,9 +267,9 @@
   (add-to-list 'all-the-icons-mode-icon-alist
                '(xwidget-webkit-mode all-the-icons-faicon "chrome" :v-adjust -0.1 :face all-the-icons-blue))
   (add-to-list 'all-the-icons-mode-icon-alist
-               '(bongo-playlist-mode all-the-icons-material "playlist_play" :height 1.2 :v-adjust -0.2 :face 'all-the-icons-green))
+               '(bongo-playlist-mode all-the-icons-material "queue_music" :height 1.2 :face 'all-the-icons-green))
   (add-to-list 'all-the-icons-mode-icon-alist
-               '(bongo-library-mode all-the-icons-material "library_music" :height 1.1 :v-adjust -0.2 :face 'all-the-icons-dgreen))
+               '(bongo-library-mode all-the-icons-material "library_music" :height 1.1 :face 'all-the-icons-green))
   (add-to-list 'all-the-icons-mode-icon-alist
                '(gnus-group-mode all-the-icons-fileicon "gnu" :face 'all-the-icons-silver))
   (add-to-list 'all-the-icons-mode-icon-alist
