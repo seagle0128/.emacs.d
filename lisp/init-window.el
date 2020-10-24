822a86a6dea66422a4d92c999422df254e4bbc67
Vincent Zhang <seagle0128@gmail.com>
2020-10-25 04:31:19 +0800

    Improve faces for ace-widnow.

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -85,6 +85,7 @@
      ("T" centaur-load-theme "theme"))))
   :custom-face
   (aw-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 3.0))))
+  (aw-minibuffer-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 2.0))))
   (aw-mode-line-face ((t (:inherit mode-line-emphasis :bold t))))
   :bind (([remap other-window] . ace-window)
          ("C-c w" . ace-window-hydra/body))
