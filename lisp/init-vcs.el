2189dec4a63b6377b1511a7b43740c84cf309f36
Vincent Zhang <seagle0128@gmail.com>
2020-10-30 12:00:55 +0800

    [Enhancement] Only kill magit buffers while exiting magit-status.

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -54,15 +54,16 @@
     (defun my-magit-kill-buffers (&rest _)
       "Restore window configuration and kill all Magit buffers."
       (interactive)
+      (magit-restore-window-configuration)
       (let ((buffers (magit-mode-get-buffers)))
-        (magit-restore-window-configuration)
-        (mapc (lambda (buf)
-                (with-current-buffer buf
-                  (if (and magit-this-process
-                           (eq (process-status magit-this-process) 'run))
-                      (bury-buffer buf)
-                    (kill-buffer buf))))
-              buffers)))
+        (when (eq major-mode 'magit-status-mode)
+          (mapc (lambda (buf)
+                  (with-current-buffer buf
+                    (if (and magit-this-process
+                             (eq (process-status magit-this-process) 'run))
+                        (bury-buffer buf)
+                      (kill-buffer buf))))
+                buffers))))
     (setq magit-bury-buffer-function #'my-magit-kill-buffers))
 
   ;; Access Git forges from Magit
