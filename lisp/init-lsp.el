8e010ab809818f51b5ae703cbc755175fa8e1d95
Vincent Zhang <seagle0128@gmail.com>
2020-12-29 22:51:53 +0800

    Prettify lsp-headerline faces.

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -43,13 +43,40 @@
    ;; Emacs client for the Language Server Protocol
    ;; https://github.com/emacs-lsp/lsp-mode#supported-languages
    (use-package lsp-mode
+     :diminish
      :defines (lsp-clients-python-library-directories
                lsp-rust-server)
      :commands (lsp-enable-which-key-integration
                 lsp-format-buffer
                 lsp-organize-imports
                 lsp-install-server)
-     :diminish
+     :custom-face
+     (lsp-headerline-breadcrumb-path-error-face
+      ((t :underline (:style line :color ,(face-foreground 'error))
+          :inherit lsp-headerline-breadcrumb-path-face)))
+     (lsp-headerline-breadcrumb-path-warning-face
+      ((t :underline (:style line :color ,(face-foreground 'warning))
+          :inherit lsp-headerline-breadcrumb-path-face)))
+     (lsp-headerline-breadcrumb-path-info-face
+      ((t :underline (:style line :color ,(face-foreground 'success))
+          :inherit lsp-headerline-breadcrumb-path-face)))
+     (lsp-headerline-breadcrumb-path-hint-face
+      ((t :underline (:style line :color ,(face-foreground 'success))
+          :inherit lsp-headerline-breadcrumb-path-face)))
+
+     (lsp-headerline-breadcrumb-symbols-error-face
+      ((t :inherit lsp-headerline-breadcrumb-symbols-face
+          :underline (:style line :color ,(face-foreground 'error)))))
+     (lsp-headerline-breadcrumb-symbols-warning-face
+      ((t :inherit lsp-headerline-breadcrumb-symbols-face
+          :underline (:style line :color ,(face-foreground 'warning)))))
+     (lsp-headerline-breadcrumb-symbols-info-face
+      ((t :inherit lsp-headerline-breadcrumb-symbols-face
+          :underline (:style line :color ,(face-foreground 'success)))))
+     (lsp-headerline-breadcrumb-symbols-hint-face
+      ((t :inherit lsp-headerline-breadcrumb-symbols-face
+          :underline (:style line :color ,(face-foreground 'success)))))
+
      :hook ((prog-mode . (lambda ()
                            (unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode)
                              (lsp-deferred))))
@@ -78,7 +105,6 @@
 
            lsp-enable-file-watchers nil
            lsp-enable-folding nil
-           lsp-enable-semantic-highlighting nil
            lsp-enable-symbol-highlighting nil
            lsp-enable-text-document-color nil
 
