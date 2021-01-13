6e502a6b84651c4baa56501d515ada280618c68f
Vincent Zhang <seagle0128@gmail.com>
2021-01-14 02:54:40 +0800

    Remove unused variables.

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -84,8 +84,7 @@
 (when (or sys/mac-x-p sys/linux-x-p)
   (use-package exec-path-from-shell
     :init
-    (setq exec-path-from-shell-check-startup-files nil
-          exec-path-from-shell-variables '("PATH" "MANPATH")
+    (setq exec-path-from-shell-variables '("PATH" "MANPATH")
           exec-path-from-shell-arguments '("-l"))
     (exec-path-from-shell-initialize)))
 
