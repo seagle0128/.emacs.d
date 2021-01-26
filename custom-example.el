89e9953d63a692f285906f2168974790d2ed5def
Vincent Zhang <seagle0128@gmail.com>
2021-01-27 02:41:01 +0800

    Fix fonts in Windows.

--- custom-example.el
+++ custom-example.el
@@ -39,7 +39,7 @@
                                                     (t 100))))
 
   ;; Specify font for all unicode characters
-  (cl-loop for font in '("Apple Color Emoji" "Segoe UI Emoji" "Symbola" "Symbol")
+  (cl-loop for font in '("Apple Color Emoji" "Segoe UI Symbol" "Symbola" "Symbol")
            when (font-installed-p font)
            return(set-fontset-font t 'unicode font nil 'prepend))
 
