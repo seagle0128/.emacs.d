dfee58bb11a5d4bed70369b4afec418686b9d34b
Vincent Zhang <seagle0128@gmail.com>
2021-03-19 00:31:06 +0800

    Display transient in child frame.

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -31,6 +31,7 @@
 ;;; Code:
 
 (require 'init-const)
+(require 'init-custom)
 
 ;; Git
 ;; See `magit-maybe-define-global-key-bindings'
@@ -78,6 +79,34 @@
       :init
       (setq magit-todos-nice (if (executable-find "nice") t nil))
       (magit-todos-mode 1))))
+
+;; Display transient in child frame
+(when (and (eq centaur-completion-style 'childframe)
+           (childframe-workable-p))
+  (use-package transient-posframe
+    :diminish
+    :custom-face
+    (transient-posframe-border ((t (:background ,(face-foreground 'font-lock-comment-face)))))
+    :hook (after-init . transient-posframe-mode)
+    :init
+    (setq transient-posframe-border-width 3
+          transient-posframe-min-height 21
+          transient-posframe-min-width 80
+          transient-posframe-poshandler #'ivy-poshandler-frame-center-near-bottom-fn)
+
+    (with-eval-after-load 'solaire-mode
+      (setq transient-posframe-parameters
+            `((background-color . ,(face-background 'solaire-default-face)))))
+    :config
+    (add-hook 'after-load-theme-hook
+              (lambda ()
+                (posframe-delete-all)
+                (custom-set-faces
+                 `(transient-posframe-border
+                   ((t (:background ,(face-foreground 'font-lock-comment-face))))))
+                (with-eval-after-load 'solaire-mode
+                  (setf (alist-get 'background-color transient-posframe-parameters)
+                        (face-background 'solaire-default-face)))))))
 
 ;; Walk through git revisions of a file
 (use-package git-timemachine
