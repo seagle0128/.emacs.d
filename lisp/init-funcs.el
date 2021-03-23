e28917871b1ac9ec263c7ebd819878216484a6a1
Vincent Zhang <seagle0128@gmail.com>
2021-03-24 00:09:45 +0800

    Add centaur-find-pdf-file.

--- lisp/init-funcs.el
+++ lisp/init-funcs.el
@@ -145,6 +145,16 @@ NEW-SESSION specifies whether to create a new xwidget-webkit session."
             (pop-to-buffer buf)
           (switch-to-buffer buf))))))
 
+(defun centaur-find-pdf-file (&optional url)
+  "Find a PDF file via URL and render by `pdf.js'."
+  (interactive "f")
+  (cond ((featurep 'xwidget-internal)
+         (xwidget-webkit-browse-url (concat "file://" url)))
+        ((bound-and-true-p counsel-mode)
+         (counsel-find-file url))
+        (t (find-file url))))
+(defalias #'find-pdf-file #'centaur-find-pdf-file)
+
 ;; Mode line
 (defun mode-line-height ()
   "Get the height of the mode-line."
