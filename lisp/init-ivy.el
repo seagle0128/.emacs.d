10f29d0366ad7c4a178c02d068f204f1afe11962
Vincent Zhang <seagle0128@gmail.com>
2021-05-05 16:42:00 +0800

    Improve ivy-posframe.

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -602,7 +602,8 @@ This is for use in `ivy-re-builders-alist'."
     (ivy-posframe-border ((t (:background ,(face-foreground 'font-lock-comment-face)))))
     :hook (ivy-mode . ivy-posframe-mode)
     :init
-    (setq ivy-posframe-border-width 3
+    (setq ivy-height 15
+          ivy-posframe-border-width 3
           ivy-posframe-parameters
           `((background-color . ,(face-background 'tooltip))))
 
@@ -621,17 +622,17 @@ This is for use in `ivy-re-builders-alist'."
                       (face-background 'tooltip))))
 
     (with-no-warnings
-      (defun ivy-display-at-frame-center-near-bottom-fn (str)
-        (ivy-posframe--display str #'ivy-poshandler-frame-center-near-bottom-fn))
+      (defun ivy-posframe-display-at-frame-center-near-bottom-fn (str)
+        (ivy-posframe--display str #'posframe-poshandler-frame-center-near-bottom-fn))
 
-      (defun ivy-poshandler-frame-center-near-bottom-fn (info)
+      (defun posframe-poshandler-frame-center-near-bottom-fn (info)
         (let ((parent-frame (plist-get info :parent-frame))
               (pos (posframe-poshandler-frame-center info)))
           (cons (car pos)
                 (truncate (/ (frame-pixel-height parent-frame) 2)))))
 
       (setf (alist-get t ivy-posframe-display-functions-alist)
-            #'ivy-display-at-frame-center-near-bottom-fn))))
+            #'ivy-posframe-display-at-frame-center-near-bottom-fn))))
 
 (provide 'init-ivy)
 
