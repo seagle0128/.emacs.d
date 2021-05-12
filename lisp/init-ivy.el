5d7ee68f6321676d9ee387f363a030c08c410e2f
Vincent Zhang <seagle0128@gmail.com>
2021-05-13 01:31:59 +0800

    Rename functions.

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -594,7 +594,7 @@ This is for use in `ivy-re-builders-alist'."
                       (face-background 'tooltip))))
 
     (with-no-warnings
-      (defun ivy-posframe-display-at-frame-center-near-bottom-fn (str)
+      (defun ivy-posframe-display-at-frame-center-near-bottom (str)
         (ivy-posframe--display str #'posframe-poshandler-frame-center-near-bottom-fn))
 
       (defun posframe-poshandler-frame-center-near-bottom-fn (info)
@@ -604,7 +604,7 @@ This is for use in `ivy-re-builders-alist'."
                 (truncate (/ (frame-pixel-height parent-frame) 2)))))
 
       (setf (alist-get t ivy-posframe-display-functions-alist)
-            #'ivy-posframe-display-at-frame-center-near-bottom-fn))))
+            #'ivy-posframe-display-at-frame-center-near-bottom))))
 
 (provide 'init-ivy)
 
