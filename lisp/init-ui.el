f1cce53b742fc15d5a9e6272920be853df3075c4
Vincent Zhang <seagle0128@gmail.com>
2021-05-30 22:10:47 +0800

    Fix icon face's issues.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -270,17 +270,17 @@
   (add-to-list 'all-the-icons-mode-icon-alist
                '(xwidget-webkit-mode all-the-icons-faicon "chrome" :v-adjust -0.1 :face all-the-icons-blue))
   (add-to-list 'all-the-icons-mode-icon-alist
-               '(bongo-playlist-mode all-the-icons-material "queue_music" :height 1.2 :face 'all-the-icons-green))
+               '(bongo-playlist-mode all-the-icons-material "queue_music" :height 1.2 :face all-the-icons-green))
   (add-to-list 'all-the-icons-mode-icon-alist
-               '(bongo-library-mode all-the-icons-material "library_music" :height 1.1 :face 'all-the-icons-green))
+               '(bongo-library-mode all-the-icons-material "library_music" :height 1.1 :face all-the-icons-green))
   (add-to-list 'all-the-icons-mode-icon-alist
-               '(gnus-group-mode all-the-icons-fileicon "gnu" :face 'all-the-icons-silver))
+               '(gnus-group-mode all-the-icons-fileicon "gnu" :face all-the-icons-silver))
   (add-to-list 'all-the-icons-mode-icon-alist
-               '(gnus-summary-mode all-the-icons-octicon "inbox" :height 1.0 :v-adjust 0.0 :face 'all-the-icons-orange))
+               '(gnus-summary-mode all-the-icons-octicon "inbox" :height 1.0 :v-adjust 0.0 :face all-the-icons-orange))
   (add-to-list 'all-the-icons-mode-icon-alist
-               '(gnus-article-mode all-the-icons-octicon "mail" :height 1.1 :v-adjust 0.0 :face 'all-the-icons-lblue))
+               '(gnus-article-mode all-the-icons-octicon "mail" :height 1.1 :v-adjust 0.0 :face all-the-icons-lblue))
   (add-to-list 'all-the-icons-mode-icon-alist
-               '(message-mode all-the-icons-octicon "mail" :height 1.1 :v-adjust 0.0 :face 'all-the-icons-lblue))
+               '(message-mode all-the-icons-octicon "mail" :height 1.1 :v-adjust 0.0 :face all-the-icons-lblue))
   (add-to-list 'all-the-icons-mode-icon-alist
                '(diff-mode all-the-icons-octicon "git-compare" :v-adjust 0.0 :face all-the-icons-lred))
   (add-to-list 'all-the-icons-mode-icon-alist
