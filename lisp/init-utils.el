deaad6f6a6b9ba9c7e6c7651927f8c07c4dcbac5
Vincent Zhang <seagle0128@gmail.com>
2021-07-04 03:44:08 +0800

    Use devdocs instead of devdocs-browse.

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -362,10 +362,43 @@ of the buffer text to be displayed in the popup"
 
 ;; Browse devdocs.io documents using EWW
 (when emacs/>=27p
-  (use-package devdocs-browser
-    :diminish devdocs-browser-eww-mode
+  (use-package devdocs
+    :commands devdocs--installed-p
     :bind (:map prog-mode-map
-           ("<f1>" . devdocs-browser-open))))
+           ("<f1>" . devdocs-lookup+))
+    :init
+    (defvar devdocs-major-mode-docs-alist
+      '((c-mode . ("C"))
+        (c++-mode . ("C++"))
+        (python-mode . ("Python 3.9" "Python 3.8"))
+        (ruby-mode . ("Ruby 3"))
+        (go-mode . ("Go"))
+        (rust-mode . ("Rust"))
+        (css-mode . ("CSS"))
+        (html-mode . ("HTML"))
+        (js-mode . ("JavaScript" "JQuery"))
+        (js2-mode . ("JavaScript" "JQuery"))
+        (emacs-lisp-mode . ("Elisp")))
+      "Alist of MAJOR-MODE and list of docset names.")
+
+    (mapc
+     (lambda (e)
+       (add-hook (intern (format "%s-hook" (car e)))
+                 (lambda ()
+                   (setq-local devdocs-current-docs (cdr e)))))
+     devdocs-major-mode-docs-alist)
+
+    (defun devdocs-lookup+()
+      "Look up a DevDocs documentation entry.
+
+Install the doc if it is not installed."
+      (interactive)
+      ;; (mapc
+      ;;  (lambda (doc)
+      ;;    (unless (devdocs--installed-p doc)
+      ;;      (devdocs-install doc)))
+      ;;  (alist-get major-mode devdocs-major-mode-docs-alist))
+      (devdocs-lookup nil (symbol-name (symbol-at-point))))))
 
 ;; A stackoverflow and its sisters' sites reader
 (when emacs/>=26p
