9282025a9a486673c13f41146d2612e46d789e7b
Vincent Zhang <seagle0128@gmail.com>
2021-07-14 03:58:36 +0800

    Optimize loading performance.
    Put all env vars in .zshenv or .profile.

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -84,7 +84,8 @@
 (when (or sys/mac-x-p sys/linux-x-p (daemonp))
   (use-package exec-path-from-shell
     :init
-    (setq exec-path-from-shell-variables '("PATH" "MANPATH"))
+    (setq exec-path-from-shell-variables '("PATH" "MANPATH")
+          exec-path-from-shell-arguments '("-l"))
     (exec-path-from-shell-initialize)))
 
 ;; Start server
