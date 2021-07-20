3c75bf0bdd968a351f84939d44fc32af60a890a2
Vincent Zhang <seagle0128@gmail.com>
2021-07-21 03:14:02 +0800

    Disable hud in mode-line.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -90,7 +90,7 @@
   :custom
   (doom-modeline-icon centaur-icon)
   (doom-modeline-bar nil)
-  (doom-modeline-hud t)
+  (doom-modeline-hud nil)
   (doom-modeline-minor-modes t)
   (doom-modeline-unicode-fallback t)
   (doom-modeline-mu4e nil)
