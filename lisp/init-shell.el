d47964dd9a6019ab62f6fb6f2293754db6f3b38d
Vincent Zhang <seagle0128@gmail.com>
2021-09-14 15:14:44 +0800

    Refactor.

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -133,9 +133,7 @@
         (defun vterm-posframe-toggle ()
           "Toggle `vterm' child frame."
           (interactive)
-          (let ((buffer (vterm--internal #'ignore 100))
-                (width  (max 80 (/ (frame-width) 2)))
-                (height (/ (frame-height) 2)))
+          (let ((buffer (vterm--internal #'ignore 100)))
             (if (and vterm-posframe--frame
                      (frame-live-p vterm-posframe--frame)
                      (frame-visible-p vterm-posframe--frame))
@@ -143,23 +141,25 @@
                   (posframe-hide buffer)
                   ;; Focus the parent frame
                   (select-frame-set-input-focus (frame-parent vterm-posframe--frame)))
-              (setq vterm-posframe--frame
-                    (posframe-show
-                     buffer
-                     :poshandler #'posframe-poshandler-frame-center
-                     :hidehandler #'vterm-posframe-hidehandler
-                     :left-fringe 8
-                     :right-fringe 8
-                     :width width
-                     :height height
-                     :min-width width
-                     :min-height height
-                     :internal-border-width 3
-                     :internal-border-color (face-foreground 'font-lock-comment-face nil t)
-                     :background-color (face-background 'tooltip nil t)
-                     :accept-focus t))
-              ;; Focus the child frame
-              (select-frame-set-input-focus vterm-posframe--frame))))
+              (let ((width  (max 80 (/ (frame-width) 2)))
+                    (height (/ (frame-height) 2)))
+                (setq vterm-posframe--frame
+                      (posframe-show
+                       buffer
+                       :poshandler #'posframe-poshandler-frame-center
+                       :hidehandler #'vterm-posframe-hidehandler
+                       :left-fringe 8
+                       :right-fringe 8
+                       :width width
+                       :height height
+                       :min-width width
+                       :min-height height
+                       :internal-border-width 3
+                       :internal-border-color (face-foreground 'font-lock-comment-face nil t)
+                       :background-color (face-background 'tooltip nil t)
+                       :accept-focus t))
+                ;; Focus the child frame
+                (select-frame-set-input-focus vterm-posframe--frame)))))
         (bind-key "C-`" #'vterm-posframe-toggle)))))
 
 ;; Shell Pop
