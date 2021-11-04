e3eed361dba3e4fc119ec71e9883593482e49679
jidibinlin <2694273649@qq.com>
2021-11-04 12:45:54 +0800

    fix init-vcs.el posframe--set-frame-size parameter type

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -118,7 +118,7 @@
             (goto-char (point-max))
             (delete-char -3)          ; delete separate
             (insert (propertize "\n" 'face '(:height 0.5)))
-            (posframe--set-frame-size posframe--frame nil nil nil nil nil nil))))
+            (posframe--set-frame-size '(posframe--frame nil nil nil nil nil nil)))))
       (advice-add #'transient--show :after #'my-transient-posframe--prettify-frame))))
 
 ;; Walk through git revisions of a file
