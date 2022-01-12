4b2498e791afcce5f98a79483cd0fee891f95b87
7ym0n.q6e <7ym0n@users.noreply.github.com>
2022-01-12 13:10:38 +0800

    add quelpa install package.

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -102,6 +102,16 @@
 (eval-when-compile
   (require 'use-package))
 
+;; Use quelpa install packages
+(use-package quelpa
+  :init
+  (setq quelpa-upgrade-p dotfairy-quelpa-upgrade
+        quelpa-dir (expand-file-name "quelpa" dotfairy-local-dir)))
+
+(use-package quelpa-use-package)
+(eval-when-compile
+  (require 'quelpa-use-package))
+
 ;; Required by `use-package'
 (use-package diminish)
 (use-package bind-key)
@@ -132,7 +142,11 @@
   :init
   (setq auto-package-update-delete-old-versions t
         auto-package-update-hide-results t)
-  (defalias 'upgrade-packages #'auto-package-update-now))
+  (defun my-upgrade-packages (&optional async)
+    (interactive)
+    (auto-package-update-now async)
+    (quelpa-upgrade-all))
+  (defalias 'upgrade-packages #'my-upgrade-packages))
 
 (provide 'init-package)
 
