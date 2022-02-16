1b1975fdff8aa38a9e3468371cff13684e54ec12
Vincent Zhang <seagle0128@gmail.com>
2022-02-17 01:12:07 +0800

    Make `package-autoremove' work with `use-package'.
    See https://github.com/Eason0210/dot-emacs/blob/ec6a253e68c5a304549ebfb03155547e559ebbda/init.el#L84-L109.

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -102,6 +102,32 @@
 (eval-when-compile
   (require 'use-package))
 
+;; package.el updates the saved version of package-selected-packages correctly only
+;; after custom-file has been loaded, which is a bug. We work around this by adding
+;; the required packages to package-selected-packages after startup is complete.
+;; Make `package-autoremove' work with `use-package'
+
+(defvar use-package-selected-packages '(use-package)
+  "Packages pulled in by use-package.")
+
+(eval-and-compile
+  (define-advice use-package-handler/:ensure (:around (fn name-symbol keyword args rest state) select)
+    (let ((items (funcall fn name-symbol keyword args rest state)))
+      (dolist (ensure args items)
+        (let ((package
+               (or (and (eq ensure t) (use-package-as-symbol name-symbol))
+                   ensure)))
+          (when package
+            (when (consp package)
+              (setq package (car package)))
+            (push `(add-to-list 'use-package-selected-packages ',package) items)))))))
+
+(when (fboundp 'package--save-selected-packages)
+  (add-hook 'after-init-hook
+            (lambda ()
+              (package--save-selected-packages
+               (seq-uniq (append use-package-selected-packages package-selected-packages))))))
+
 ;; Required by `use-package'
 (use-package diminish)
 (use-package bind-key)
