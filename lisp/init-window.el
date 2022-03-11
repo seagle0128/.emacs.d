f61a689bf1d197dc7c3e06a8cfe77edc13b84e3c
Vincent Zhang <seagle0128@gmail.com>
2022-03-11 14:21:45 +0800

    Improve popper window height.

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -161,6 +161,7 @@
           "\\*Apropos\\*"
           "\\*Backtrace\\*"
           "\\*Calendar\\*"
+          "\\*Finder\\*"
 
           bookmark-bmenu-mode
           comint-mode
@@ -212,7 +213,16 @@
   (setq popper-echo-dispatch-actions t)
   :config
   (popper-echo-mode 1)
+
   (with-no-warnings
+    (defun my-popper-fit-window-height (win)
+      "Determine the height of popup window WIN by fitting it to the buffer's content."
+      (fit-window-to-buffer
+       win
+       (floor (frame-height) 3)
+       (floor (frame-height) 3)))
+    (setq popper-window-height #'my-popper-fit-window-height)
+
     (defun popper-close-window-hack (&rest _)
       "Close popper window via `C-g'."
       ;; `C-g' can deactivate region
