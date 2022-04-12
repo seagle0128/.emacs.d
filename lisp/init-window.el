85d7838a44759ee0ad05ca5e1837b74cc854a8f0
Vincent Zhang <seagle0128@gmail.com>
2022-04-12 22:38:18 +0800

    Improve popper.

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -199,6 +199,7 @@
             "^\\*elfeed-entry\\*$"
             "^\\*macro expansion\\**"
 
+            "\\*Agenda Commands\\*" "\\*Org Agenda.*\\*"
             "\\*Org Select\\*" "\\*Capture\\*" "^CAPTURE-.*\\.org*"
             "\\*Gofmt Errors\\*$" "\\*Go Test\\*$" godoc-mode
             "\\*docker-containers\\*" "\\*docker-images\\*" "\\*docker-networks\\*" "\\*docker-volumes\\*"
