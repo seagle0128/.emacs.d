66d80e930ee2f292ca0fed3d55abe16fb2698100
Vincent Zhang <seagle0128@gmail.com>
2022-04-26 16:16:28 +0800

    Remove origami.
    Close #351.

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -358,30 +358,6 @@
   :bind (:map hs-minor-mode-map
          ("C-`" . hs-toggle-hiding)))
 
-;; Flexible text folding
-(use-package origami
-  :pretty-hydra
-  ((:title (pretty-hydra-title "Origami" 'octicon "fold" :height 1.1 :v-adjust -0.05)
-    :color amaranth :quit-key "q")
-   ("Node"
-    ((";" origami-recursively-toggle-node "toggle recursively")
-     ("a" origami-toggle-all-nodes "toggle all")
-     ("t" origami-toggle-node "toggle current")
-     ("o" origami-open-node "open current")
-     ("c" origami-close-node "close current")
-     ("s" origami-show-only-node "only show current"))
-    "Actions"
-    (("u" origami-undo "undo")
-     ("d" origami-redo "redo")
-     ("r" origami-reset "reset")
-     ("n" origami-next-fold "next fold")
-     ("p" origami-previous-fold "previous fold"))))
-  :bind (:map origami-mode-map
-         ("C-~" . origami-hydra/body))
-  :hook (prog-mode . origami-mode)
-  :init (setq origami-show-fold-header t)
-  :config (face-spec-reset-face 'origami-fold-header-face))
-
 ;; Open files as another user
 (unless sys/win32p
   (use-package sudo-edit))
