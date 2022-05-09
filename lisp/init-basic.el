11eed9177455839cc6d92551b813d516f6e9c848
Vincent Zhang <seagle0128@gmail.com>
2022-05-10 01:52:10 +0800

    Simplify UTF-8 coding system.\.

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -92,17 +92,6 @@
 ;; Explicitly set the prefered coding systems to avoid annoying prompt
 ;; from emacs (especially on Microsoft Windows)
 (prefer-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8)
-
-(set-language-environment 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-buffer-file-coding-system 'utf-8)
-(set-clipboard-coding-system 'utf-8)
-(set-file-name-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(modify-coding-system-alist 'process "*" 'utf-8)
 
 ;; Environment
 (when (or sys/mac-x-p sys/linux-x-p (daemonp))
