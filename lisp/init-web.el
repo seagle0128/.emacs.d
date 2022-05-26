c85733dee7d1c4f3e2a156d15137bdb4c57cf999
Vincent Zhang <seagle0128@gmail.com>
2022-05-27 00:30:41 +0800

    Fix #364: jsp file not opened in web-mode.

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -152,7 +152,7 @@
 
 ;; Major mode for editing web templates
 (use-package web-mode
-  :mode "\\.\\(phtml\\|php|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\|vue\\)$"
+  :mode "\\.\\(phtml\\|php\\|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\|vue\\)$"
   :config
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
