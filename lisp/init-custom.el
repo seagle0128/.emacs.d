baabb4e2d94104643064c2d9ce12f12c7dddf568
Vincent Zhang <seagle0128@gmail.com>
2022-06-16 10:41:04 +0800

    Fix nongnu url.

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -81,7 +81,7 @@
 ;; @see https://github.com/melpa/melpa and https://elpa.emacs-china.org/.
 (defcustom centaur-package-archives-alist
   '((melpa    . (("gnu"    . "http://elpa.gnu.org/packages/")
-                 ("nongnu" . "http://elpa.nongnu.org/packages/")
+                 ("nongnu" . "http://elpa.nongnu.org/nongnu/")
                  ("melpa"  . "http://melpa.org/packages/")))
     (emacs-cn . (("gnu"    . "http://elpa.emacs-china.org/gnu/")
                  ("nongnu" . "http://elpa.emacs-china.org/nongnu/")
