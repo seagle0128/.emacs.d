a6998f2401b186bb574f4c3d2701e5dc06c00f92
Vincent Zhang <seagle0128@gmail.com>
2022-06-20 00:18:06 +0800

    Update fonts.

--- custom-example.el
+++ custom-example.el
@@ -34,8 +34,9 @@
   "Setup fonts."
   (when (display-graphic-p)
     ;; Set default font
-    (cl-loop for font in '("SF Mono" "Hack" "Source Code Pro" "Fira Code"
-                           "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
+    (cl-loop for font in '("Cascadia Code" "Fira Code" "Jetbrains Mono"
+                           "SF Mono" "Hack" "Source Code Pro" "Menlo"
+                           "Monaco" "DejaVu Sans Mono" "Consolas")
              when (font-installed-p font)
              return (set-face-attribute 'default nil
                                         :font font
