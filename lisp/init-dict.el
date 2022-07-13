91cf65b07a3f9b3ebd0348ddbea3a913d2db883d
Vincent Zhang <seagle0128@gmail.com>
2022-07-14 00:06:48 +0800

    [Refactor] Remove workarounds of pdf-tools.

--- lisp/init-dict.el
+++ lisp/init-dict.el
@@ -38,8 +38,7 @@
     :bind (("C-c d f" . fanyi-dwim)
            ("C-c d d" . fanyi-dwim2)
            ("C-c d h" . fanyi-from-history))
-    :custom (fanyi-providers '(fanyi-haici-provider
-                               fanyi-longman-provider)))
+    :custom (fanyi-providers '(fanyi-haici-provider fanyi-longman-provider)))
 
   (use-package go-translate
     :bind (("C-c d g" . gts-do-translate))
--- lisp/init-reader.el
+++ lisp/init-reader.el
@@ -49,79 +49,10 @@
     :magic ("%PDF" . pdf-view-mode)
     :bind (:map pdf-view-mode-map
            ("C-s" . isearch-forward))
-    :init
-    (setq pdf-view-use-scaling t
-          pdf-view-use-imagemagick nil
-          pdf-annot-activate-created-annotations t)
-    :config
-    ;; Activate the package
-    (pdf-tools-install t nil t nil)
-
-    (with-no-warnings
-      ;; Highlight matches
-      (defun my-pdf-isearch-hl-matches (current matches &optional occur-hack-p)
-        "Highlighting edges CURRENT and MATCHES."
-        (cl-destructuring-bind (fg1 bg1 fg2 bg2)
-            (pdf-isearch-current-colors)
-          (let* ((width (car (pdf-view-image-size)))
-                 (page (pdf-view-current-page))
-                 (window (selected-window))
-                 (buffer (current-buffer))
-                 (tick (cl-incf pdf-isearch--hl-matches-tick))
-                 (pdf-info-asynchronous
-                  (lambda (status data)
-                    (when (and (null status)
-                               (eq tick pdf-isearch--hl-matches-tick)
-                               (buffer-live-p buffer)
-                               (window-live-p window)
-                               (eq (window-buffer window)
-                                   buffer))
-                      (with-selected-window window
-                        (when (and (derived-mode-p 'pdf-view-mode)
-                                   (or isearch-mode
-                                       occur-hack-p)
-                                   (eq page (pdf-view-current-page)))
-                          (pdf-view-display-image
-                           (pdf-view-create-image data :width width))))))))
-            (pdf-info-renderpage-text-regions
-             page width t nil
-             `(,fg1 ,bg1 ,@(pdf-util-scale-pixel-to-relative
-                            current))
-             `(,fg2 ,bg2 ,@(pdf-util-scale-pixel-to-relative
-                            (apply 'append
-                                   (remove current matches))))))))
-      (advice-add #'pdf-isearch-hl-matches :override #'my-pdf-isearch-hl-matches)
-
-      ;; Show annotation
-      (defun my-pdf-annot-show-annotation (a &optional highlight-p window)
-        "Make annotation A visible."
-        (save-selected-window
-          (when window (select-window window 'norecord))
-          (pdf-util-assert-pdf-window)
-          (let ((page (pdf-annot-get a 'page))
-                (size (pdf-view-image-size)))
-            (unless (= page (pdf-view-current-page))
-              (pdf-view-goto-page page))
-            (let ((edges (pdf-annot-get-display-edges a)))
-              (when highlight-p
-                (pdf-view-display-image
-                 (pdf-view-create-image
-                     (pdf-cache-renderpage-highlight
-                      page (car size)
-                      `("white" "steel blue" 0.35 ,@edges))
-                   :map (pdf-view-apply-hotspot-functions
-                         window page size)
-                   :width (car size))))
-              (pdf-util-scroll-to-edges
-               (pdf-util-scale-relative-to-pixel (car edges)))))))
-      (advice-add #'pdf-annot-show-annotation :override #'my-pdf-annot-show-annotation))
-
-    ;; Recover last viewed position
-    (use-package saveplace-pdf-view
-      :config
-      (with-no-warnings
-        (advice-add 'save-place-find-file-hook :around #'saveplace-pdf-view-find-file-advice)
-        (advice-add 'save-place-to-alist :around #'saveplace-pdf-view-to-alist-advice)))))
+    :init (setq pdf-view-use-scaling t
+                pdf-view-use-imagemagick nil
+                pdf-annot-activate-created-annotations t)
+    :config (pdf-tools-install t nil t nil)))    ; Activate the package
 
 ;; Epub reader
 (use-package nov
