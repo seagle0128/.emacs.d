196fb6cf313d11a7e7e12c18ee61d6816b60fa90
zhangr <zhangr@qingcigame.com>
2022-08-05 21:23:59 +0800

    for lua function new line.

--- lisp/init-lua.el
+++ lisp/init-lua.el
@@ -33,68 +33,17 @@
 ;; lua Mode
 (use-package lua-mode)
 
-;; use an indentation width of two spaces
 (setq lua-indent-level 4)
 
-;; Add dangling '(', remove '='
-(setq lua-cont-eol-regexp
-      (eval-when-compile
-        (concat
-         "\\((\\|\\_<"
-         (regexp-opt '("and" "or" "not" "in" "for" "while"
-                       "local" "function") t)
-         "\\_>\\|"
-         "\\(^\\|[^" "]\\)"
-         (regexp-opt '("+" "-" "*" "/" "^" ".." "==" "<" ">" "<=" ">=" "~=") t)
-         "\\)"
-         "\\s *\\=")))
+(setq lua-indent-nested-block-content-align nil)
+(setq lua-indent-close-paren-align nil)
 
-(defun lua-calculate-indentation (&optional parse-start)
-  "Overwrites the default lua-mode function that calculates the
-column to which the current line should be indented to."
-  (save-excursion
-    (when parse-start
-      (goto-char parse-start))
+(defun lua-at-most-one-indent (old-function &rest arguments)
+  (let ((old-res (apply old-function arguments)))
+    (if (> old-res lua-indent-level) lua-indent-level old-res)))
 
-    ;; We calculate the indentation column depending on the previous
-    ;; non-blank, non-comment code line. Also, when the current line
-    ;; is a continuation of that previous line, we add one additional
-    ;; unit of indentation.
-    (+ (if (lua-is-continuing-statement-p) lua-indent-level 0)
-       (if (lua-goto-nonblank-previous-line)
-           (+ (current-indentation) (lua-calculate-indentation-right-shift-next))
-         0))))
-
-(defun lua-calculate-indentation-right-shift-next (&optional parse-start)
-  "Assuming that the next code line is not a block ending line,
-this function returns the column offset that line should be
-indented to with respect to the current line."
-  (let ((eol)
-        (token)
-        (token-info)
-        (shift 0))
-    (save-excursion
-      (when parse-start
-        (goto-char parse-start))
-
-      ;; count the balance of block-opening and block-closing tokens
-      ;; from the beginning to the end of this line.
-      (setq eol (line-end-position))
-      (beginning-of-line)
-      (while (and (lua-find-regexp 'forward lua-indentation-modifier-regexp)
-                  (<= (point) eol)
-                  (setq token (match-string 0))
-                  (setq token-info (assoc token lua-block-token-alist)))
-        ;; we found a token. Now, is it an opening or closing token?
-        (if (eq (nth 2 token-info) 'open)
-            (setq shift (+ shift lua-indent-level))
-          (when (or (> shift 0)
-                    (string= token ")"))
-            (setq shift (- shift lua-indent-level))))))
-    shift))
-
-(add-hook 'lua-mode-hook
-          (lambda () (unless (fboundp 'lua-calculate-indentation-right-shift-next))))
+(advice-add #'lua-calculate-indentation-block-modifier
+            :around #'lua-at-most-one-indent)
 
 (provide 'init-lua)
 
