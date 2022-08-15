dee554c90c00bffe83eb0980e7554123e9299a26
zhangr <zhangr@qingcigame.com>
2022-08-15 11:07:37 +0800

    config update.

--- init.el
+++ init.el
@@ -109,7 +109,7 @@ Otherwise the startup will be very slow. "
 ;; Preferences
 (require 'init-basic)
 (require 'init-hydra)
-(require 'init-pyim)
+;; (require 'init-pyim)
 (require 'init-orderless)
 
 (require 'init-ui)
--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -37,7 +37,7 @@
 ;; e.g. display “lambda” as “λ”
 (use-package prog-mode
   :ensure nil
-  :hook (prog-mode . prettify-symbols-mode)
+  ;; :hook (prog-mode . prettify-symbols-mode)
   :init
   (setq-default prettify-symbols-alist centaur-prettify-symbols-alist)
   (setq prettify-symbols-unprettify-at-point 'right-edge))
