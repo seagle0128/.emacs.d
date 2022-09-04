6a33850c6a500227e52ae1e8b0ea4dbad59d4faf
Vincent Zhang <seagle0128@gmail.com>
2022-09-05 03:09:19 +0800

    Update font configurations.

--- custom-example.el
+++ custom-example.el
@@ -66,9 +66,11 @@
                       (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend)))
 
     ;; Specify font for Chinese characters
-    (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
+    (cl-loop for font in '("WenQuanYi Micro Hei" "PingFang SC" "Microsoft Yahei" "STFangsong")
              when (font-installed-p font)
-             return (set-fontset-font t '(#x4e00 . #x9fff) font))))
+             return (progn
+                      (setq face-font-rescale-alist `((,font . 1.3)))
+                      (set-fontset-font t '(#x4e00 . #x9fff) (font-spec :family font))))))
 
 (centaur-setup-fonts)
 (add-hook 'window-setup-hook #'centaur-setup-fonts)
