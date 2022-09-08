c61e12fa8a790250ea769ba69d58b3e90fec9e25
Vincent Zhang <seagle0128@gmail.com>
2022-09-09 01:50:38 +0800

    Don't highlight face by the face itself in elisp buffers.

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -51,8 +51,7 @@
 
   ;; Syntax highlighting of known Elisp symbols
   (use-package highlight-defined
-    :hook ((emacs-lisp-mode inferior-emacs-lisp-mode) . highlight-defined-mode)
-    :init (setq highlight-defined-face-use-itself t))
+    :hook ((emacs-lisp-mode inferior-emacs-lisp-mode) . highlight-defined-mode))
 
   (with-no-warnings
     ;; Align indent keywords
