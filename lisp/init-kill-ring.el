6a0020e2ef4a3e3dac7f04650a0e593a19ada9e5
Vincent Zhang <seagle0128@gmail.com>
2022-09-10 23:53:04 +0800

    Enhance browse-kill-ring.

--- lisp/init-kill-ring.el
+++ lisp/init-kill-ring.el
@@ -43,7 +43,9 @@
 ;; Interactively insert and edit items from kill-ring
 (use-package browse-kill-ring
   :bind ("C-c k" . browse-kill-ring)
-  :hook (after-init . browse-kill-ring-default-keybindings))
+  :hook (after-init . browse-kill-ring-default-keybindings)
+  :init (setq browse-kill-ring-separator "────────────────"
+              browse-kill-ring-separator-face 'shadow))
 
 (provide 'init-kill-ring)
 
