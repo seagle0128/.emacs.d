7695a2ef1fa959e5efe5d05b659509d9ac0ccf67
Vincent Zhang <seagle0128@gmail.com>
2022-12-21 21:43:41 +0800

    Remove modern-cpp-font-lock.

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -37,13 +37,7 @@
   :ensure nil
   :bind (:map c-mode-base-map
          ("<f12>" . compile))
-  :init
-  (setq c-default-style "stroustrup")
-  (setq-default c-basic-offset 4)
-  :config
-  (use-package modern-cpp-font-lock
-    :diminish
-    :init (modern-c++-font-lock-global-mode t)))
+  :init (setq-default c-basic-offset 4))
 
 (use-package c-ts-mode
   :ensure nil
