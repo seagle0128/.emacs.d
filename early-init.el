248bb6e260290411b06d8c09c064ce9103b4954f
Vincent Zhang <seagle0128@gmail.com>
2023-01-03 10:35:40 +0800

    [29] use-package is builtin. use-package-enable-imenu-support must be set before loading it.

--- early-init.el
+++ early-init.el
@@ -44,6 +44,10 @@
 ;; initialization, so we must prevent Emacs from doing it early!
 (setq package-enable-at-startup nil)
 
+;; `use-package' is builtin since 29.
+;; It must be set before loading `use-package'.
+(setq use-package-enable-imenu-support t)
+
 ;; In noninteractive sessions, prioritize non-byte-compiled source files to
 ;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
 ;; to skip the mtime checks on every *.elc file.
