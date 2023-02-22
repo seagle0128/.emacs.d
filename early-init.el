4a24d0b89b5612c23e5afec4e153b3c1a0a5aee2
Vincent Zhang <seagle0128@gmail.com>
2023-02-22 23:52:12 +0800

    native-comp-deferred-compilation is obsolete since 29.1.

--- early-init.el
+++ early-init.el
@@ -37,7 +37,8 @@
 ;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
 ;; packages are compiled ahead-of-time when they are installed and site files
 ;; are compiled when gccemacs is installed.
-(setq native-comp-deferred-compilation nil)
+(setq native-comp-deferred-compilation nil ;; obsolete since 29.1
+      native-comp-jit-compilation nil)
 
 ;; Package initialize occurs automatically, before `user-init-file' is
 ;; loaded, but after `early-init-file'. We handle package
