4ffcedfbd6d65786fdcc938adbe549a49a7c1936
Vincent Zhang <seagle0128@gmail.com>
2023-03-06 23:40:13 +0800

    Update dired.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -39,6 +39,9 @@
   :bind (:map dired-mode-map
          ("C-c C-p" . wdired-change-to-wdired-mode))
   :config
+  ;; Guess a default target directory
+  (setq dired-dwim-target t)
+
   ;; Always delete and copy recursively
   (setq dired-recursive-deletes 'always
         dired-recursive-copies 'always)
