91dd2ecafd2bd955a697861c948a5219927dc0f9
Vincent Zhang <seagle0128@gmail.com>
2023-03-10 16:19:25 +0800

    Refactor.

--- init.el
+++ init.el
@@ -60,25 +60,25 @@
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum)
 
-;; Prevent flash of unstyled modeline at startup
+;; Prevent flashing of unstyled modeline at startup
 (setq-default mode-line-format nil)
-
-;; Prevent flash of messages at startup
-(unless (or (daemonp) noninteractive init-file-debug (not (display-graphic-p)))
-  (setq-default inhibit-redisplay t
-                inhibit-message t)
-  (defun reset-inhibit-vars ()
-    (setq-default inhibit-redisplay nil
-                  inhibit-message nil)
-    (redraw-frame))
-  (add-hook 'window-setup-hook #'reset-inhibit-vars)
-  (define-advice startup--load-user-init-file (:after (&rest _) reset-inhibit-vars)
-    (and init-file-had-error (reset-inhibit-vars))))
 
 ;; Don't pass case-insensitive to `auto-mode-alist'
 (setq auto-mode-case-fold nil)
 
 (unless (or (daemonp) noninteractive init-file-debug)
+  ;; Prevent flashing of messages at startup
+  (when (display-graphic-p)
+    (setq-default inhibit-redisplay t
+                  inhibit-message t)
+    (defun reset-inhibit-vars ()
+      (setq-default inhibit-redisplay nil
+                    inhibit-message nil)
+      (redraw-frame))
+    (add-hook 'window-setup-hook #'reset-inhibit-vars)
+    (define-advice startup--load-user-init-file (:after (&rest _) reset-inhibit-vars)
+      (and init-file-had-error (reset-inhibit-vars))))
+
   ;; Suppress file handlers operations at startup
   ;; `file-name-handler-alist' is consulted on each call to `require' and `load'
   (let ((old-value file-name-handler-alist))
