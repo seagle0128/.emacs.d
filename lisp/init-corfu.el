b2fbb2053e58ff82572bcef89b9873e0140b8829
Vincent Zhang <seagle0128@gmail.com>
2023-05-30 18:29:27 +0800

    Enhance corfu.

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -33,14 +33,13 @@
 (use-package corfu
   :custom
   (corfu-auto t)                 ;; Enable auto completion
-  (corfu-separator ?\s)          ;; Orderless field separator
-  (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  (corfu-preview-current nil)    ;; Disable current candidate preview
-  (corfu-preselect 'prompt)      ;; Preselect the prompt
-  (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  (corfu-auto-prefix 2)          ;; Trigger auto completion with 2 chars
+  (corfu-commit-predicate nil)   ;; Do not commit selected candidates on next input
+  (corfu-quit-at-boundary t)     ;; Automatically quit at word boundary
+  (corfu-quit-no-match t)        ;; Automatically quit if there is no match
+  (corfu-preselect-first t)      ;; Disable candidate preselection
   (corfu-scroll-margin 5)        ;; Use scroll margin
-  :bind ("M-/" . corfu-complete)
+  :bind ("M-/" . completion-at-point)
   :hook ((after-init . global-corfu-mode)
          (global-corfu-mode . corfu-popupinfo-mode)))
 
@@ -50,48 +49,103 @@
 
 (use-package kind-icon
   :after corfu
-  :custom
-  (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
   :init
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter)
-  (setq kind-icon-use-icons nil)
-  (setq kind-icon-mapping
-        `((array ,(nerd-icons-codicon "nf-cod-symbol_array") :face font-lock-type-face)
-          (boolean ,(nerd-icons-codicon "nf-cod-symbol_boolean") :face font-lock-builtin-face)
-          (class ,(nerd-icons-codicon "nf-cod-symbol_class") :face font-lock-type-face)
-          (color ,(nerd-icons-codicon "nf-cod-symbol_color") :face success)
-          (command ,(nerd-icons-codicon "nf-cod-terminal") :face default)
-          (constant ,(nerd-icons-codicon "nf-cod-symbol_constant") :face font-lock-constant-face)
-          (constructor ,(nerd-icons-codicon "nf-cod-triangle_right") :face font-lock-function-name-face)
-          (enummember ,(nerd-icons-codicon "nf-cod-symbol_enum_member") :face font-lock-builtin-face)
-          (enum-member ,(nerd-icons-codicon "nf-cod-symbol_enum_member") :face font-lock-builtin-face)
-          (enum ,(nerd-icons-codicon "nf-cod-symbol_enum") :face font-lock-builtin-face)
-          (event ,(nerd-icons-codicon "nf-cod-symbol_event") :face font-lock-warning-face)
-          (field ,(nerd-icons-codicon "nf-cod-symbol_field") :face font-lock-variable-name-face)
-          (file ,(nerd-icons-codicon "nf-cod-symbol_file") :face font-lock-string-face)
-          (folder ,(nerd-icons-codicon "nf-cod-folder") :face font-lock-doc-face)
-          (interface ,(nerd-icons-codicon "nf-cod-symbol_interface") :face font-lock-type-face)
-          (keyword ,(nerd-icons-codicon "nf-cod-symbol_keyword") :face font-lock-keyword-face)
-          (macro ,(nerd-icons-codicon "nf-cod-symbol_misc") :face font-lock-keyword-face)
-          (magic ,(nerd-icons-codicon "nf-cod-wand") :face font-lock-builtin-face)
-          (method ,(nerd-icons-codicon "nf-cod-symbol_method") :face font-lock-function-name-face)
-          (function ,(nerd-icons-codicon "nf-cod-symbol_method") :face font-lock-function-name-face)
-          (module ,(nerd-icons-codicon "nf-cod-file_submodule") :face font-lock-preprocessor-face)
-          (numeric ,(nerd-icons-codicon "nf-cod-symbol_numeric") :face font-lock-builtin-face)
-          (operator ,(nerd-icons-codicon "nf-cod-symbol_operator") :face font-lock-comment-delimiter-face)
-          (param ,(nerd-icons-codicon "nf-cod-symbol_parameter") :face default)
-          (property ,(nerd-icons-codicon "nf-cod-symbol_property") :face font-lock-variable-name-face)
-          (reference ,(nerd-icons-codicon "nf-cod-references") :face font-lock-variable-name-face)
-          (snippet ,(nerd-icons-codicon "nf-cod-symbol_snippet") :face font-lock-string-face)
-          (string ,(nerd-icons-codicon "nf-cod-symbol_string") :face font-lock-string-face)
-          (struct ,(nerd-icons-codicon "nf-cod-symbol_structure") :face font-lock-variable-name-face)
-          (text ,(nerd-icons-codicon "nf-cod-text_size") :face font-lock-doc-face)
-          (typeparameter ,(nerd-icons-codicon "nf-cod-list_unordered") :face font-lock-type-face)
-          (type-parameter ,(nerd-icons-codicon "nf-cod-list_unordered") :face font-lock-type-face)
-          (unit ,(nerd-icons-codicon "nf-cod-symbol_ruler") :face font-lock-constant-face)
-          (value ,(nerd-icons-codicon "nf-cod-symbol_field") :face font-lock-builtin-face)
-          (variable ,(nerd-icons-codicon "nf-cod-symbol_variable") :face font-lock-variable-name-face)
-          (t ,(nerd-icons-codicon "nf-cod-code") :face font-lock-warning-face))))
+  (require 'color)
+  (defsubst rgb-blend (rgb1 rgb2 frac)
+    "Return a fractional blend between two colors RGB1 and RGB2.
+Each is a 3 element list.  The fractional blend point is the
+float FRAC."
+    (apply #'color-rgb-to-hex
+           (cl-mapcar (lambda (a b)
+                        (+ (* a frac) (* b (- 1.0 frac))))
+                      rgb1 rgb2)))
+
+  (defsubst icon-face-spec (face)
+    "Compute icon face spec from FACE."
+    (let* ((default-bg (frame-parameter nil 'background-color))
+           (fg (or (face-foreground face) (frame-parameter nil 'foreground-color)))
+           (bg (rgb-blend (color-name-to-rgb fg) (color-name-to-rgb default-bg) 0.12)))
+      `(:foreground ,fg :background ,bg)))
+
+  (defun codicon (name face)
+    "Get cod icon NAME with FACE foreground and blended background."
+    (nerd-icons-codicon name :face (icon-face-spec face)))
+
+  (defconst corfu-kind-icon-mapping
+    `((array . ,(codicon "nf-cod-symbol_array" 'font-lock-type-face))
+      (boolean . ,(codicon "nf-cod-symbol_boolean" 'font-lock-builtin-face))
+      (class . ,(codicon "nf-cod-symbol_class" 'font-lock-type-face))
+      (color . ,(codicon "nf-cod-symbol_color" 'success) )
+      (command . ,(codicon "nf-cod-terminal" 'default) )
+      (constant . ,(codicon "nf-cod-symbol_constant" 'font-lock-constant-face) )
+      (constructor . ,(codicon "nf-cod-triangle_right" 'font-lock-function-name-face) )
+      (enummember . ,(codicon "nf-cod-symbol_enum_member" 'font-lock-builtin-face) )
+      (enum-member . ,(codicon "nf-cod-symbol_enum_member" 'font-lock-builtin-face) )
+      (enum . ,(codicon "nf-cod-symbol_enum" 'font-lock-builtin-face) )
+      (event . ,(codicon "nf-cod-symbol_event" 'font-lock-warning-face) )
+      (field . ,(codicon "nf-cod-symbol_field" 'font-lock-variable-name-face) )
+      (file . ,(codicon "nf-cod-symbol_file" 'font-lock-string-face) )
+      (folder . ,(codicon "nf-cod-folder" 'font-lock-doc-face) )
+      (interface . ,(codicon "nf-cod-symbol_interface" 'font-lock-type-face) )
+      (keyword . ,(codicon "nf-cod-symbol_keyword" 'font-lock-keyword-face) )
+      (macro . ,(codicon "nf-cod-symbol_misc" 'font-lock-keyword-face) )
+      (magic . ,(codicon "nf-cod-wand" 'font-lock-builtin-face) )
+      (method . ,(codicon "nf-cod-symbol_method" 'font-lock-function-name-face) )
+      (function . ,(codicon "nf-cod-symbol_method" 'font-lock-function-name-face) )
+      (module . ,(codicon "nf-cod-file_submodule" 'font-lock-preprocessor-face) )
+      (numeric . ,(codicon "nf-cod-symbol_numeric" 'font-lock-builtin-face) )
+      (operator . ,(codicon "nf-cod-symbol_operator" 'font-lock-comment-delimiter-face) )
+      (param . ,(codicon "nf-cod-symbol_parameter" 'default) )
+      (property . ,(codicon "nf-cod-symbol_property" 'font-lock-variable-name-face) )
+      (reference . ,(codicon "nf-cod-references" 'font-lock-variable-name-face) )
+      (snippet . ,(codicon "nf-cod-symbol_snippet" 'font-lock-string-face) )
+      (string . ,(codicon "nf-cod-symbol_string" 'font-lock-string-face) )
+      (struct . ,(codicon "nf-cod-symbol_structure" 'font-lock-variable-name-face) )
+      (text . ,(codicon "nf-cod-text_size" 'font-lock-doc-face) )
+      (typeparameter . ,(codicon "nf-cod-list_unordered" 'font-lock-type-face) )
+      (type-parameter . ,(codicon "nf-cod-list_unordered" 'font-lock-type-face) )
+      (unit . ,(codicon "nf-cod-symbol_ruler" 'font-lock-constant-face) )
+      (value . ,(codicon "nf-cod-symbol_field" 'font-lock-builtin-face) )
+      (variable . ,(codicon "nf-cod-symbol_variable" 'font-lock-variable-name-face) )
+      (t . ,(codicon "nf-cod-code" 'font-lock-warning-face))))
+
+  (defsubst nerd-icon--metadata-get (metadata type-name)
+    "Get METADATA for keyword TYPE-NAME from the completion properties."
+    (or
+     (plist-get completion-extra-properties (intern (format ":%s" type-name)))
+     (cdr (assq (intern type-name) metadata))))
+
+  (defsubst nerd-icon-formatted (kind)
+    "Get icon for KIND."
+    (let* ((icon (alist-get kind corfu-kind-icon-mapping))
+           (icon-face (get-text-property 0 'face icon))
+           (icon-bg (plist-get icon-face :inherit))
+           (icon-pad (propertize " " 'face (append '(:height 0.5) icon-bg)))
+           (item-pad (propertize " " 'face '(:height 0.5))))
+      (concat icon-pad icon icon-pad item-pad)))
+
+  (defun nerd-icon-margin-formatter (metadata)
+    "Return a margin-formatter function which produces kind icons.
+METADATA is the completion metadata supplied by the caller (see
+info node `(elisp)Programmed Completion').  To use, add this
+function to the relevant margin-formatters list."
+    (if-let ((kind-func (nerd-icon--metadata-get metadata "company-kind")))
+        (lambda (cand)
+          (if-let ((kind (funcall kind-func cand)))
+              (nerd-icon-formatted kind)
+            (nerd-icon-formatted t)))))
+
+  (add-to-list 'corfu-margin-formatters #'nerd-icon-margin-formatter))
+
+;; Add extensions
+(use-package cape
+  :init
+  (setq cape-dict-case-fold t)
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  ;; (add-to-list 'completion-at-point-functions #'cape-tex)
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-keyword)
+  (add-to-list 'completion-at-point-functions #'cape-abbrev))
 
 (provide 'init-corfu)
 
