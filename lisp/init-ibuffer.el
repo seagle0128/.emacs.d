30c2fb778ef83c074cd1dbb4d5c5c6a7dc5133ee
Vincent Zhang <seagle0128@gmail.com>
2023-07-14 11:09:08 +0800

    Improve ibuffer.

--- lisp/init-ibuffer.el
+++ lisp/init-ibuffer.el
@@ -49,13 +49,22 @@
                        (ibuffer-do-sort-by-project-file-relative))))
   :init (setq ibuffer-project-use-cache t)
   :config
+  (defun my-ibuffer-project-group-name (root type)
+    "Return group name for project ROOT and TYPE."
+    (if (and (stringp type) (> (length type) 0))
+        (format "%s %s" type root)
+      (format "%s" root)))
   (if (icons-displayable-p)
+      (progn
+        (advice-add #'ibuffer-project-group-name :override #'my-ibuffer-project-group-name)
+        (setq ibuffer-project-root-functions
+              `((ibuffer-project-project-root . ,(nerd-icons-octicon "nf-oct-repo" :height 1.2 :face ibuffer-filter-group-name-face))
+                (file-remote-p . ,(nerd-icons-codicon "nf-cod-radio_tower" :height 1.2 :face ibuffer-filter-group-name-face)))))
+    (progn
+      (advice-remove #'ibuffer-project-group-name :override #'my-ibuffer-project-group-name)
       (setq ibuffer-project-root-functions
-            `((ibuffer-project-project-root . ,(nerd-icons-octicon "nf-oct-repo" :height 1.2 :face ibuffer-filter-group-name-face))
-              (file-remote-p . ,(nerd-icons-codicon "nf-cod-radio_tower" :height 1.2 :face ibuffer-filter-group-name-face))))
-    (setq ibuffer-project-root-functions
-          '((ibuffer-project-project-root . "Project")
-            (file-remote-p . "Remote")))))
+            '((ibuffer-project-project-root . "Project")
+              (file-remote-p . "Remote"))))))
 
 (provide 'init-ibuffer)
 
