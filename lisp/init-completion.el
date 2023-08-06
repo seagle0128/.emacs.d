732f1d651e016ab3cb931b698855bf67bcccc2f8
Vincent Zhang <seagle0128@gmail.com>
2023-08-06 17:31:35 +0800

    minor fix.

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -50,6 +50,7 @@
 (use-package orderless
   :init
   (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion)))))
 
 (use-package vertico
