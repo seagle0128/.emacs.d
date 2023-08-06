d38424328cdf38130f302687d16cba1d362a95aa
Vincent Zhang <seagle0128@gmail.com>
2023-08-06 17:32:39 +0800

    Improve orderless.

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -50,8 +50,7 @@
 (use-package orderless
   :init
   (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
+        completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package vertico
   :hook (after-init . vertico-mode))
