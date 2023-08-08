82040deff14eae24999ab2b7288503bf9221e4bc
Vincent Zhang <seagle0128@gmail.com>
2023-08-08 22:30:19 +0800

    Enable visual bell.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -80,7 +80,27 @@
       ;; Excellent themes
       (use-package doom-themes
         :bind ("C-c T" . centaur-load-theme)
-        :init (centaur-load-theme centaur-theme t)))
+        :init (centaur-load-theme centaur-theme t)
+        :config
+        ;; Enable flashing mode-line on errors
+        (doom-themes-visual-bell-config)
+
+        ;; WORKAROUND: Visual bell on 29+
+        ;; @see https://github.com/doomemacs/themes/issues/733
+        (with-no-warnings
+          (defun my-doom-themes-visual-bell-fn ()
+            "Blink the mode-line red briefly. Set `ring-bell-function' to this to use it."
+            (let ((buf (current-buffer))
+                  (cookies (mapcar (lambda (face)
+                                     (face-remap-add-relative face 'doom-themes-visual-bell))
+                                   '(mode-line mode-line-active))))
+              (force-mode-line-update)
+              (run-with-timer 0.15 nil
+                              (lambda ()
+                                (with-current-buffer buf
+                                  (mapc #'face-remap-remove-relative cookies)
+                                  (force-mode-line-update))))))
+          (advice-add #'doom-themes-visual-bell-fn :override #'my-doom-themes-visual-bell-fn))))
   (progn
     (warn "The current theme is incompatible!")
     (centaur-load-theme centaur-theme t)))
