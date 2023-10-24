0b3f1300b334c847264d63d4192fcf31165429d8
Vincent Zhang <seagle0128@gmail.com>
2023-10-24 15:08:58 +0800

    refactor: update keybindings.

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -103,7 +103,7 @@
 (use-package avy
   :bind (("C-:"   . avy-goto-char)
          ("C-'"   . avy-goto-char-2)
-         ("M-g f" . avy-goto-line)
+         ("M-g l" . avy-goto-line)
          ("M-g w" . avy-goto-word-1)
          ("M-g e" . avy-goto-word-0))
   :hook (after-init . avy-setup-default)
