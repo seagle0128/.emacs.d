c0c3031aa71cafb0cbfa2fbdda697de914a4825d
Vincent Zhang <seagle0128@gmail.com>
2023-12-03 22:00:08 +0800

    feat: support icons in elfeed
    See https://emacs-china.org/t/elfeed-nerd-icons/26125.

--- lisp/init-reader.el
+++ lisp/init-reader.el
@@ -139,6 +139,55 @@
   ;; Ignore db directory in recentf
   (push elfeed-db-directory recentf-exclude)
 
+  ;; Add icons via tags
+  (when (icons-displayable-p)
+    (defun nerd-icon-for-tags (tags)
+      "Generate Nerd Font icon based on tags.
+  Returns default if no match."
+      (cond ((member "youtube" tags)  (nerd-icons-faicon "nf-fa-youtube_play" :face '(:foreground "#FF0200")))
+            ((member "instagram" tags) (nerd-icons-faicon "nf-fa-instagram" :face '(:foreground "#FF00B9")))
+            ((or (member "emacs" tags) (member "emacslife" tags) (member "mastering" tags))
+             (nerd-icons-sucicon "nf-custom-emacs" :face '(:foreground "#9A5BBE")))
+            ((member "github" tags) (nerd-icons-faicon "nf-fa-github"))
+            (t (nerd-icons-faicon "nf-fae-feedly" :face '(:foreground "#2AB24C")))))
+
+    (defun lucius/elfeed-search-print-entry--better-default (entry)
+      "Print ENTRY to the buffer."
+      (let* ((date (elfeed-search-format-date (elfeed-entry-date entry)))
+             (date-width (car (cdr elfeed-search-date-format)))
+             (title (concat (or (elfeed-meta entry :title)
+                                (elfeed-entry-title entry) "")
+                            ;; NOTE: insert " " for overlay to swallow
+                            " "))
+             (title-faces (elfeed-search--faces (elfeed-entry-tags entry)))
+             (feed (elfeed-entry-feed entry))
+             (feed-title (when feed (or (elfeed-meta feed :title) (elfeed-feed-title feed))))
+             (tags (mapcar #'symbol-name (elfeed-entry-tags entry)))
+             (tags-str (mapconcat (lambda (s) (propertize s 'face 'elfeed-search-tag-face)) tags ","))
+             (title-width (- (frame-width)
+                             ;; (window-width (get-buffer-window (elfeed-search-buffer) t))
+                             date-width elfeed-search-trailing-width))
+             (title-column (elfeed-format-column
+                            title (elfeed-clamp
+                                   elfeed-search-title-min-width
+                                   title-width
+ elfeed-search-title-max-width) :left))
+
+             ;; Title/Feed ALIGNMENT
+             (align-to-feed-pixel (+ date-width
+                                     (max elfeed-search-title-min-width
+                                          (min title-width elfeed-search-title-max-width)))))
+        (insert (propertize date 'face 'elfeed-search-date-face) " ")
+        (insert (propertize title-column 'face title-faces 'kbd-help title))
+        (put-text-property (1- (point)) (point) 'display `(space :align-to ,align-to-feed-pixel))
+        ;; (when feed-title (insert " " (propertize feed-title 'face 'elfeed-search-feed-face) " "))
+        (when feed-title
+          (insert " " (concat (nerd-icon-for-tags tags) " ")
+                  (propertize feed-title 'face 'elfeed-search-feed-face) " "))
+        (when tags (insert "(" tags-str ")"))))
+
+    (setq  elfeed-search-print-entry-function #'lucius/elfeed-search-print-entry--better-default))
+
   ;; Use xwidget if possible
   (with-no-warnings
     (defun my-elfeed-show-visit (&optional use-generic-p)
