1cfe7a45691f0de0e4daa2b152f74dfd4bead501
Vincent Zhang <seagle0128@gmail.com>
2024-01-04 14:10:36 +0800

    fix consult-preview-key

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -180,7 +180,7 @@
   ;; Optionally configure preview. The default value
   ;; is 'any, such that any key triggers the preview.
   ;; (setq consult-preview-key 'any)
-  (setq consult-preview-key "M-.")
+  (setq consult-preview-key '(:debounce 1.0 any))
   ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
   ;; For some commands and buffer sources it is useful to configure the
   ;; :preview-key on a per-command basis using the `consult-customize' macro.
