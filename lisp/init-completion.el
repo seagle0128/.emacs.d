8c27954622952b3623833580bdce6ec4c1170070
Vincent Zhang <seagle0128@gmail.com>
2024-01-10 10:12:52 +0800

    feat: add more utils about colors

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -176,6 +176,48 @@
     (setq xref-show-xrefs-function #'consult-xref
           xref-show-definitions-function #'consult-xref))
 
+  ;; More utils
+  (defvar consult-colors-history nil
+    "History for `consult-colors-emacs' and `consult-colors-web'.")
+
+  ;; No longer preloaded in Emacs 28.
+  (autoload 'list-colors-duplicates "facemenu")
+  ;; No preloaded in consult.el
+  (autoload 'consult--read "consult")
+
+  (defun consult-colors-emacs (color)
+    "Show a list of all supported colors for a particular frame.\
+
+You can insert the name (default), or insert or kill the hexadecimal or RGB value of the
+selected color."
+    (interactive
+     (list (consult--read (list-colors-duplicates (defined-colors))
+                          :prompt "Emacs color: "
+                          :require-match t
+                          :category 'color
+                          :history '(:input consult-colors-history)
+                          )))
+    (insert color))
+
+  ;; Adapted from counsel.el to get web colors.
+  (defun consult-colors--web-list nil
+    "Return list of CSS colors for `counsult-colors-web'."
+    (require 'shr-color)
+    (sort (mapcar #'downcase (mapcar #'car shr-color-html-colors-alist)) #'string-lessp))
+
+  (defun consult-colors-web (color)
+    "Show a list of all CSS colors.\
+
+You can insert the name (default), or insert or kill the hexadecimal or RGB value of the
+selected color."
+    (interactive
+     (list (consult--read (consult-colors--web-list)
+                          :prompt "Color: "
+                          :require-match t
+                          :category 'color
+                          :history '(:input consult-colors-history)
+                          )))
+    (insert color))
   :config
   ;; Optionally configure preview. The default value
   ;; is 'any, such that any key triggers the preview.
