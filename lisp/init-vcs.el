792d2dddff2eead695c0d518e6024bf4ab97abae
Vincent Zhang <seagle0128@gmail.com>
2024-01-19 17:17:12 +0800

    fix forge

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -44,20 +44,13 @@
   ;; Access Git forges from Magit
   (use-package forge
     :demand t
-    :defines (forge-database-connector forge-topic-list-columns)
     :custom-face
     (forge-topic-label ((t (:inherit variable-pitch :height 0.9 :width condensed :weight regular :underline nil))))
-    :init
-    (setq forge-database-connector (if (and (require 'emacsql-sqlite-builtin nil t)
-                                            (functionp 'emacsql-sqlite-builtin)
-                                            (functionp 'sqlite-open))
-                                       'sqlite-builtin
-                                     'sqlite)
-          forge-topic-list-columns
-          '(("#" 5 forge-topic-list-sort-by-number (:right-align t) number nil)
-            ("Title" 60 t nil title  nil)
-            ("State" 6 t nil state nil)
-            ("Updated" 10 t nil updated nil)))))
+    :init (setq forge-topic-list-columns
+                '(("#" 5 forge-topic-list-sort-by-number (:right-align t) number nil)
+                  ("Title" 60 t nil title  nil)
+                  ("State" 6 t nil state nil)
+                  ("Updated" 10 t nil updated nil)))))
 
 ;; Display transient in child frame
 (when (childframe-completion-workable-p)
