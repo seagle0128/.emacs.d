b5158448e3c38cef2f81b53f894e6a0b7b302d48
Vincent Zhang <seagle0128@gmail.com>
2024-01-24 16:06:18 +0800

    Update elisp-flymake-byte-compile-load-path while checking

--- lisp/init-flymake.el
+++ lisp/init-flymake.el
@@ -32,12 +32,20 @@
 
 (use-package flymake
   :diminish
+  :functions my-elisp-flymake-byte-compile
   :bind ("C-c f" . flymake-show-buffer-diagnostics)
   :hook (prog-mode . flymake-mode)
   :init (setq flymake-no-changes-timeout nil
               flymake-fringe-indicator-position 'right-fringe)
-  :config (setq elisp-flymake-byte-compile-load-path
-                (append elisp-flymake-byte-compile-load-path load-path)))
+  :config
+  ;; Check elisp with ``load-path''
+  (defun my-elisp-flymake-byte-compile (fn &rest args)
+    ;; checkdoc-params: (fn args)
+    "Wrapper for `elisp-flymake-byte-compile'."
+    (let ((elisp-flymake-byte-compile-load-path
+           (append elisp-flymake-byte-compile-load-path load-path)))
+      (apply fn args)))
+  (advice-add 'elisp-flymake-byte-compile :around #'my-elisp-flymake-byte-compile))
 
 (use-package sideline-flymake
   :diminish sideline-mode
