7439c39dca093fc336e5d8546d021ec180d2777d
Vincent Zhang <seagle0128@gmail.com>
2024-05-13 20:02:34 +0800

    enhance go-translate

--- lisp/init-dict.el
+++ lisp/init-dict.el
@@ -44,14 +44,35 @@
   :bind (("C-c y"   . gt-do-translate)
          ("C-c d g" . gt-do-translate)
          ("C-c d s" . gt-do-setup))
-
-  :config
+  :init
   (setq gt-langs '(en zh)
         gt-buffer-render-window-config
         '((display-buffer-reuse-window display-buffer-in-side-window)
           (side . bottom)
           (slot . 1)
           (window-height . 0.4)))
+  :config
+  (setq gt-default-translator
+        (if (childframe-workable-p)
+            (gt-translator :engines (gt-youdao-dict-engine)
+                           :render (gt-posframe-pop-render))
+          (gt-translator :engines (list (gt-bing-engine)
+                                        (gt-youdao-dict-engine)
+                                        (gt-youdao-suggest-engine))
+                         :render (gt-buffer-render))))
+  (setq gt-preset-translators
+        `((default . ,(gt-translator :taker   (cdar (gt-ensure-plain gt-preset-takers))
+                                     :engines (cdar (gt-ensure-plain gt-preset-engines))
+                                     :render  (cdar (gt-ensure-plain gt-preset-renders))))
+          (simple . ,(gt-translator :engines (gt-youdao-dict-engine)
+                                    :render (gt-posframe-pop-render)))
+          (detailed . ,(gt-translator :engines (list (gt-bing-engine)
+                                                     (gt-youdao-dict-engine)
+                                                     (gt-youdao-suggest-engine))
+                                      :render (gt-buffer-render)))
+          (Text-Utility . ,(gt-text-utility
+                            :taker (gt-taker :pick nil)
+                            :render (gt-buffer-render)))))
 
   ;; Same behavior with `popper'
   (add-hook 'gt-buffer-render-output-hook
@@ -65,23 +86,21 @@
                 (when-let ((win (get-buffer-window gt-buffer-render-buffer-name)))
                   (and (window-live-p win) (delete-window win)))))
 
-  (defclass gt-posframe-pos-render (gt-posframe-pop-render)
+  ;; Tweak child frame
+  (defclass gt-posframe-pop-render (gt-buffer-render)
     ((width       :initarg :width        :initform 70)
-     (height      :initarg :height       :initform 20)
+     (height      :initarg :height       :initform 15)
+     (forecolor   :initarg :forecolor    :initform nil)
+     (backcolor   :initarg :backcolor    :initform nil)
      (padding     :initarg :padding      :initform 16)
      (bd-width    :initarg :bd-width     :initform 1)
-     (bd-color    :initarg :bd-color     :initform nil)
-     (backcolor   :initarg :backcolor    :initform nil))
-    "Pop up a childframe to show the result at point.
+     (bd-color    :initarg :bd-color     :initform nil))
+    "Pop up a childframe to show the result.
 The frame will disappear when do do anything but focus in it.
 Manually close the frame with `q'.")
 
-  (cl-defmethod gt-init :before ((_ gt-posframe-pos-render) _)
-    (unless (require 'posframe nil t)
-      (user-error "To use `gt-posframe-render', you should install and load package `posframe' first")))
-
-  (cl-defmethod gt-init ((render gt-posframe-pos-render) translator)
-    (with-slots (width height min-width min-height bd-width forecolor backcolor bd-color padding position) render
+  (cl-defmethod gt-init ((render gt-posframe-pop-render) translator)
+    (with-slots (width height bd-width forecolor backcolor bd-color padding) render
       (let ((inhibit-read-only t)
             (buf gt-posframe-pop-render-buffer))
         ;; create
@@ -101,43 +120,12 @@ Manually close the frame with `q'.")
                          :right-fringe padding
                          :position (point)
                          :poshandler gt-posframe-pop-render-poshandler))
-
         ;; render
         (gt-buffer-render-init buf render translator)
         (posframe-refresh buf)
         ;; setup
         (with-current-buffer buf
-          (gt-buffer-render-key ("q" "Close") (posframe-delete buf))))))
-
-  (cl-defmethod gt-output ((render gt-posframe-pos-render) translator)
-    (when-let (buf (get-buffer gt-posframe-pop-render-buffer))
-      (gt-buffer-render-output buf render translator)
-      (posframe-refresh buf)
-      (add-hook 'post-command-hook #'gt-posframe-render-auto-close-handler)))
-
-  (setq gt-default-translator
-        (if (childframe-workable-p)
-            (gt-translator :engines (gt-youdao-dict-engine)
-                           :render (gt-posframe-pos-render))
-          (gt-translator :engines (list (gt-bing-engine)
-                                        (gt-youdao-dict-engine)
-                                        (gt-youdao-suggest-engine))
-                         :render (gt-buffer-render))))
-
-  (setq gt-preset-translators
-        (lambda ()
-          `((default . ,(gt-translator :taker   (cdar (gt-ensure-plain gt-preset-takers))
-                                       :engines (cdar (gt-ensure-plain gt-preset-engines))
-                                       :render  (cdar (gt-ensure-plain gt-preset-renders))))
-            (simple . ,(gt-translator :engines (gt-youdao-dict-engine)
-                                      :render (gt-posframe-pos-render)))
-            (detailed . ,(gt-translator :engines (list (gt-bing-engine)
-                                                       (gt-youdao-dict-engine)
-                                                       (gt-youdao-suggest-engine))
-                                        :render (gt-buffer-render)))
-            (Text-Utility . ,(gt-text-utility
-                              :taker (gt-taker :pick nil)
-                              :render (gt-buffer-render)))))))
+          (gt-buffer-render-key ("q" "Close") (posframe-delete buf)))))))
 
 ;; OSX dictionary
 (when sys/macp
