31d49721addcacde7f1f5a5c4d3137e5c4d05a0a
Vincent Zhang <seagle0128@gmail.com>
2024-05-14 15:13:00 +0800

    feat: more tranlator utilities

--- lisp/init-dict.el
+++ lisp/init-dict.el
@@ -38,12 +38,23 @@
   :bind (("C-c d f" . fanyi-dwim)
          ("C-c d d" . fanyi-dwim2)
          ("C-c d h" . fanyi-from-history))
-  :custom (fanyi-providers '(fanyi-haici-provider fanyi-longman-provider)))
+  :custom (fanyi-providers '(fanyi-haici-provider
+                             fanyi-youdao-thesaurus-provider
+                             fanyi-etymon-provider
+                             fanyi-longman-provider)))
 
 (use-package go-translate
   :bind (("C-c g"   . gt-do-translate)
+         ("C-c u"   . gt-do-text-utiliuty)
+         ("C-c y"   . gt-youdao-dict-translate-dwim)
+         ("C-c d b" . gt-bing-translate)
+         ("C-c d B" . gt-bing-translate-dwim)
          ("C-c d g" . gt-do-translate)
-         ("C-c d s" . gt-do-setup))
+         ("C-c d m" . gt-multi-translate)
+         ("C-c d y" . gt-youdao-dict-translate)
+         ("C-c d Y" . gt-youdao-dict-translate-dwim)
+         ("C-c d s" . gt-do-setup)
+         ("C-c d u" . gt-do-text-utility))
   :init
   (setq gt-langs '(en zh)
         gt-buffer-render-window-config
@@ -52,28 +63,6 @@
           (slot . 1)
           (window-height . 0.4)))
   :config
-  (setq gt-default-translator
-        (if (childframe-workable-p)
-            (gt-translator :engines (gt-youdao-dict-engine)
-                           :render (gt-posframe-pop-render))
-          (gt-translator :engines (list (gt-bing-engine)
-                                        (gt-youdao-dict-engine)
-                                        (gt-youdao-suggest-engine))
-                         :render (gt-buffer-render))))
-  (setq gt-preset-translators
-        `((default . ,(gt-translator :taker   (cdar (gt-ensure-plain gt-preset-takers))
-                                     :engines (cdar (gt-ensure-plain gt-preset-engines))
-                                     :render  (cdar (gt-ensure-plain gt-preset-renders))))
-          (simple . ,(gt-translator :engines (gt-youdao-dict-engine)
-                                    :render (gt-posframe-pop-render)))
-          (detailed . ,(gt-translator :engines (list (gt-bing-engine)
-                                                     (gt-youdao-dict-engine)
-                                                     (gt-youdao-suggest-engine))
-                                      :render (gt-buffer-render)))
-          (Text-Utility . ,(gt-text-utility
-                            :taker (gt-taker :pick nil)
-                            :render (gt-buffer-render)))))
-
   ;; Same behavior with `popper'
   (add-hook 'gt-buffer-render-output-hook
             (lambda ()
@@ -85,6 +74,67 @@
                 "Close dict result window via `C-g'."
                 (when-let ((win (get-buffer-window gt-buffer-render-buffer-name)))
                   (and (window-live-p win) (delete-window win)))))
+
+  ;; Translators
+  (setq gt-preset-translators
+        `((default . ,(gt-translator :taker   (cdar (gt-ensure-plain gt-preset-takers))
+                                     :engines (cdar (gt-ensure-plain gt-preset-engines))
+                                     :render  (cdar (gt-ensure-plain gt-preset-renders))))
+          (youdao-dict . ,(gt-translator :taker (gt-taker :langs '(en zh) :text 'word :prompt t)
+                                         :engines (gt-youdao-dict-engine)
+                                         :render (gt-buffer-render)))
+          (youdao-dict-dwim . ,(gt-translator :taker (gt-taker :langs '(en zh) :text 'word)
+                                              :engines (gt-youdao-dict-engine)
+                                              :render (if (display-graphic-p)
+                                                          (gt-posframe-pop-render)
+                                                        (gt-buffer-render))))
+          (bing . ,(gt-translator :taker (gt-taker :langs '(en zh) :text 'word :prompt t)
+                                  :engines (gt-bing-engine)
+                                  :render (gt-buffer-render)))
+          (bing-dwim . ,(gt-translator :taker (gt-taker :langs '(en zh) :text 'word)
+                                       :engines (gt-bing-engine)
+                                       :render (if (display-graphic-p)
+                                                   (gt-posframe-pop-render)
+                                                 (gt-buffer-render))))
+          (multi . ,(gt-translator :taker (gt-taker :langs '(en zh) :text 'word :prompt t)
+                                   :engines (list (gt-bing-engine)
+                                                  (gt-youdao-dict-engine)
+                                                  (gt-youdao-suggest-engine)
+                                                  (gt-google-engine))
+                                   :render (gt-buffer-render)))
+          (Text-Utility . ,(gt-text-utility :taker (gt-taker :pick nil)
+                                            :render (gt-buffer-render)))))
+  (setq gt-default-translator (alist-get 'youdao-dict-dwim gt-preset-translators))
+
+  (defun gt-youdao-dict-translate ()
+    (interactive)
+    (let ((gt-default-translator (alist-get 'youdao-dict gt-preset-translators)))
+      (gt-do-translate)))
+
+  (defun gt-youdao-dict-translate-dwim ()
+    (interactive)
+    (let ((gt-default-translator (alist-get 'youdao-dict-dwim gt-preset-translators)))
+      (gt-do-translate)))
+
+  (defun gt-bing-translate ()
+    (interactive)
+    (let ((gt-default-translator (alist-get 'bing gt-preset-translators)))
+      (gt-do-translate)))
+
+  (defun gt-bing-translate-dwim ()
+    (interactive)
+    (let ((gt-default-translator (alist-get 'bing-dwim gt-preset-translators)))
+      (gt-do-translate)))
+
+  (defun gt-multi-translate ()
+    (interactive)
+    (let ((gt-default-translator (alist-get 'multi gt-preset-translators)))
+      (gt-do-translate)))
+
+  (defun gt-do-text-utility ()
+    (interactive)
+    (let ((gt-default-translator (alist-get 'Text-Utility gt-preset-translators)))
+      (gt-do-translate)))
 
   ;; Tweak child frame
   (defclass gt-posframe-pop-render (gt-buffer-render)
