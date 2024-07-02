acc29f6e660e93b4176d4c3d93c4638ba7299053
Vincent Zhang <seagle0128@gmail.com>
2024-07-02 20:02:24 +0800

    fix: rust-mode is overrided by rust-ts-mode (global-treesit-auto-mode)

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -32,8 +32,19 @@
 
 ;; Rust
 (use-package rust-mode
+  :mode ("\\.rs\\'" . rustic-mode)
   :init (setq rust-format-on-save t
-              rust-mode-treesitter-derive t))
+              rust-mode-treesitter-derive t)
+  :config
+  ;; HACK: `global-treesit-auto-mode' will override `rust-mode'.
+  (define-derived-mode rustic-mode rust-mode "Rust"
+    "Major mode for Rust code.
+
+\\{rust-mode-map}")
+
+  (when (centaur-treesit-available-p)
+    (setq auto-mode-alist (delete '("\\.rs\\'" . rust-mode) auto-mode-alist))
+    (setq auto-mode-alist (delete '("\\.rs\\'" . rust-ts-mode) auto-mode-alist))))
 
 (use-package ron-mode
   :mode ("\\.ron" . ron-mode))
