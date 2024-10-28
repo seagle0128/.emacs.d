23cd514bebb2c7ce9c9a0f676ff54c5287f4bcec
Vincent Zhang <seagle0128@gmail.com>
2024-10-28 14:37:57 +0800

    fix: unable to load grip-mode in org buffers

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -96,53 +96,55 @@ mermaid.initialize({
       (if (and (featurep 'xwidget-internal) (display-graphic-p))
           (centaur-webkit-browse-url (concat "file://" (markdown-export)) t)
         (funcall fn)))
-    (advice-add #'markdown-export-and-preview :around #'my-markdown-export-and-preview))
+    (advice-add #'markdown-export-and-preview :around #'my-markdown-export-and-preview)))
 
-  ;; Preview markdown files
-  ;; @see: https://github.com/seagle0128/grip-mode?tab=readme-ov-file#prerequisite
-  (use-package grip-mode
-    :defines org-mode-map
-    :functions auth-source-user-and-password
-    :bind (:map markdown-mode-command-map
-           ("g" . grip-mode))
-    :init
-    (with-eval-after-load 'org
-      (bind-key "C-c C-g" #'grip-mode org-mode-map))
+;; Table of contents
+(use-package markdown-toc
+  :diminish
+  :bind (:map markdown-mode-command-map
+         ("r" . markdown-toc-generate-or-refresh-toc))
+  :hook (markdown-mode . markdown-toc-mode)
+  :init (setq markdown-toc-indentation-space 2
+              markdown-toc-header-toc-title "\n## Table of Contents"
+              markdown-toc-user-toc-structure-manipulation-fn 'cdr)
+  :config
+  (with-no-warnings
+    (define-advice markdown-toc-generate-toc (:around (fn &rest args) lsp)
+      "Generate or refresh toc after disabling lsp."
+      (cond
+       ((bound-and-true-p lsp-managed-mode)
+        (lsp-managed-mode -1)
+        (apply fn args)
+        (lsp-managed-mode 1))
+       ((bound-and-true-p eglot--manage-mode)
+        (eglot--manage-mode -1)
+        (apply fn args)
+        (eglot--manage-mode 1))
+       (t
+        (apply fn args))))))
 
-    (setq grip-update-after-change nil)
+;; Preview markdown files
+;; @see: https://github.com/seagle0128/grip-mode?tab=readme-ov-file#prerequisite
+(use-package grip-mode
+  :defines markdown-mode-command-map org-mode-map
+  :functions auth-source-user-and-password
+  :autoload grip-mode
+  :init
+  (with-eval-after-load 'markdown-mode
+    (bind-key "g" #'grip-mode markdown-mode-command-map))
 
-    ;; mdopen doesn't need credentials, and only support external browsers
-    (if (executable-find "mdopen")
-        (setq grip-use-mdopen t)
-      (when-let ((credential (and (require 'auth-source nil t)
-                                  (auth-source-user-and-password "api.github.com"))))
-        (setq grip-github-user (car credential)
-              grip-github-password (cadr credential)))))
+  (with-eval-after-load 'org
+    (bind-key "C-c C-g" #'grip-mode org-mode-map))
 
-  ;; Table of contents
-  (use-package markdown-toc
-    :diminish
-    :bind (:map markdown-mode-command-map
-           ("r" . markdown-toc-generate-or-refresh-toc))
-    :hook (markdown-mode . markdown-toc-mode)
-    :init (setq markdown-toc-indentation-space 2
-                markdown-toc-header-toc-title "\n## Table of Contents"
-                markdown-toc-user-toc-structure-manipulation-fn 'cdr)
-    :config
-    (with-no-warnings
-      (define-advice markdown-toc-generate-toc (:around (fn &rest args) lsp)
-        "Generate or refresh toc after disabling lsp."
-        (cond
-         ((bound-and-true-p lsp-managed-mode)
-          (lsp-managed-mode -1)
-          (apply fn args)
-          (lsp-managed-mode 1))
-         ((bound-and-true-p eglot--manage-mode)
-          (eglot--manage-mode -1)
-          (apply fn args)
-          (eglot--manage-mode 1))
-         (t
-          (apply fn args)))))))
+  (setq grip-update-after-change nil)
+
+  ;; mdopen doesn't need credentials, and only support external browsers
+  (if (executable-find "mdopen")
+      (setq grip-use-mdopen t)
+    (when-let ((credential (and (require 'auth-source nil t)
+                                (auth-source-user-and-password "api.github.com"))))
+      (setq grip-github-user (car credential)
+            grip-github-password (cadr credential)))))
 
 (provide 'init-markdown)
 
