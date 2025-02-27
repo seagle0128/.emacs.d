95608d0266bb4523215bef71db5c8afe8aa16a85
Vincent Zhang <seagle0128@gmail.com>
2025-02-27 15:02:39 +0800

    feat: add emacs/>=31p

--- lisp/init-const.el
+++ lisp/init-const.el
@@ -107,6 +107,10 @@ Loaded by `org-babel-load-file'.")
   (>= emacs-major-version 30)
   "Emacs is 30 or above.")
 
+(defconst emacs/>=31p
+  (>= emacs-major-version 31)
+  "Emacs is 31 or above.")
+
 (provide 'init-const)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
