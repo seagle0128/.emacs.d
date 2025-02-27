920627575a71bfec7ecc3ed68e5a21aef1a6f751
Vincent Zhang <seagle0128@gmail.com>
2025-02-27 11:16:06 +0800

    feat: disable symbol-overlay in easy-kill

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -133,14 +133,17 @@ FACE defaults to inheriting from default and highlight."
       "Turn off symbol highlighting."
       (interactive)
       (symbol-overlay-mode -1))
-    (advice-add #'set-mark :after #'turn-off-symbol-overlay)
 
     (defun turn-on-symbol-overlay (&rest _)
       "Turn on symbol highlighting."
       (interactive)
       (when (derived-mode-p 'prog-mode 'yaml-mode)
         (symbol-overlay-mode 1)))
-    (advice-add #'deactivate-mark :after #'turn-on-symbol-overlay)))
+
+    (advice-add #'set-mark :after #'turn-off-symbol-overlay)
+    (advice-add #'deactivate-mark :after #'turn-on-symbol-overlay)
+    (advice-add #'easy-kill :after #'turn-off-symbol-overlay)
+    (advice-add #'easy-kill-destroy-candidate :after #'turn-on-symbol-overlay)))
 
 ;; Mark occurrences of current region (selection)
 (use-package region-occurrences-highlighter
