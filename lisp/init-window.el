abc2870579264624671cb7b924b2c1750bede716
Vincent Zhang <seagle0128@gmail.com>
2025-03-17 10:44:57 +0800

    refactor: follow by gptel

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -151,9 +151,9 @@
   (popper-group-function #'popper-group-by-directory)
   (popper-echo-dispatch-actions t)
   :bind (:map popper-mode-map
-              ("C-h z"       . popper-toggle)
-              ("C-<tab>"     . popper-cycle)
-              ("C-M-<tab>"   . popper-toggle-type))
+         ("C-h z"       . popper-toggle)
+         ("C-<tab>"     . popper-cycle)
+         ("C-M-<tab>"   . popper-toggle-type))
   :hook (emacs-startup . popper-echo-mode)
   :init
   (setq popper-mode-line ""
@@ -217,27 +217,25 @@
   :config
   (with-no-warnings
     (defun my-popper-fit-window-height (win)
-      "Determine the height of popup window WIN by fitting it to the buffer's content."
-      (fit-window-to-buffer
-       win
-       (floor (frame-height) 3)
-       (floor (frame-height) 3)))
+      "Adjust the height of popup window WIN to fit the buffer's content."
+      (let ((desired-height (floor (/ (frame-height) 3))))
+        (fit-window-to-buffer win desired-height desired-height)))
     (setq popper-window-height #'my-popper-fit-window-height)
 
-    (defun popper-close-window-hack (&rest _)
+    (defun popper-close-window-hack (&rest _args)
       "Close popper window via `C-g'."
-      ;; `C-g' can deactivate region
-      (when (and ;(called-interactively-p 'interactive)
-             (not (region-active-p))
-             popper-open-popup-alist)
-        (when-let* ((window (caar popper-open-popup-alist))
-                    (buffer (cdar popper-open-popup-alist)))
-          (when (and (with-current-buffer buffer
-                       (not (derived-mode-p 'eshell-mode
+      (when (and ; (called-interactively-p 'interactive)
+                 (not (region-active-p))
+                 popper-open-popup-alist)
+        (let ((window (caar popper-open-popup-alist))
+              (buffer (cdar popper-open-popup-alist)))
+          (when (and (window-live-p window)
+                     (buffer-live-p buffer)
+                     (not (with-current-buffer buffer
+                            (derived-mode-p 'eshell-mode
                                             'shell-mode
                                             'term-mode
-                                            'vterm-mode)))
-                     (window-live-p window))
+                                            'vterm-mode))))
             (delete-window window)))))
     (advice-add #'keyboard-quit :before #'popper-close-window-hack)))
 
