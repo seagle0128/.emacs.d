dbe6f03ccc9ae75838921a9fa92c922e92eee8e4
Vincent Zhang <seagle0128@gmail.com>
2025-03-31 02:34:47 +0800

    fix: hydra faces

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -36,20 +36,19 @@
   (when (childframe-completion-workable-p)
     (setq hydra-hint-display-type 'posframe)
 
-    (with-eval-after-load 'posframe
-      (defun hydra-set-posframe-show-params ()
-        "Set hydra-posframe style."
-        (setq hydra-posframe-show-params
-              `(:left-fringe 8
-                :right-fringe 8
-                :internal-border-width 2
-                :internal-border-color ,(face-background 'posframe-border nil t)
-                :background-color ,(face-background 'tooltip nil t)
-                :foreground-color ,(face-foreground 'tooltip nil t)
-                :lines-truncate t
-                :poshandler posframe-poshandler-frame-center-near-bottom)))
-      (hydra-set-posframe-show-params)
-      (add-hook 'after-load-theme-hook #'hydra-set-posframe-show-params t))))
+    (defun hydra-set-posframe-show-params ()
+      "Set hydra-posframe style."
+      (setq hydra-posframe-show-params
+            `(:left-fringe 8
+              :right-fringe 8
+              :internal-border-width 2
+              :internal-border-color ,(face-background 'posframe-border nil t)
+              :background-color ,(face-background 'tooltip nil t)
+              :foreground-color ,(face-foreground 'tooltip nil t)
+              :lines-truncate t
+              :poshandler posframe-poshandler-frame-center-near-bottom)))
+    (hydra-set-posframe-show-params)
+    (add-hook 'after-load-theme-hook #'hydra-set-posframe-show-params t)))
 
 (use-package pretty-hydra
   :bind ("<f6>" . toggles-hydra/body)
