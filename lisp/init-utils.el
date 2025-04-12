61f3196d3718a19a43839d55c6ce26915f66c07c
Vincent Zhang <seagle0128@gmail.com>
2025-04-12 23:00:41 +0800

    feat: add reveal-in-folder

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -30,6 +30,9 @@
 
 ;;; Code:
 
+(eval-when-compile
+  (require 'init-const))
+
 ;; Display available keybindings in popup
 (use-package which-key
   :diminish
@@ -260,6 +263,7 @@
 ;; Misc
 (use-package disk-usage)
 (use-package memory-usage)
+(use-package reveal-in-folder)
 
 (use-package list-environment
   :init
