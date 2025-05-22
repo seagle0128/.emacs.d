3ee8065e1d7a880d3c37e4564e3887ac38d56705
Vincent Zhang <seagle0128@gmail.com>
2025-05-22 14:25:36 +0800

    fix: hide all child frames before saving sessions

--- lisp/init-workspace.el
+++ lisp/init-workspace.el
@@ -34,6 +34,7 @@
   (require 'init-custom))
 
 (use-package tabspaces
+  :functions tabspaces-mode
   :hook (after-init . (lambda() (unless centaur-dashboard (tabspaces-mode t))))
   :custom
   (tab-bar-show nil)
@@ -46,30 +47,44 @@
   (tabspaces-session t)
   (tabspaces-session-auto-restore (not centaur-dashboard))
   :config
-  ;; Filter Buffers for Consult-Buffer
-  (with-eval-after-load 'consult
-    ;; hide full buffer list (still available with "b" prefix)
-    (consult-customize consult--source-buffer :hidden t :default nil)
-    ;; set consult-workspace buffer list
-    (defvar consult--source-workspace
-      (list :name     "Workspace Buffer"
-            :narrow   ?w
-            :history  'buffer-name-history
-            :category 'buffer
-            :state    #'consult--buffer-state
-            :default  t
-            :items    (lambda () (consult--buffer-query
-                             :predicate #'tabspaces--local-buffer-p
-                             :sort 'visibility
-                             :as #'buffer-name)))
-      "Set workspace buffer list for consult-buffer.")
-    (add-to-list 'consult-buffer-sources 'consult--source-workspace))
+  (with-no-warnings
+    ;; Filter Buffers for Consult-Buffer
+    (with-eval-after-load 'consult
+      ;; hide full buffer list (still available with "b" prefix)
+      (consult-customize consult--source-buffer :hidden t :default nil)
+      ;; set consult-workspace buffer list
+      (defvar consult--source-workspace
+        (list :name     "Workspace Buffer"
+              :narrow   ?w
+              :history  'buffer-name-history
+              :category 'buffer
+              :state    #'consult--buffer-state
+              :default  t
+              :items    (lambda () (consult--buffer-query
+                               :predicate #'tabspaces--local-buffer-p
+                               :sort 'visibility
+                               :as #'buffer-name)))
+        "Set workspace buffer list for consult-buffer.")
+      (add-to-list 'consult-buffer-sources 'consult--source-workspace))
 
-  (defun my-tabspaces-burry-window (&rest _)
-    "Burry *Messages* buffer"
-    (quit-windows-on messages-buffer-name))
-  (advice-add #'tabspaces-restore-session :after #'my-tabspaces-burry-window))
+    (defun my-tabspaces-hide-childframes (&rest _)
+      "Hide all child frames."
+      (when (childframe-workable-p)
+        ;; Hide shell pop frames
+        (when (and (frame-live-p shell-pop--frame)
+                   (frame-visible-p shell-pop--frame))
+          ;; Hide child frame and refocus in parent frame
+          (make-frame-invisible shell-pop--frame)
+          (select-frame-set-input-focus (frame-parent shell-pop--frame))
+          (setq shell-pop--frame nil))
+        ;; Hide posframe
+        (posframe-hide-all)))
+    (advice-add #'tabspaces-save-session :before #'my-tabspaces-hide-childframes)
 
+    (defun my-tabspaces-burry-window (&rest _)
+      "Burry *Messages* buffer."
+      (quit-windows-on messages-buffer-name))
+    (advice-add #'tabspaces-restore-session :after #'my-tabspaces-burry-window)))
 
 (provide 'init-workspace)
 
