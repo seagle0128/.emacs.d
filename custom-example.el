75baa1f0b69b56b0717bc7f2e2c30c708a088ae6
Vincent Zhang <seagle0128@gmail.com>
2025-09-05 09:32:07 +0800

    chore(fonts): Update preferred font order for Chinese characters
    Adjusted the order of preferred fonts in the commented Chinese font
    loop for better prioritization. Removed "LXGW WenKai Screen" and
    reorganized the list.

--- custom-example.el
+++ custom-example.el
@@ -66,8 +66,8 @@
              return (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend))
 
     ;; Specify font for Chinese characters
-    ;; (cl-loop for font in '("LXGW Neo Xihei" "WenQuanYi Micro Hei Mono" "LXGW WenKai Screen"
-    ;;                        "LXGW WenKai Mono" "PingFang SC" "Microsoft Yahei UI" "Simhei")
+    ;; (cl-loop for font in '("LXGW Neo Xihei" "LXGW WenKai Mono" "WenQuanYi Micro Hei Mono"
+    ;;                        "PingFang SC" "Microsoft Yahei UI" "Simhei")
     ;;          when (font-available-p font)
     ;;          return (progn
     ;;                   (setq face-font-rescale-alist `((,font . 1.3)))
