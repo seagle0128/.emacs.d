4ec5fc782973cc1f8bbc0211a451239aaa397af6
Vincent Zhang <seagle0128@gmail.com>
2025-10-08 16:13:30 +0800

    feat(dired): customize directory icons with specific face
    Add a custom face for directory icons in dired using
    `nerd-icons-dired`. Introduce `my-nerd-icons-icon-for-dir` to apply
    the new directory icon face and update
    `nerd-icons-dired-dir-icon-function` to use it.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -83,7 +83,16 @@
   ;; Shows icons in dired
   (use-package nerd-icons-dired
     :diminish
-    :hook (dired-mode . nerd-icons-dired-mode))
+    :functions (nerd-icons-icon-for-dir my-nerd-icons-icon-for-dir)
+    :hook (dired-mode . nerd-icons-dired-mode)
+    :init
+    (defface nerd-icons-dired-dir-face
+      '((t (:inherit 'font-lock-doc-face)))
+      "Face for the directory icon."
+      :group 'nerd-icons-faces)
+    (defun my-nerd-icons-icon-for-dir (dir)
+      (nerd-icons-icon-for-dir dir :face 'nerd-icons-dired-dir-face))
+    (setq nerd-icons-dired-dir-icon-function #'my-nerd-icons-icon-for-dir))
 
   ;; Extra Dired functionality
   (use-package dired-aux :ensure nil)
