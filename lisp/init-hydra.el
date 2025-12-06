16b25095645b4de2a9b6c0066582df02cec01eba
Vincent Zhang <seagle0128@gmail.com>
2025-12-07 02:29:27 +0800

    refactor(init-hydra): remove unnecessary `with-no-warnings` wrapper
    Simplified the code by removing the `with-no-warnings` wrapper for
    `hydra-set-posframe-show-params`. This change ensures cleaner and
    more maintainable code without impacting functionality.

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -32,25 +32,24 @@
 
 (use-package hydra
   :defines posframe-border-width
-  :functions childframe-completion-workable-p
+  :functions childframe-completion-workable-p hydra-set-posframe-show-params
   :hook (emacs-lisp-mode . hydra-add-imenu)
   :init
   (when (childframe-completion-workable-p)
     (setq hydra-hint-display-type 'posframe)
-    (with-no-warnings
-      (defun hydra-set-posframe-show-params ()
-        "Set hydra-posframe style."
-        (setq hydra-posframe-show-params
-              `(:left-fringe 8
-                :right-fringe 8
-                :internal-border-width ,posframe-border-width
-                :internal-border-color ,(face-background 'posframe-border nil t)
-                :background-color ,(face-background 'tooltip nil t)
-                :foreground-color ,(face-foreground 'tooltip nil t)
-                :lines-truncate t
-                :poshandler posframe-poshandler-frame-center-near-bottom)))
-      (hydra-set-posframe-show-params)
-      (add-hook 'after-load-theme-hook #'hydra-set-posframe-show-params t))))
+    (defun hydra-set-posframe-show-params ()
+      "Set hydra-posframe style."
+      (setq hydra-posframe-show-params
+            `(:left-fringe 8
+              :right-fringe 8
+              :internal-border-width ,posframe-border-width
+              :internal-border-color ,(face-background 'posframe-border nil t)
+              :background-color ,(face-background 'tooltip nil t)
+              :foreground-color ,(face-foreground 'tooltip nil t)
+              :lines-truncate t
+              :poshandler posframe-poshandler-frame-center-near-bottom)))
+    (hydra-set-posframe-show-params)
+    (add-hook 'after-load-theme-hook #'hydra-set-posframe-show-params t)))
 
 (use-package pretty-hydra
   :functions icons-displayable-p
