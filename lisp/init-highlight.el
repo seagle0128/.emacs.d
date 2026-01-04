da57b4b3db18c84f5ad157a0d0e5a7a122e1a7fe
Vincent Zhang <seagle0128@gmail.com>
2026-01-04 18:08:50 +0800

    perf(init-highlight): disable indent-bars-mode for large files
    Add `too-long-file-p` check to conditionally enable
    `indent-bars-mode`. This prevents performance issues on large files
    by ensuring the mode is not enabled unnecessarily.

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -159,6 +159,8 @@ FACE defaults to inheriting from default and highlight."
 
 ;; Highlight indentions
 (use-package indent-bars
+  :functions too-long-file-p
+  :commands indent-bars-mode
   :custom
   (indent-bars-color '(font-lock-comment-face :face-bg nil :blend 0.4))
   (indent-bars-highlight-current-depth '(:face default :blend 0.4))
@@ -172,7 +174,9 @@ FACE defaults to inheriting from default and highlight."
   (indent-bars-prefer-character t)
   (indent-bars-treesit-scope '((python function_definition class_definition for_statement
 				                       if_statement with_statement while_statement)))
-  :hook (prog-mode yaml-mode)
+  :hook ((prog-mode yaml-mode) . (lambda ()
+                                   (unless (too-long-file-p)
+                                     (indent-bars-mode 1))))
   :config (require 'indent-bars-ts))
 
 ;; Colorize color names in buffers
