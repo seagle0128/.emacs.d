e5da76a05b92fa88ab169d647e539efa3ff82dd0
Vincent Zhang <seagle0128@gmail.com>
2026-01-07 23:52:35 +0800

    refactor(hydra): simplify hooks and improve toggle labels
    Removed redundant `emacs-lisp-mode` hooks for hydras. Simplified
    toggle logic and updated toggle labels to be more descriptive.

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -33,8 +33,7 @@
 (use-package hydra
   :defines (consult-imenu-config posframe-border-width)
   :functions childframe-completion-workable-p
-  :hook ((emacs-lisp-mode . hydra-add-imenu)
-         (after-load-theme . hydra-set-posframe-appearance))
+  :hook (after-load-theme . hydra-set-posframe-appearance)
   :init
   (with-eval-after-load 'consult-imenu
     (setq consult-imenu-config
@@ -64,13 +63,7 @@
 (use-package pretty-hydra
   :functions icons-displayable-p
   :bind ("<f6>" . toggles-hydra/body)
-  :hook (emacs-lisp-mode . pretty-hydra-add-imenu)
   :init
-  (defun pretty-hydra-add-imenu ()
-    "Have hydras in `imenu'."
-    (add-to-list 'imenu-generic-expression
-                 '("Hydras" "^.*(\\(pretty-hydra-define\\) \\([a-zA-Z-]+\\)" 2)))
-
   (cl-defun pretty-hydra-title (title &optional icon-type icon-name
                                       &key face height v-adjust)
     "Add an icon in the hydra title."
@@ -92,42 +85,36 @@
       (:title (pretty-hydra-title "Toggles" 'faicon "nf-fa-toggle_on")
        :color amaranth :quit-key ("q" "C-g"))
       ("Basic"
-       (("n" (cond ((fboundp 'display-line-numbers-mode)
-                    (display-line-numbers-mode (if display-line-numbers-mode -1 1)))
-                   ((fboundp 'gblobal-linum-mode)
-                    (global-linum-mode (if global-linum-mode -1 1))))
-         "line number"
-         :toggle (or (bound-and-true-p display-line-numbers-mode)
-                     (bound-and-true-p global-linum-mode)))
-        ("a" global-aggressive-indent-mode "aggressive indent" :toggle t)
-        ("d" global-hungry-delete-mode "hungry delete" :toggle t)
-        ("e" electric-pair-mode "electric pair" :toggle t)
+       (("n" display-line-numbers-mode "line number" :toggle t)
+        ("a" global-aggressive-indent-mode "aggressive indent *" :toggle t)
+        ("d" global-hungry-delete-mode "hungry delete *" :toggle t)
+        ("e" electric-pair-mode "electric pair *" :toggle t)
         ("c" flyspell-mode "spell check" :toggle t)
         ("s" prettify-symbols-mode "pretty symbol" :toggle t)
-        ("l" global-page-break-lines-mode "page break lines" :toggle t)
-        ("b" display-battery-mode "battery" :toggle t)
-        ("i" display-time-mode "time" :toggle t)
-        ("m" doom-modeline-mode "modern mode-line" :toggle t))
+        ("l" global-page-break-lines-mode "page break lines *" :toggle t)
+        ("b" display-battery-mode "battery *" :toggle t)
+        ("i" display-time-mode "time *" :toggle t)
+        ("m" doom-modeline-mode "modern mode-line *" :toggle t))
        "Highlight"
-       (("h l" global-hl-line-mode "line" :toggle t)
-        ("h p" show-paren-mode "paren" :toggle t)
+       (("h l" global-hl-line-mode "line *" :toggle t)
+        ("h p" show-paren-mode "parenthesis *" :toggle t)
         ("h s" symbol-overlay-mode "symbol" :toggle t)
-        ("h r" rainbow-mode "rainbow" :toggle t)
+        ("h r" global-colorful-mode "color *" :toggle t)
         ("h w" (setq-default show-trailing-whitespace (not show-trailing-whitespace))
          "whitespace" :toggle show-trailing-whitespace)
         ("h d" rainbow-delimiters-mode "delimiter" :toggle t)
-        ("h i" highlight-indent-guides-mode "indent" :toggle t)
-        ("h t" global-hl-todo-mode "todo" :toggle t))
+        ("h i" indent-bars-mode "indent" :toggle t)
+        ("h t" global-hl-todo-mode "todo *" :toggle t))
        "Program"
        (("f" flymake-mode "flymake" :toggle t)
         ("O" hs-minor-mode "hideshow" :toggle t)
         ("u" subword-mode "subword" :toggle t)
-        ("W" which-function-mode "which function" :toggle t)
+        ("W" which-function-mode "current function *" :toggle t)
         ("E" toggle-debug-on-error "debug on error" :toggle (default-value 'debug-on-error))
         ("Q" toggle-debug-on-quit "debug on quit" :toggle (default-value 'debug-on-quit))
-        ("v" global-diff-hl-mode "gutter" :toggle t)
-        ("V" diff-hl-flydiff-mode "live gutter" :toggle t)
-        ("M" diff-hl-margin-mode "margin gutter" :toggle t)
+        ("v" global-diff-hl-mode "gutter *" :toggle t)
+        ("V" diff-hl-flydiff-mode "live gutter *" :toggle t)
+        ("M" diff-hl-margin-mode "margin gutter *" :toggle t)
         ("D" diff-hl-dired-mode "dired gutter" :toggle t))
        "Theme"
        (("t a" (centaur-load-theme 'auto) "auto"
