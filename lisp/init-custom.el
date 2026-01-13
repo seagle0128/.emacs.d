235440cd468cfcfb119329d8b155f4267c5b676a
Vincent Zhang <seagle0128@gmail.com>
2026-01-13 19:52:29 +0800

    fix(init-custom): update variable name for emacs-plus path detection
    Refactor variable name from `ns-emacs-plus-version` to
    `ns-emacs-plus-injected-path` for improved clarity and compatibility
    in exec-path handling logic.

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -77,7 +77,7 @@
 
 (defcustom centaur-use-exec-path-from-shell
   (and (or (memq window-system '(mac ns x)) (daemonp))
-       (not (bound-and-true-p ns-emacs-plus-version)))
+       (not (bound-and-true-p ns-emacs-plus-injected-path)))
   "Use `exec-path-from-shell' or not.
 If using emacs-plus with path ejection, set to nil."
   :group 'centaur
