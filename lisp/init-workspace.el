b8549c2dc5e56b88f121228343c46440091acac2
Vincent Zhang <seagle0128@gmail.com>
2026-01-14 10:27:22 +0800

    fix(workspace): correct incorrect variable names for consult sources
    Correct obsolete variable names from `consult--source-xxx` to
    `consult-source-xxx` for proper functionality.
    
    See https://github.com/minad/consult/commit/3a8ac46916c0699f0905ac4fe65d72768add3efb.

--- lisp/init-workspace.el
+++ lisp/init-workspace.el
@@ -52,9 +52,9 @@
     ;; Filter Buffers for Consult-Buffer
     (with-eval-after-load 'consult
       ;; hide full buffer list (still available with "b" prefix)
-      (consult-customize consult--source-buffer :hidden t :default nil)
+      (consult-customize consult-source-buffer :hidden t :default nil)
       ;; set consult-workspace buffer list
-      (defvar consult--source-workspace
+      (defvar consult-source-workspace
         (list :name     "Workspace Buffer"
               :narrow   ?w
               :history  'buffer-name-history
@@ -66,7 +66,7 @@
                                :sort 'visibility
                                :as #'buffer-name)))
         "Set workspace buffer list for consult-buffer.")
-      (add-to-list 'consult-buffer-sources 'consult--source-workspace))
+      (add-to-list 'consult-buffer-sources 'consult-source-workspace))
 
     (defun my-tabspaces-delete-childframe (&rest _)
       "Delete all child frames."
