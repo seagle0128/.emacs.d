a8f9270779eeb8fecff491277ca51185299da66c
Vincent Zhang <seagle0128@gmail.com>
2026-03-01 21:50:05 +0800

    refactor(ai): enable gptel and simplify configuration
    gptel-magit and :custom have conflicts, and throw error:
    "Eager macro-expansion failure".
    
    Close https://github.com/karthink/gptel/issues/1272.

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -33,27 +33,22 @@
 (eval-when-compile
   (require 'init-const))
 
-;; FIXME: Eager macro-expansion failure
-;; @see https://github.com/karthink/gptel/issues/1272
 ;; Interact with ChatGPT or other LLMs
 (use-package gptel
-  :disabled
   :functions gptel-make-openai
-  :custom
-  (gptel-model 'gpt-4o)
+  :init
   ;; Put the apikey to `auth-sources'
-  ;; Format: "machine {HOST} login {USER} password {APIKEY}"
-  ;; The LLM host is used as HOST, and "apikey" as USER.
-  (gptel-backend (gptel-make-openai "Github Models"
-                   :host "models.inference.ai.azure.com"
-                   :endpoint "/chat/completions?api-version=2024-05-01-preview"
-                   :stream t
-                   :key 'gptel-api-key
-                   :models '(gpt-4o))))
+  ;; format: "machine {HOST} login apikey password {token}"
+  (setq gptel-model 'gpt-4o
+        gptel-backend (gptel-make-openai "Github Models"
+                        :host "models.inference.ai.azure.com"
+                        :endpoint "/chat/completions?api-version=2024-05-01-preview"
+                        :stream t
+                        :key 'gptel-api-key
+                        :models '(gpt-4o))))
 
 ;; Generate commit messages for magit
 (use-package gptel-magit
-  :disabled
   :hook (magit-mode . gptel-magit-install))
 
 ;; A native shell experience to interact with ACP agents
