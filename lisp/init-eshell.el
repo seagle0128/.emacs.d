a394103f180d75b6cddac70f7094c807621f21ef
Chetan Koneru <kchetan.hadoop@gmail.com>
2026-09-09 21:43:13 -0400

    fix(eshell): avoid enabling esh-help Eldoc integration by default
    Prevent esh-help's Eldoc support from stalling Eshell startup due to
    synchronous manual-page lookups. Load esh-help only for its explicit
    interactive command instead of enabling Eldoc integration in all
    Eshell buffers.

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -33,6 +33,7 @@
 ;; Emacs command shell
 (use-package eshell
   :ensure nil
+  :after esh-mode
   :defines eshell-prompt-function
   :bind (:map eshell-mode-map
          ([remap recenter-top-bottom] . eshell/clear))
@@ -111,15 +112,10 @@
     :init (setq eshell-highlight-prompt nil
                 eshell-prompt-function #'epe-theme-lambda))
 
-  ;; `eldoc' support
+  ;; Load `esh-help' only for its explicit command.  Its Eldoc integration
+  ;; performs synchronous manual-page lookups and can stall Eshell on open.
   (use-package esh-help
-    :commands esh-help-eldoc-command
-    :preface
-    (defun my/eshell-enable-esh-help-eldoc ()
-      "Enable `esh-help' Eldoc support in the current Eshell buffer."
-      (setq-local eldoc-documentation-function
-                  #'esh-help-eldoc-command))
-    :hook (eshell-mode . my/eshell-enable-esh-help-eldoc))
+    :commands esh-help-run-help)
 
   ;; `cd' to frequent directory in `eshell'
   (use-package eshell-z
